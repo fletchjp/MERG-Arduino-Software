d6b4c596c49d782410706aeb2e6232253dfbcb1f
John Fletcher <john@bunbury28.plus.com>
2021-05-08 22:50:48 +0100

    Add CANASIGNALI2C_CS15 example

---
+++ CANASIGNALI2C_CS15/ASIGNALI2C.h
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////////
+// ASIGNALI2C.h  Modular header for I2C signals
+////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ASIGNALI2C_H
+#define ASIGNALI2C_H
+
+
+// I2C libraries
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+// Signal control libraries as needed.
+#include "FastPWMLight.h"
+#include "Signal2Aspect.h"
+#include "DigitalInput.h"
+
+
+
+#endif
