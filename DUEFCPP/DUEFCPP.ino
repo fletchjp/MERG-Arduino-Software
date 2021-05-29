e470f41fc9dceab3f9bfdd00a165066d5eb150be
John Fletcher <john@bunbury28.plus.com>
2021-05-29 23:21:08 +0100

    Update DUEFCPP.ino
    Devlop example

--- DUEFCPP/DUEFCPP.ino
+++ DUEFCPP/DUEFCPP.ino
@@ -1,12 +1,33 @@
 // DUEFCPP
 
+
+
 // Demo of FC++ Maybe operation
 #include "fcpp/prelude.h"
 
+#include <Streaming.h>
+
+
+using namespace fcpp;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin (115200);
+  //Serial << endl << endl << F("> DUEFCPP ** ") << __FILE__ << endl;
+  Serial << Plus()(1,2) << endl;
+  Serial << Plus()(1.5,2.3) << endl;
+  Serial << Plus()(1)(2) << endl;
+  Serial << Inc()(1) << endl;
 
+  Maybe<int> mx(2), my(3);
+  Maybe<int> mz;
+  if (mz.is_nothing()) Serial << "mz is nothing" << endl;
+  mz = just(Plus()(mx.value(),my.value()));
+  if (mz.is_nothing()) { 
+     Serial << "mz is nothing" << endl;
+  } else {
+     Serial << "mz has " << mz.value() << endl;
+  }
 }
 
 void loop() {
