44ee313d62b3a171dbf46fd92e17a5fa0f5460b5
John Fletcher <john@bunbury28.plus.com>
2021-05-29 16:34:13 +0100

    Create DUEFCPP.ino
    Start fcpp example

---
+++ DUEFCPP/DUEFCPP.ino
@@ -0,0 +1,15 @@
+// DUEFCPP
+
+// Demo of FC++ Maybe operation
+#include "fcpp/prelude.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
