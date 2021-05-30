641689a8c1240009b1f9b6c42aedd70ba5ea7738
John Fletcher <john@bunbury28.plus.com>
2021-05-30 01:26:50 +0100

    Update DUEFCPP.ino
    Change to use declared objects

--- DUEFCPP/DUEFCPP.ino
+++ DUEFCPP/DUEFCPP.ino
@@ -1,8 +1,7 @@
 // DUEFCPP
-
-
-
 // Demo of FC++ Maybe operation
+// This will work on ARDUINO DUE but not on AVR boards.
+
 #include "fcpp/prelude.h"
 
 #include <Streaming.h>
@@ -13,16 +12,16 @@ using namespace fcpp;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (115200);
-  //Serial << endl << endl << F("> DUEFCPP ** ") << __FILE__ << endl;
-  Serial << Plus()(1,2) << endl;
-  Serial << Plus()(1.5,2.3) << endl;
-  Serial << Plus()(1)(2) << endl;
-  Serial << Inc()(1) << endl;
+  Serial << endl << endl << F("> DUEFCPP ** ") << __FILE__ << endl;
+  Serial << plus(1,2) << endl;
+  Serial << plus(1.5,2.3) << endl;
+  Serial << plus(1)(2) << endl;
+  Serial << inc(1) << endl;
 
   Maybe<int> mx(2), my(3);
   Maybe<int> mz;
   if (mz.is_nothing()) Serial << "mz is nothing" << endl;
-  mz = just(Plus()(mx.value(),my.value()));
+  mz = just(plus(mx.value(),my.value()));
   if (mz.is_nothing()) { 
      Serial << "mz is nothing" << endl;
   } else {
