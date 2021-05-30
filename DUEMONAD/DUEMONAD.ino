2fe066b2f91c65c81614043360632cbd63b932c0
John Fletcher <john@bunbury28.plus.com>
2021-05-30 21:48:35 +0100

    Create DUEMONAD.ino
    New file with Monad examples

---
+++ DUEMONAD/DUEMONAD.ino
@@ -0,0 +1,151 @@
+// DUEMONAD
+// Demo of FC++ Maybe Monad operations
+// This will work on ARDUINO DUE but not on AVR boards.
+#define FCPP_ENABLE_LAMBDA
+#include "fcpp/prelude.h"
+
+#include <Streaming.h>
+
+
+using namespace fcpp;
+
+// Failure example from https://people.cs.umass.edu/~yannis/fc++/fcpp-lambda.pdf
+
+int f(int x) {
+  if (x >= 0) return x;
+  else return -1;
+}
+
+int g(int x)
+{
+  return f(x);
+}
+
+int h(int x, int y)
+{
+  if (f(x) < 0) return f(x);
+  else
+  return f(x) + g(y);
+}
+
+Maybe<int> mf(int x) {
+  if (x >= 0) return just(x);
+  else return NOTHING;
+}
+
+Maybe<int> mg(int x)
+{
+  return mf(x);
+}
+
+Maybe<int> mh(int x, int y)
+{
+  if (mf(x).is_nothing()) return mf(x);
+  else
+  return just(mf(x).value() + mg(y).value());
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (115200);
+  Serial << endl << endl << F("> DUEFCPP ** ") << __FILE__ << endl;
+  Serial << "Some simple FC++ operations" << endl;
+  Serial << "plus(1,2) = " << plus(1,2) << endl;
+  Serial << "plus(1.5,2.3) = " << plus(1.5,2.3) << endl;
+  Serial << "plus(1)(2) = " << plus(1)(2) << endl;
+  Serial << "inc(1) = " << inc(1) << endl;
+  Serial << "--------------------------" << endl;
+
+  Maybe<int> mx(2), my(3);
+  Maybe<int> mz;
+  if (mz.is_nothing()) Serial << "mz is nothing" << endl;
+  mz = just(plus(mx.value(),my.value()));
+  if (mz.is_nothing()) { 
+     Serial << "mz is nothing" << endl;
+  } else {
+     Serial << "mz has " << mz.value() << endl;
+  }
+  // Use of Monad MaybeM with liftM to move inc into Maybe.
+  mz = liftM<MaybeM>()(inc)(mx);
+  Serial << "mz = liftM<MaybeM>()(inc)(mx);" << endl;
+  if (mz.is_nothing()) { 
+     Serial << "mz is nothing" << endl;
+  } else {
+     Serial << "mz has " << mz.value() << endl;
+  }
+  Serial << "--------------------------" << endl;
+  LambdaVar<4> W;
+  LambdaVar<1> X;
+  LambdaVar<2> Y;
+  LambdaVar<3> Z;
+  Serial << "h(-1,2) = " << h(-1,2) << endl;
+  Maybe<int> mhr = mh(-1,2);
+  if (mhr.is_nothing()) { 
+     Serial << "mhr is nothing" << endl;
+  } else {
+     Serial << "mhr has " << mhr.value() << endl;
+  }
+  Maybe<int> mgr = liftM<MaybeM>()(ptr_to_fun(&g))(mf(2));
+  if (mgr.is_nothing()) { 
+     Serial << "mgr is nothing" << endl;
+  } else {
+     Serial << "mgr has " << mgr.value() << endl;
+  }
+  mz = lambda()[ compM<MaybeM>()[ inc[Z] | Z <= just[2] ] ]();
+  if (mz.is_nothing()) { 
+     Serial << "mz is nothing" << endl;
+  } else {
+     Serial << "mz has " << mz.value() << endl;
+  }
+  mhr = lambda()[ compM<MaybeM>()[ Z | Z <= just[2] ] ]();
+  mhr = lambda(X)[ compM<MaybeM>()[ Z | Z <= just[X] ] ](2);
+  mhr = lambda(X)[ compM<MaybeM>()[ Z | Z <= ptr_to_fun(&mf)[X] ] ](2);
+  mhr = lambda(X)[ compM<MaybeM>()[ Z | Y <= ptr_to_fun(&mf)[X], W <=  ptr_to_fun(&mg)[Y], Z <=  ptr_to_fun(&mh)[Y,W] ] ](2);
+  if (mhr.is_nothing()) { 
+     Serial << "mhr is nothing" << endl;
+  } else {
+     Serial << "mhr has " << mhr.value() << endl;
+  }
+  // Failure case
+  mhr = lambda(X)[ compM<MaybeM>()[ Z | Y <= ptr_to_fun(&mf)[X], W <=  ptr_to_fun(&mg)[Y], Z <=  ptr_to_fun(&mh)[Y,W] ] ](-2);
+  if (mhr.is_nothing()) { 
+     Serial << "mhr is nothing" << endl;
+  } else {
+     Serial << "mhr has " << mhr.value() << endl;
+  }
+  Serial << "--------------------------" << endl;
+
+
+  List<double>::iterator idx1, idx2;
+  List<double> x1 = list_with(0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0);
+  List<double> x2 = fcpp::map(fcpp::minus(1.0),x1);
+  double sumx1 = foldl(plus,0.,x1);
+  for (idx1 = x1.begin(), idx2 = x2.begin(); idx1 != x1.end(); ++idx1, ++idx2) {
+     Serial << *idx1 << " " << *idx2 << endl;
+  }
+  Serial << "Sum of x1 = " << sumx1 << endl;
+  Serial << "--------------------------" << endl;
+
+  List<int> ix1 = enumFromTo(0,10);
+  List<bool> bodds = fcpp::map(odd,ix1);
+  List<bool> bevens = fcpp::map(even,ix1);
+  List<int> odds = filter(odd,ix1);
+  int sum_odds = foldl(plus,0,odds);
+  int sum_odds2 = foldl(plus,0,filter(odd,ix1));
+  List<int>::iterator iix1;
+  List<bool>::iterator ib1, ib2;
+  Serial << "Length of bodds is " << length(bodds) << endl;
+  for (iix1 = ix1.begin(), ib1 = bodds.begin(), ib2 = bevens.begin(); iix1 != ix1.end(); ++iix1, ++ib1, ++ib2) {
+     Serial << *iix1 << " " << *ib1 << " " << *ib2 << endl;
+  }
+
+  Serial << "Length of odds is " << length(odds) << endl;
+  Serial << "sum of the odds is " << sum_odds << endl;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
