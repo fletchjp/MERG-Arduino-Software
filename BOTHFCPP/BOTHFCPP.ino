f503dd08b13c019570bee4b8ed342058e0e9b201
John Fletcher <john@bunbury28.plus.com>
2021-06-10 12:15:13 +0100

    Update BOTHFCPP.ino
    Change to remove use of Streaming

--- BOTHFCPP/BOTHFCPP.ino
+++ BOTHFCPP/BOTHFCPP.ino
@@ -1,63 +1,68 @@
-// DUEFCPP
+// BOTHFCPP
 // Demo of FC++ Maybe operation
-// This will work on ARDUINO DUE but not on AVR boards.
-
+// This will work on ARDUINO DUE and also on AVR boards.
+// The secret is to include this to have the std library.
 #include<ArduinoSTL.h>
+#include<type_traits>
 
 #include "fcpp/prelude.h"
 
-#include <Streaming.h>
+// Now there is a problem with std::is_signed on the DUE but not on the MEGA.
+//#include <Streaming.h>
 
+using namespace std;
 
 using namespace fcpp;
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (115200);
-  Serial << F("> DUEFCPP ** ") << __FILE__ << endl;
-  Serial << "Some simple FC++ operations" << endl;
-  Serial << "plus(1,2) = " << plus(1,2) << endl;
-  Serial << "plus(1.5,2.3) = " << plus(1.5,2.3) << endl;
-  Serial << "plus(1)(2) = " << plus(1)(2) << endl;
-  Serial << "inc(1) = " << inc(1) << endl;
-  Serial << "--------------------------" << endl;
+  cout << F("> BOTHFCPP ** ") << __FILE__ << endl;
+  cout << "Some simple FC++ operations" << endl;
+  cout << "plus(1,2) = " << fcpp::plus(1,2) << endl;
+  cout << "plus(1.5,2.3) = " << fcpp::plus(1.5,2.3) << endl;
+  cout << "plus(1)(2) = " << fcpp::plus(1)(2) << endl;
+  cout << "inc(1) = " << inc(1) << endl;
+  cout << "--------------------------" << endl;
 
   Maybe<int> mx(2), my(3);
   Maybe<int> mz;
-  if (mz.is_nothing()) Serial << "mz is nothing" << endl;
-  mz = just(plus(mx.value(),my.value()));
+  if (mz.is_nothing()) cout << "mz is nothing" << endl;
+  mz = just(fcpp::plus(mx.value(),my.value()));
   if (mz.is_nothing()) { 
-     Serial << "mz is nothing" << endl;
+     cout << "mz is nothing" << endl;
   } else {
-     Serial << "mz has " << mz.value() << endl;
+     cout << "mz has " << mz.value() << endl;
   }
-  Serial << "--------------------------" << endl;
+  cout << "--------------------------" << endl;
 
   List<double>::iterator idx1, idx2;
   List<double> x1 = list_with(0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0);
   List<double> x2 = fcpp::map(fcpp::minus(1.0),x1);
-  double sumx1 = foldl(plus,0.,x1);
+  double sumx1 = foldl(fcpp::plus,0.,x1);
   for (idx1 = x1.begin(), idx2 = x2.begin(); idx1 != x1.end(); ++idx1, ++idx2) {
-     Serial << *idx1 << " " << *idx2 << endl;
+     cout << *idx1 << " " << *idx2 << endl;
   }
-  Serial << "Sum of x1 = " << sumx1 << endl;
-  Serial << "--------------------------" << endl;
+  cout << "Sum of x1 = " << sumx1 << endl;
+  cout << "--------------------------" << endl;
 
   List<int> ix1 = enumFromTo(0,10);
   List<bool> bodds = fcpp::map(odd,ix1);
   List<bool> bevens = fcpp::map(even,ix1);
   List<int> odds = filter(odd,ix1);
-  int sum_odds = foldl(plus,0,odds);
-  int sum_odds2 = foldl(plus,0,filter(odd,ix1));
+  int sum_odds = foldl(fcpp::plus,0,odds);
+  int sum_odds2 = foldl(fcpp::plus,0,filter(odd,ix1));
   List<int>::iterator iix1;
   List<bool>::iterator ib1, ib2;
-  Serial << "Length of bodds is " << length(bodds) << endl;
+  cout << "Length of bodds is " << length(bodds) << endl;
   for (iix1 = ix1.begin(), ib1 = bodds.begin(), ib2 = bevens.begin(); iix1 != ix1.end(); ++iix1, ++ib1, ++ib2) {
-     Serial << *iix1 << " " << *ib1 << " " << *ib2 << endl;
+     cout << *iix1 << " " << *ib1 << " " << *ib2 << endl;
   }
 
-  Serial << "Length of odds is " << length(odds) << endl;
-  Serial << "sum of the odds is " << sum_odds << endl;
+  cout << "Length of odds is " << length(odds) << endl;
+  cout << "sum of the odds is " << sum_odds << endl;
 
 }
 
