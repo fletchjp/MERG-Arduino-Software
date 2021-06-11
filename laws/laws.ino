bd2b8b5566aa167f8cbaa094a2b67b46934a3d76
John Fletcher <john@bunbury28.plus.com>
2021-06-11 17:43:00 +0100

    Create laws.ino
    Start new example

---
+++ laws/laws.ino
@@ -0,0 +1,33 @@
+// laws 
+
+// A first attempt to get FC++ functor laws operational
+
+// This will now work with FC++ as an Arduino library - just include "prelude.h" and it will find it.
+
+#include<ArduinoSTL.h>
+
+#include <iostream>
+#include <vector>
+#define FCPP_DEBUG
+#include "prelude.h"
+
+using namespace fcpp;
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  cout << "========================" << endl;
+  cout << "ecoop1b Observer example" << endl;
+  cout << "========================" << endl;
+  cout << "========================" << endl;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
