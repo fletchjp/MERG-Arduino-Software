45a4eb365d04f2de6cd5f575c1d7ea06b8eab3e8
John Fletcher <john@bunbury28.plus.com>
2021-06-11 18:39:02 +0100

    Update laws.ino
    Add code for functor laws

--- laws/laws.ino
+++ laws/laws.ino
@@ -18,19 +18,111 @@
 #define FCPP_PATTERN // define this to include pattern.h
 #include "prelude.h"
 #include "functors.h"
+#include "comonad.h"
 
 using namespace fcpp;
 
-using std::cout;
-using std::endl;
+//using std::cout;
+//using std::endl;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  cout << "========================" << endl;
-  cout << "Functor laws examples" << endl;
-  cout << "========================" << endl;
-  cout << "========================" << endl;
+  std::cout << "==================================" << std::endl;
+  std::cout << "Functor laws examples" << std::endl;
+  std::cout << "Page numbers refer to the book" << std::endl;
+  std::cout << "Learn You a Haskell for Great Good" << std::endl;
+  std::cout << "==================================" << std::endl;
+  List<int>::iterator it;
+  std::cout << "===============================" << std::endl;
+  std::cout << "Here is fmap actually working!!" << std::endl;
+  std::cout << "===============================" << std::endl;
+  std::cout << "id ( just(3) )                 : " << id ( just(3) )
+            << " (page 224)"<< std::endl;  
+  std::cout << "===============================" << std::endl;
+  std::cout << "Functor Law 1        (page 223)" << std::endl;
+  std::cout << "     fmap id = id" << std::endl;
+  std::cout << "===============================" << std::endl;
+  std::cout << "id ( just(3) )                 : "
+            <<  id ( just(3) ) << std::endl;
+  std::cout << "fmap (id) ( just(3) )          : "
+            <<  fmap (id)( just(3) ) << std::endl;
+  std::cout << "fmap (id) ( right(3) )         : "
+            <<  fmap (id)( right(3) ) << std::endl;
+  std::cout << "fmap (id) ( ident(3) )         : "
+            <<  fmap (id)( ident(3) ) << std::endl;
+  std::cout << "=================================" << std::endl;
+  std::cout << "Functor Law 2          (page 224)" << std::endl;
+  std::cout << "fmap (f . g ) = fmap(f) . fmap(g)" << std::endl;
+  std::cout << "=================================" << std::endl;
+  std::cout << " fmap (inc  ^dot^ dec)( ident(3) )     : "
+            <<  fmap (inc ^dot^ dec)( ident(3) ) << std::endl;
+  std::cout << "(fmap (inc) ^dot^ fmap(dec))(ident(3)) : "
+            <<  (fmap (inc) ^dot^ fmap(dec)) (ident(3) ) << std::endl;
+  std::cout << " fmap (inc  ^dot^ dec)( just(3) )      : "
+            <<  fmap (inc ^dot^ dec)( just(3) ) << std::endl;
+  std::cout << "(fmap (inc) ^dot^ fmap(dec))(just(3))  : "
+            <<  (fmap (inc) ^dot^ fmap(dec)) (just(3) ) << std::endl;
+  std::cout << " fmap (inc  ^dot^ dec)( right(3) )     : "
+            <<  fmap (inc ^dot^ dec)( right(3) ) << std::endl;
+  std::cout << "(fmap (inc) ^dot^ fmap(dec))(right(3)) : "
+            <<  (fmap (inc) ^dot^ fmap(dec)) (right(3) ) << std::endl;
+  std::cout << "=================================" << std::endl;
+  List<int> fl2_1 = makeList1(3);
+  std::cout << " List<int> fl2_1 = makeList1(3);" << std::endl;
+  List<int> fl2_2 = fmap (inc ^dot^ dec)( fl2_1 );
+  List<int> fl2_3 = (fmap (inc) ^dot^ fmap(dec)) (fl2_1 );
+  std::cout << " fmap (inc ^dot^ dec)( fl2_1 ) = [ ";
+  for (it = fl2_2.begin(); it != fl2_1.end(); ++it) {
+     std::cout << *it << " ";
+  }
+  std::cout << "]" << std::endl;
+  std::cout << " (fmap (inc) ^dot^ fmap(dec))(fl2_1) = [ ";
+  for (it = fl2_3.begin(); it != fl2_3.end(); ++it) {
+     std::cout << *it << " ";
+  }
+  std::cout << "]" << std::endl;
+  std::cout << "=================================" << std::endl;
+  std::cout << "More examples. p.229" << std::endl;
+  std::cout << "=================================" << std::endl;
+  std::cout << "(just(plus(3)) ^star^ just(9)) : "
+            <<  (just(plus(3)) ^star^ just(9)) << std::endl;  // p.229
+  std::cout << "====================================" << std::endl;
+  std::cout << "Applicative Functor Law 1 (page 238)" << std::endl;
+  std::cout << " pure f <*> x = fmap f x" << std::endl;
+  std::cout << "====================================" << std::endl;
+  std::cout << "fmap (inc) ( just(3) )         : "
+            <<  fmap (inc)( just(3) ) << std::endl;
+/*
+  Maybe<int> mx1 = pure (inc) ^star^ ( just(3) );
+  std::cout << "pure (inc) ^star^ ( just(3) )  : "
+            <<  mx1 << std::endl;
+  Maybe<int> mx1a = MaybeA::pure()(inc) ^star^ (3);
+  std::cout << "MaybeA::pure()(inc) ^star^ (3) : "
+            <<  mx1a << std::endl;
+  // pure is actually optional here as pure == id when on its own.
+  Maybe<int> mx1b = inc ^star^ ( just(3) );
+*/
+  std::cout << "====================================" << std::endl;
+  std::cout << "Applicative Functor Law 2 (page 238)" << std::endl;
+  std::cout << " pure id <*> v = v" << std::endl;
+  std::cout << "====================================" << std::endl;
+  //Maybe<int> mx2 = pure (id) ^star^ ( just(3) );
+  //std::cout << "pure (id) ^star^ ( just(3) )    : "
+  //          <<  mx2 << std::endl;
+  std::cout << "====================================" << std::endl;
+  std::cout << "Applicative Functor Law 3 (page 238)" << std::endl;
+  std::cout << "pure (.)<*>u<*>v<*>w = u<*>(v<*>w)" << std::endl;
+  std::cout << "Note: in the example one of the operators cannot be inferred."
+            << std::endl;
+  std::cout << "====================================" << std::endl;
+
+  std::cout << "====================================" << std::endl;
+  std::cout << "Applicative Functor Law 4 (page 238)" << std::endl;
+  std::cout << " pure f <*> pure x = pure ( f x )" << std::endl;
+  std::cout << "====================================" << std::endl;
+
+  std::cout << "=================================" << std::endl;
 
 }
 
