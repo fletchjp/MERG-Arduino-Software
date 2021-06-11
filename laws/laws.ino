e4f084f51cb0a72d24fb2fc9dae7a929dc40659a
John Fletcher <john@bunbury28.plus.com>
2021-06-11 18:00:28 +0100

    Update laws.ino
    Bringing in headers

--- laws/laws.ino
+++ laws/laws.ino
@@ -2,26 +2,33 @@
 
 // A first attempt to get FC++ functor laws operational
 
+// This comes from the old file unify.cpp which has a lot of explanations.
+
 // This will now work with FC++ as an Arduino library - just include "prelude.h" and it will find it.
 
 #include<ArduinoSTL.h>
 
 #include <iostream>
-#include <vector>
+// Need 5 parameter functoids for example.
+#define FCPP152
+#define FCPP_ENABLE_LAMBDA
+#define FCPP_USE_PROMOTE
 #define FCPP_DEBUG
+#define FCPP_EXTRA_STRONG_CURRY
+#define FCPP_PATTERN // define this to include pattern.h
 #include "prelude.h"
+#include "functors.h"
 
 using namespace fcpp;
 
 using std::cout;
 using std::endl;
-using std::vector;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   cout << "========================" << endl;
-  cout << "ecoop1b Observer example" << endl;
+  cout << "Functor laws examples" << endl;
   cout << "========================" << endl;
   cout << "========================" << endl;
 
