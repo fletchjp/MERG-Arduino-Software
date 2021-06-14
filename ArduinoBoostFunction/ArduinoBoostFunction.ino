3066978a44c3cefcc9782c19416de59bee8aa5fd
John Fletcher <john@bunbury28.plus.com>
2021-06-14 15:58:33 +0100

    Update ArduinoBoostFunction.ino
    Sort out the problem that Arduino C++ does not support exceptions and boost::function does not disable them.

--- ArduinoBoostFunction/ArduinoBoostFunction.ino
+++ ArduinoBoostFunction/ArduinoBoostFunction.ino
@@ -6,14 +6,56 @@
 // Example use of a Boost header
 #define BOOST_DISABLE_ASSERTS
 #define BOOST_NO_EXCEPTIONS
+#define BOOST_EXCEPTION_DISABLE
 #undef B1
 #include <iostream>
-//#include <boost/function.hpp>
+#include <exception>
+#include <stdexcept>
+
+namespace std {
+
+   class runtime_error {
+   public:
+   runtime_error( std::exception & e ) { }
+   runtime_error( std::string const & s ) { }
+  };
+}
+
+namespace boost {
+
+  void throw_exception( std::exception & e ) { }
+  void throw_exception(const std::exception & e ) { }
+  void throw_exception( std::runtime_error& e) { }
+  void throw_exception(const std::runtime_error& e) { }
+
+}
+
+#include <boost/function.hpp>
 #include <string>
 
-
+/*
+  class SomeError : public std::runtime_error {
+    public:
+      SomeError() : std::runtime_error(std::string("Some error")) {}
+  };
+*/
 
 using namespace std;
+
+int f0()
+{
+  return 0;
+}
+
+int f1(int x)
+{
+  return x + x;
+}
+
+int f2(int x, int y)
+{
+  return x + y;
+}
 
 void setup() {
   Serial.begin(9600);
