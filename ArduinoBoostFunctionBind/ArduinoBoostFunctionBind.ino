64e3a6f179f239917f789650dee9df31cc711031
John Fletcher <john@bunbury28.plus.com>
2021-06-14 21:38:05 +0100

    Create ArduinoBoostFunctionBind.ino
    New example adding boost/bind

---
+++ ArduinoBoostFunctionBind/ArduinoBoostFunctionBind.ino
@@ -0,0 +1,69 @@
+// ArduinoBoostFunctionBind
+// This example now works as long as the AVR Boards are 1.8.2 not 1.8.3
+#include <ArduinoSTL.h>
+// This is an old version of boost from 2012.
+#include <boost_1_51_0.h>
+// Example use of a Boost header
+
+//////////////////////////////////////////////////////////////////////
+// boost::function
+// I set out to work with an example of boost function.
+// It has taken some time to get the header to compile.
+// The problem is that boost::function throws exceptions
+// which are not supported by the Arduino C++.
+// The solution is to put some code here which will
+// satsify the compiler.
+//////////////////////////////////////////////////////////////////////
+#define BOOST_DISABLE_ASSERTS
+#define BOOST_NO_EXCEPTIONS
+#define BOOST_EXCEPTION_DISABLE
+#undef B1
+#undef F  // This is needed for the DUE.
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+namespace std {
+
+   class runtime_error {
+   public:
+   runtime_error( std::exception & e ) { }
+   runtime_error( std::string const & s ) { }
+  };
+}
+
+namespace boost {
+
+  void throw_exception( std::exception & e ) { }
+  void throw_exception(const std::exception & e ) { }
+  void throw_exception( std::runtime_error& e) { }
+  void throw_exception(const std::runtime_error& e) { }
+
+}
+
+#include <boost/utility/result_of.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <string>
+
+/*
+  class SomeError : public std::runtime_error {
+    public:
+      SomeError() : std::runtime_error(std::string("Some error")) {}
+  };
+*/
+
+
+void setup() {
+  Serial.begin(9600);
+  std::cout << std::endl;
+  std::cout << "==============================================" << std::endl;
+  std::cout << "Boost function bind example running on Arduino" << std::endl;
+  std::cout << "==============================================" << std::endl;
+ 
+  
+}
+
+void loop() {
+
+}
