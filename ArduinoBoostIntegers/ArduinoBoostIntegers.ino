836cc99e4329ed2562876b7467998393f51cf88b
John Fletcher <john@bunbury28.plus.com>
2021-06-14 12:05:03 +0100

    Create ArduinoBoostIntegers.ino
    New example for Boost and STL libraries

---
+++ ArduinoBoostIntegers/ArduinoBoostIntegers.ino
@@ -0,0 +1,24 @@
+// ArduinoBoostIntegers
+// This example now works as long as the AVR Boards are 1.8.2 not 1.8.3
+#include <ArduinoSTL.h>
+// This is an old version of boost from 2012.
+#include <boost_1_51_0.h>
+
+#include <boost/type_traits/is_void.hpp>
+
+using namespace std;
+
+void setup() {
+  Serial.begin(9600);
+  cout << "Feed me an integers." << endl;
+}
+
+void loop() {
+  int foo;
+  if (cin >> foo) { 
+    cout << "You fed me " << foo << endl;
+  }else{
+    cin.clear();
+    cin.ignore();
+  }
+}
