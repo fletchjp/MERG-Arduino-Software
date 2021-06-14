4e2da4597258ac6fb2b976dde8f0bc4550008b2a
John Fletcher <john@bunbury28.plus.com>
2021-06-14 12:05:14 +0100

    Update ArduinoSTLIntegers.ino
    Remove boost content

--- ArduinoSTLIntegers/ArduinoSTLIntegers.ino
+++ ArduinoSTLIntegers/ArduinoSTLIntegers.ino
@@ -1,10 +1,6 @@
 // ArduinoSTLIntegers
 // This example now works as long as the AVR Boards are 1.8.2 not 1.8.3
 #include <ArduinoSTL.h>
-// This is an old version of boost from 2012.
-#include <boost_1_51_0.h>
-
-#include <boost/type_traits/is_void.hpp>
 
 using namespace std;
 
