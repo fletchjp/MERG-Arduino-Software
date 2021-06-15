c93ab377779b0d17a04b40bbeeca09f679f8e53a
John Fletcher <john@bunbury28.plus.com>
2021-06-15 12:32:53 +0100

    Update ArduinoBoost166Integers.ino
    Make this just integers example

--- ArduinoBoost166Integers/ArduinoBoost166Integers.ino
+++ ArduinoBoost166Integers/ArduinoBoost166Integers.ino
@@ -1,46 +1,13 @@
-// ArduinoBoostIntegers
+// ArduinoBoost166Integers
 // This example now works as long as the AVR Boards are 1.8.2 not 1.8.3
 #include <ArduinoSTL.h>
 // This is an old version of boost from 2012.
 //#include <boost_1_51_0.h>
 //#include <typeinfo>
 //#include <array>
-#include <functional>
-#define BOOST_NO_CWCHAR
-#define BOOST_NO_CXX11_HDR_TYPEINDEX
-#define BOOST_NO_CXX11_HDR_ARRAY
-#define BOOST_NO_CXX11_HDR_TUPLE
-#define BOOST_NO_CXX11_SMART_PTR
-#define BOOST_NO_STD_WSTRING
-#define BOOST_DISABLE_ASSERTS
-#define BOOST_NO_EXCEPTIONS
-#define BOOST_EXCEPTION_DISABLE
-
-namespace std {
-
-   class runtime_error {
-   public:
-   runtime_error( std::exception & e ) { }
-   runtime_error( std::string const & s ) { }
-  };
-}
-
-namespace boost {
-
-  void throw_exception( std::exception & e ) { }
-  void throw_exception(const std::exception & e ) { }
-  void throw_exception( std::runtime_error& e) { }
-  void throw_exception(const std::runtime_error& e) { }
-
-}
-
-#undef B1
-#undef F
-
-
-#include <boost_function.hpp>
-// Example use of a Boost header
-#include <boost_type_traits.hpp>
+// Example of use with Boost for Arduino
+// replace / with _ in nameing of headers.
+#include <boost_type_traits_is_void.hpp>
 
 using namespace std;
 
