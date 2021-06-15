37e187b5e735c10aed4fa51842bf7fa1d72e87e3
John Fletcher <john@bunbury28.plus.com>
2021-06-15 12:04:07 +0100

    Update ArduinoBoost166Integers.ino
    Solved some problems. There are still others.

--- ArduinoBoost166Integers/ArduinoBoost166Integers.ino
+++ ArduinoBoost166Integers/ArduinoBoost166Integers.ino
@@ -3,13 +3,41 @@
 #include <ArduinoSTL.h>
 // This is an old version of boost from 2012.
 //#include <boost_1_51_0.h>
-#include <typeinfo>
+//#include <typeinfo>
 //#include <array>
+#include <functional>
 #define BOOST_NO_CWCHAR
 #define BOOST_NO_CXX11_HDR_TYPEINDEX
 #define BOOST_NO_CXX11_HDR_ARRAY
 #define BOOST_NO_CXX11_HDR_TUPLE
+#define BOOST_NO_CXX11_SMART_PTR
 #define BOOST_NO_STD_WSTRING
+#define BOOST_DISABLE_ASSERTS
+#define BOOST_NO_EXCEPTIONS
+#define BOOST_EXCEPTION_DISABLE
+
+namespace std {
+
+   class runtime_error {
+   public:
+   runtime_error( std::exception & e ) { }
+   runtime_error( std::string const & s ) { }
+  };
+}
+
+namespace boost {
+
+  void throw_exception( std::exception & e ) { }
+  void throw_exception(const std::exception & e ) { }
+  void throw_exception( std::runtime_error& e) { }
+  void throw_exception(const std::runtime_error& e) { }
+
+}
+
+#undef B1
+#undef F
+
+
 #include <boost_function.hpp>
 // Example use of a Boost header
 #include <boost_type_traits.hpp>
