552408efaec475613a29dad740ee40e759e3f39f
John Fletcher <john@bunbury28.plus.com>
2021-08-14 16:21:58 +0100

    Update CAN1602PIN.ino
    add comment

--- CAN1602PIN/CAN1602PIN.ino
+++ CAN1602PIN/CAN1602PIN.ino
@@ -140,6 +140,8 @@ const int pin_d7 = 7;
 const int pin_BL = 10; 
 LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 
+// Variables for buttons
+// Most are not needed here.
 //int x;
 //int prevx = 0;
 //int range;
