361ff10e1586934e7c82a07e59f439b67a44f486
John Fletcher <john@bunbury28.plus.com>
2021-09-15 18:54:51 +0100

    Update arduino_state_blink.ino
    First try to put it together

--- StateMachine/arduino_state_blink/arduino_state_blink.ino
+++ StateMachine/arduino_state_blink/arduino_state_blink.ino
@@ -21,20 +21,89 @@ StateMachine machine = StateMachine();
  ***********************************************************/
 
 
+// Global variables for the actual state.
 
-static enum { LED_off, LED_on } led_state;
+enum { LED_off, LED_on } Led_State;
 
-static enum { RESET, BLINK, WAIT } blink_state;
+enum { RESET, BLINK, WAIT } Blink_State;
+
+uint32_t timeLastTransition = 0;
+ 
+/*
+ * States for the blink_state machine.
+ */
+
+// Reset
+State* S0 = machine.addState([]() {
+  if(machine.executeOnce){
+    Serial.println("State 0, reset");
+    digitalWrite(LED,LOW);
+    Blink_State = RESET;
+    Led_State = LED_off;
+    timeLastTransition = millis();
+  }
+});
+
+// Blink
+State* S1 = machine.addState([]() {
+    if(machine.executeOnce){
+      Blink_State = BLINK;
+      Serial.println("State 1, blink");
+      digitalWrite(LED, !digitalRead(LED));
+    }
+});
+
+// Wait
+State* S2 = machine.addState([]() {
+    if(machine.executeOnce){
+      Blink_State = WAIT;
+      Serial.println("State 1, wait");
+      timeLastTransition = millis();
+    }
+});
+
+/*
+ * Transitions between states for the blink_state machine.
+ */
+
+bool transitionS0S1(){
+    if (millis() - timeLastTransition >= STATE_DELAY) {
+      return true;
+    }
+    return false;
+}
+
+// Go straight away to the wait state.
+bool transitionS1S2(){
+    return true;
+}
+
+bool transitionS2S1(){
+    if (millis() - timeLastTransition >= STATE_DELAY) {
+      return true;
+    }
+    return false;
+}
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT);
+
+  // Add transition from S0 to S1
+  S0->addTransition(&transitionS0S1,S1);
+  // Add transitions from S1 to S2 and back again.
+  S1->addTransition(&transitionS1S2,S2);
+  S2->addTransition(&transitionS2S1,S1);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  machine.run();
+  delay(STATE_DELAY);
 }
