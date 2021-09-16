a447b8321da14df7c32efc4afc6da9d70fbb338b
John Fletcher <john@bunbury28.plus.com>
2021-09-16 17:36:36 +0100

    Update arduino_state_event_keypad.ino
    The example is now working

--- StateMachine/arduino_state_event_keypad/arduino_state_event_keypad.ino
+++ StateMachine/arduino_state_event_keypad/arduino_state_event_keypad.ino
@@ -1,11 +1,8 @@
-// arduino_state_keypad
+// arduino_state_event_keypad
 // Modify to include keypad
+// This example now runs IoAbstraction and TaskManagerIO to run the state machine.
 
-// This example now runs the state machine example.
 // Reset can be made by pressing key 0
-
-// arduino_state_blink
-// Simple example using the StateMachine library.
 
 /*
  * Converting example from 
@@ -42,14 +39,10 @@ MatrixKeyboardManager keyboard;
 // IoExpanders or shift registers instead.
 IoAbstractionRef arduinoIo = ioUsingArduino();
 
-#include <StateMachine.h>
-
-const int STATE_DELAY = 1000;
+const int STATE_DELAY = 5000;
 const int LED = 13;
 
 char key_pressed = 'Z';
-
-StateMachine machine = StateMachine();
 
 /***********************************************************
  * What do we mean by state?
@@ -66,71 +59,54 @@ enum { LED_off, LED_on } Led_State;
 enum { RESET, BLINK, WAIT } Blink_State;
 
 uint32_t timeLastTransition = 0;
- 
-/*
- * States for the blink_state machine.
- */
 
-// Reset
-State* S0 = machine.addState([]() {
-  if(machine.executeOnce){
-    Serial.println("State 0, reset");
-    digitalWrite(LED,LOW);
-    Blink_State = RESET;
-    Led_State = LED_off;
-    timeLastTransition = millis();
-  }
-});
-
-// Blink
-State* S1 = machine.addState([]() {
-    if(machine.executeOnce){
+class Blinker : public BaseEvent {
+  private: 
+    byte blinker_pin;
+    static const uint32_t NEXT_CHECK_INTERVAL = 60UL * 1000UL; // 60 seconds away, maximum is about 1 hour.
+  public:
+    int taskId;
+    Blinker(int pin) : blinker_pin(pin) 
+    { 
+        taskId = TASKMGR_INVALIDID;
+    }
+    uint32_t timeOfNextCheck() override {  
+      //Serial.println("Event Next Check");
+       Blink_State = WAIT;
+       if (key_pressed == '0') { // This is S2S0
+          Serial.println("Reset requested by key press");
+          setStart();
+       }  
+       // This is the same as S0S1 and S2S1
+       auto elapsed_millis = millis() - timeLastTransition;
+       //Serial.println(elapsed_millis);
+       if (elapsed_millis >= STATE_DELAY) {
+          //Serial.println(elapsed_millis);
+          //setTriggered(true);
+          markTriggeredAndNotify();
+       }
+       return 100UL * 1000UL; // every 15 milliseconds we increment
+    }
+    void setStart() { // This is also for reset - was state S0
+      Serial.println("State 0, reset");
+      digitalWrite(blinker_pin,LOW);
+      Blink_State = RESET;
+      Led_State = LED_off;
+      timeLastTransition = millis();
+      Serial.println("State 2, wait");
+    }     
+    void exec() override {
+      // State 1
       Blink_State = BLINK;
       Serial.println("State 1, blink");
-      digitalWrite(LED, !digitalRead(LED));
-    }
-});
-
-// Wait
-State* S2 = machine.addState([]() {
-    if(machine.executeOnce){
-      Blink_State = WAIT;
+      digitalWrite(blinker_pin, !digitalRead(blinker_pin));
+      timeLastTransition = millis();      
       Serial.println("State 2, wait");
-      timeLastTransition = millis();
-    }
-});
+    }  
+    ~Blinker() override = default;
+};
 
-/*
- * Transitions between states for the blink_state machine.
- */
-
-bool transitionS0S1(){
-    if (millis() - timeLastTransition >= STATE_DELAY) {
-      return true;
-    }
-    return false;
-}
-
-// Go straight away to the wait state.
-bool transitionS1S2(){
-    return true;
-}
-
-bool transitionS2S1(){
-    if (millis() - timeLastTransition >= STATE_DELAY) {
-      return true;
-    }
-    return false;
-}
-
-// Go to the reset state
-bool transitionS2S0(){
-   if (key_pressed == '0') {
-     Serial.println("Reset requested by key press");
-     return true;
-   }
-   return false;
-}
+Blinker blinker(LED);
 
 //
 // We need a class that extends from KeyboardListener. this gets notified when
@@ -160,15 +136,7 @@ void setup() {
    while(!Serial) { }
    Serial.begin(115200);
    pinMode(LED,OUTPUT);
-   Serial.println("Arduino state blink");
-
-  
-   // Add transition from S0 to S1
-   S0->addTransition(&transitionS0S1,S1);
-   // Add transitions from S1 to S2 and back again.
-   S1->addTransition(&transitionS1S2,S2);
-   S2->addTransition(&transitionS2S1,S1);
-   S2->addTransition(&transitionS2S0,S0);
+   Serial.println("Arduino state event blink");
 
     // Converted to copy the arrays.
     for (byte i = 0; i < ROWS; i++)
@@ -184,13 +152,12 @@ void setup() {
     keyboard.setRepeatKeyMillis(850, 350);
 
     Serial.println("Keyboard is initialised!");
-
+    blinker.setStart();
+    taskManager.registerEvent(&blinker);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
     // as this indirectly uses taskmanager, we must include this in loop.
     taskManager.runLoop();
-    machine.run();
-  //delay(STATE_DELAY);
 }
