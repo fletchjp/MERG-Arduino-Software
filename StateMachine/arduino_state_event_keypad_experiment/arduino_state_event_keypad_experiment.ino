99bf79fec6b1157be69f5b5c718618455ffe28b7
John Fletcher <john@bunbury28.plus.com>
2021-09-18 17:29:17 +0100

    Move Blinker code to blinker.h

--- StateMachine/arduino_state_event_keypad_experiment/arduino_state_event_keypad_experiment.ino
+++ StateMachine/arduino_state_event_keypad_experiment/arduino_state_event_keypad_experiment.ino
@@ -44,52 +44,8 @@ IoAbstractionRef arduinoIo = ioUsingArduino();
  * I think that is up to the user.
  ***********************************************************/
 
-class Blinker : public BaseEvent {
-  private: 
-    byte blinker_pin;
-    char reset_key;
-    static const uint32_t NEXT_CHECK_INTERVAL = 60UL * 1000UL; // 60 seconds away, maximum is about 1 hour.
-  public:
-    int taskId;
-    Blinker(int pin, char key) : blinker_pin(pin), reset_key(key) 
-    { 
-        taskId = TASKMGR_INVALIDID;
-    }
-    uint32_t timeOfNextCheck() override {  
-      //Serial.println("Event Next Check");
-       Blink_State = WAIT;
-       if (key_pressed == reset_key) { // This is S2S0
-          Serial.println("Reset requested by key press");
-          setStart();
-       }  
-       // This is the same as S0S1 and S2S1
-       auto elapsed_millis = millis() - timeLastTransition;
-       //Serial.println(elapsed_millis);
-       if (elapsed_millis >= STATE_DELAY) {
-          //Serial.println(elapsed_millis);
-          //setTriggered(true);
-          markTriggeredAndNotify();
-       }
-       return 100UL * 1000UL; // every 15 milliseconds we increment
-    }
-    void setStart() { // This is also for reset - was state S0
-      Serial.println("State 0, reset");
-      digitalWrite(blinker_pin,LOW);
-      Blink_State = RESET;
-      Led_State = LED_off;
-      timeLastTransition = millis();
-      Serial.println("State 2, wait");
-    }     
-    void exec() override {
-      // State 1
-      Blink_State = BLINK;
-      Serial.println("State 1, blink");
-      digitalWrite(blinker_pin, !digitalRead(blinker_pin));
-      timeLastTransition = millis();      
-      Serial.println("State 2, wait");
-    }  
-    ~Blinker() override = default;
-};
+// Blinker now in header file
+#include "blinker.h"
 
 Blinker blinker(LED,'0'); // LED and reset key.
 
