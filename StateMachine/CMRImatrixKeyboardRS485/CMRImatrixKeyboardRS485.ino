2d08a20980ec44942fe546c510b2b17f8534b5ca
John Fletcher <john@bunbury28.plus.com>
2021-09-30 22:02:44 +0100

    Create CMRImatrixKeyboardRS485.ino
    Copy to be modified to work with RS485

---
+++ StateMachine/CMRImatrixKeyboardRS485/CMRImatrixKeyboardRS485.ino
@@ -0,0 +1,110 @@
+/**
+ * This example shows how to use the matrix keyboard support that's built into IoAbstraction,
+ * it can be used out the box with either a 3x4 or 4x4 keypad, but you can modify it to use
+ * any matrix keyboard quite easily.
+ * It just sends the characters that are typed on the keyboard to Serial. The keyboard in This
+ * example is connected directly to Arduino pins, but could just as easily be connected over
+ * a PCF8574, MCP23017 or other IoAbstraction.
+ */
+
+// This version sets up the keyboard adapting the custom_keyboard example.
+
+#include <CMRI.h>
+#include <Wire.h>
+#include <IoAbstraction.h>
+#include <TaskManagerIO.h>
+#include <KeyboardManager.h>
+
+CMRI cmri; // defaults to a SMINI with address 0. SMINI = 24 inputs, 48 outputs
+
+//
+// We need to make a keyboard layout that the manager can use. choose one of the below.
+// The parameter in brackets is the variable name.
+//
+//MAKE_KEYBOARD_LAYOUT_3X4(keyLayout)
+//MAKE_KEYBOARD_LAYOUT_4X4(keyLayout)
+//
+// I want to use the alternative based on the custom_keyboard example.
+//const char pgmLayout[] PROGMEM = "charsColByRow";
+//KeyboardLayout layout(rows, cols, const char* pgmLayout)
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the symbols on the buttons of the keypads
+// PROGMEM is important.
+const char layout[] PROGMEM = "123A456B789C*0#D"; // Chars have to be in a string.
+//  '1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'
+//};
+// These are in order of Keypad pins from 1 to 8.
+// Pin 1 is on the left with the pad face up.
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+// This seems fussy. ROWS and COLS will not work here.
+uint8_t rows = ROWS;
+uint8_t cols = COLS;
+KeyboardLayout keyLayout(rows, cols, layout);
+
+//
+// We need a keyboard manager class too
+//
+MatrixKeyboardManager keyboard;
+
+// this examples connects the pins directly to an arduino but you could use
+// IoExpanders or shift registers instead.
+IoAbstractionRef arduinoIo = ioUsingArduino();
+
+//
+// We need a class that extends from KeyboardListener. this gets notified when
+// there are changes in the keyboard state.
+//
+class MyKeyboardListener : public KeyboardListener {
+public:
+    void keyPressed(char key, bool held) override {
+        //Serial.print("Key ");
+        //Serial.print(key);
+        //Serial.print(" is pressed, held = ");
+        //Serial.println(held);
+        if (key == '1') cmri.set_bit(0, LOW);
+        if (key == '2') cmri.set_bit(1, LOW);
+    }
+
+    void keyReleased(char key) override {
+        //Serial.print("Released ");
+        //Serial.println(key);
+        if (key == '1') cmri.set_bit(0, HIGH);
+        if (key == '2') cmri.set_bit(1, HIGH);
+    }
+} myListener;
+
+void setup() {
+    //while(!Serial);
+    //Serial.begin(115200);
+    Serial.begin(9600, SERIAL_8N2); // make sure this matches your speed set in JMRI
+    pinMode(13, OUTPUT);
+  
+    // Converted to copy the arrays.
+    for (byte i = 0; i < ROWS; i++)
+      keyLayout.setRowPin(i, rowPins[i]);
+    for (byte i = 0; i < COLS; i++)
+      keyLayout.setColPin(i, colPins[i]);
+
+    // create the keyboard mapped to arduino pins and with the layout chosen above.
+    // it will callback our listener
+    keyboard.initialise(arduinoIo, &keyLayout, &myListener);
+
+    // start repeating at 850 millis then repeat every 350ms
+    keyboard.setRepeatKeyMillis(850, 350);
+
+    //Serial.println("Keyboard is initialised!");
+}
+
+void loop() {
+    // as this indirectly uses taskmanager, we must include this in loop.
+    taskManager.runLoop();
+
+    // 1: main processing node of cmri library
+    cmri.process();
+
+    // 2: update output. Reads bit 0 of T packet and sets the LED to this
+    digitalWrite(13, cmri.get_bit(0));
+}
