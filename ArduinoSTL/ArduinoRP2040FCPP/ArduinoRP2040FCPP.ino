a39207eb1f567ab06b3567b18a9345da678cc173
John Fletcher <john@bunbury28.plus.com>
2021-10-24 22:00:50 +0100

    Create ArduinoRP2040FCPP.ino
    Copy of DUEFCPP

---
+++ ArduinoSTL/ArduinoRP2040FCPP/ArduinoRP2040FCPP.ino
@@ -0,0 +1,64 @@
+// DUEFCPP
+// Demo of FC++ Maybe operation
+// This will work on ARDUINO DUE but not on AVR boards.
+
+#include "fcpp_prelude.h"
+
+#include <Streaming.h>
+
+using namespace fcpp;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (115200);
+  Serial << endl << endl << F("> DUEFCPP ** ") << __FILE__ << endl;
+  Serial << "Some simple FC++ operations" << endl;
+  Serial << "plus(1,2) = " << plus(1,2) << endl;
+  Serial << "plus(1.5,2.3) = " << plus(1.5,2.3) << endl;
+  Serial << "plus(1)(2) = " << plus(1)(2) << endl;
+  Serial << "inc(1) = " << inc(1) << endl;
+  Serial << "--------------------------" << endl;
+
+  Maybe<int> mx(2), my(3);
+  Maybe<int> mz;
+  if (mz.is_nothing()) Serial << "mz is nothing" << endl;
+  mz = just(plus(mx.value(),my.value()));
+  if (mz.is_nothing()) { 
+     Serial << "mz is nothing" << endl;
+  } else {
+     Serial << "mz has " << mz.value() << endl;
+  }
+  Serial << "--------------------------" << endl;
+
+  List<double>::iterator idx1, idx2;
+  List<double> x1 = list_with(0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0);
+  List<double> x2 = fcpp::map(fcpp::minus(1.0),x1);
+  double sumx1 = foldl(plus,0.,x1);
+  for (idx1 = x1.begin(), idx2 = x2.begin(); idx1 != x1.end(); ++idx1, ++idx2) {
+     Serial << *idx1 << " " << *idx2 << endl;
+  }
+  Serial << "Sum of x1 = " << sumx1 << endl;
+  Serial << "--------------------------" << endl;
+
+  List<int> ix1 = enumFromTo(0,10);
+  List<bool> bodds = fcpp::map(odd,ix1);
+  List<bool> bevens = fcpp::map(even,ix1);
+  List<int> odds = filter(odd,ix1);
+  int sum_odds = foldl(plus,0,odds);
+  int sum_odds2 = foldl(plus,0,filter(odd,ix1));
+  List<int>::iterator iix1;
+  List<bool>::iterator ib1, ib2;
+  Serial << "Length of bodds is " << length(bodds) << endl;
+  for (iix1 = ix1.begin(), ib1 = bodds.begin(), ib2 = bevens.begin(); iix1 != ix1.end(); ++iix1, ++ib1, ++ib2) {
+     Serial << *iix1 << " " << *ib1 << " " << *ib2 << endl;
+  }
+
+  Serial << "Length of odds is " << length(odds) << endl;
+  Serial << "sum of the odds is " << sum_odds << endl;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
