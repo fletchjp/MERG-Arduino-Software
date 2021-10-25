9127eac9dd737776ae6a8be98cd3040190fb2f81
John Fletcher <john@bunbury28.plus.com>
2021-10-25 16:49:22 +0100

    Update IoAbsTimedBlinkSerial.ino
    Add Serial output to the example

--- ArduinoSTL/IoAbsTimedBlinkSerial/IoAbsTimedBlinkSerial.ino
+++ ArduinoSTL/IoAbsTimedBlinkSerial/IoAbsTimedBlinkSerial.ino
@@ -26,12 +26,41 @@ int ledOn = LOW;
 // create an IO abstraction, so later we could put the led on a shift register or i2c.
 IoAbstractionRef ioDevice = ioUsingArduino();
 
+/////////////////////////////////////////////////////////
+
+// This comes from the cdc_multi example
+// Helper: non-blocking "delay" alternative.
+boolean delay_without_delaying(unsigned long time) {
+  // return false if we're still "delaying", true if time ms has passed.
+  // this should look a lot like "blink without delay"
+  static unsigned long previousmillis = 0;
+  unsigned long currentmillis = millis();
+  if (currentmillis - previousmillis >= time) {
+    previousmillis = currentmillis;
+    return true;
+  }
+  return false;
+}
+
 void setup() {
+  Serial.begin(115200);
+  unsigned long t1 = millis();
+  unsigned long t2;
+  while (!Serial && ((millis() - t1) <= 10000));
+  t2 = millis() - t1;
+  Serial.print("Waited for ");
+  Serial.print(t2);
+  Serial.println(" millis");
+  while (!delay_without_delaying(5000) ) { };
+  Serial.println("Arduino RP2040 IoAbstraction Blink Test");
+
 	// set the pin we are to use as output using the io abstraction
 	ioDevicePinMode(ioDevice, ledPin, OUTPUT);
 
 	// and create the task that toggles the led every second.
 	taskManager.scheduleFixedRate(1000, toggle);
+  Serial.println("--------");
+  while (!delay_without_delaying(5000) ) { };
 }
 
 // this is the call back method that gets called once a second
@@ -41,6 +70,11 @@ void toggle() {
 	ioDeviceDigitalWriteS(ioDevice, ledPin, ledOn);
 
 	ledOn = !ledOn; // toggle the LED state.
+  if (ledOn) {
+      Serial.println("Arduino blink ON");
+  } else {
+      Serial.println("Arduino blink OFF");
+  }
 }
 
 
