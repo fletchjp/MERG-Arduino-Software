d69243d3e28676b67c69a42f777fbc0aed843091
John Fletcher <john@bunbury28.plus.com>
2021-10-27 14:14:05 +0100

    Update ArduinoRP2040Eraserface.ino
    Add use of the interface

--- ArduinoSTL/ArduinoRP2040Eraserface/ArduinoRP2040Eraserface.ino
+++ ArduinoSTL/ArduinoRP2040Eraserface/ArduinoRP2040Eraserface.ino
@@ -67,7 +67,35 @@ void print_data(my_interface obj) {
 
 
 void setup() {
-  
+  Serial.begin(115200);
+  unsigned long t1 = millis();
+  unsigned long t2;
+  while (!Serial && ((millis() - t1) <= 10000));
+  t2 = millis() - t1;
+  Serial.print("Waited for ");
+  Serial.print(t2);
+  Serial.println(" millis");
+  Serial << "Eraserface basic example" << endl;
+    a_class x;
+    another_class y{ 43 };
+
+    my_interface i = x;
+
+    i.a_func(12);
+    i.a_func(77L);
+    i.another_func();
+
+    print_data(i);
+    i.some_data() = "x's data has changed.";
+    print_data(x);
+
+    // reusing the same interface object
+    i = y;
+    i.a_func(13);
+    i.a_func(21L);
+    i.another_func();
+    print_data(y);
+
 }
 
 void loop() {
