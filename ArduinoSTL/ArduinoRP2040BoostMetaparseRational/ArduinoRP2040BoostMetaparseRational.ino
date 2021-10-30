b2065c69ee07c8c37feee710138421205ed7ac3c
John Fletcher <john@bunbury28.plus.com>
2021-10-31 00:14:00 +0100

    Update ArduinoRP2040BoostMetaparseRational.ino
    Start building example. There are problems.

--- ArduinoSTL/ArduinoRP2040BoostMetaparseRational/ArduinoRP2040BoostMetaparseRational.ino
+++ ArduinoSTL/ArduinoRP2040BoostMetaparseRational/ArduinoRP2040BoostMetaparseRational.ino
@@ -1,5 +1,13 @@
 // ArduinoRP2040BoostMetaparseRational
 
+#undef F
+#include <cstdio>
+#include <boost_metaparse.hpp>
+
+// This will need to be implemented somehow.
+template <class Num, class Denom>
+struct rational;
+
 void setup() {
   // put your setup code here, to run once:
 
