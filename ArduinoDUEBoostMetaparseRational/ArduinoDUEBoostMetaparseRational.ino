2db6ad9f87fdedfea16d96a9135772acd2961018
John Fletcher <john@bunbury28.plus.com>
2021-10-31 15:43:56 +0000

    Create ArduinoDUEBoostMetaparseRational.ino
    New example for boost metaparse

---
+++ ArduinoDUEBoostMetaparseRational/ArduinoDUEBoostMetaparseRational.ino
@@ -0,0 +1,19 @@
+// ArduinoDUEBoostMetaparseRational
+
+#undef F
+#include <cstdio>
+#include <boost_metaparse.hpp>
+
+// This will need to be implemented somehow.
+template <class Num, class Denom>
+struct rational;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
