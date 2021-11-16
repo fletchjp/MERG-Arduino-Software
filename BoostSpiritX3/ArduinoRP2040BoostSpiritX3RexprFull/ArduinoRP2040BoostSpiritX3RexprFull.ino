ca2bdb91c0a3c97493ec7287173399fdb72a5133
John Fletcher <john@bunbury28.plus.com>
2021-11-16 17:15:44 +0000

    Update ArduinoRP2040BoostSpiritX3RexprFull.ino
    Remove old case

--- BoostSpiritX3/ArduinoRP2040BoostSpiritX3RexprFull/ArduinoRP2040BoostSpiritX3RexprFull.ino
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3RexprFull/ArduinoRP2040BoostSpiritX3RexprFull.ino
@@ -1,8 +1,8 @@
-/// @file ArduinoRP2040BoostSpiritX3Rexpr.ino
-/// @brief Example of Boost Spirit X3 Rexpr minimal example
+/// @file ArduinoRP2040BoostSpiritX3RexprFull.ino
+/// @brief Example of Boost Spirit X3 Rexpr full example
 ///
 /// Boost Spirit 3.0.0 is included from Arduino Boost (Boost 1.66.0)
-/// This is taken from spirit/example/x3/rexpr_min/rexpr
+/// This is taken from spirit/example/x3/rexpr/rexpr_full
 /// and adapted to run on the Arduino NANO RP2040 connect.
 ///
 /// This has involved a number of adaptions to the Arduino environment.
@@ -52,123 +52,11 @@
 #include <boost/fusion/include/std_pair.hpp>
 #include <boost/fusion/include/io.hpp>
 
-///////////////////////////////////////////////////////////////////////////////
-//  Our AST
-///////////////////////////////////////////////////////////////////////////////
-namespace client { namespace ast
-{
-    namespace fusion = boost::fusion;
-    namespace x3 = boost::spirit::x3;
+#include "rexpr_def.hpp"
+#include "config.hpp"
 
-    struct rexpr;
 
-    struct rexpr_value : x3::variant<
-            std::string
-          , x3::forward_ast<rexpr>
-        >
-    {
-        using base_type::base_type;
-        using base_type::operator=;
-    };
 
-    typedef std::map<std::string, rexpr_value> rexpr_map;
-    typedef std::pair<std::string, rexpr_value> rexpr_key_value;
-
-    struct rexpr
-    {
-        rexpr_map entries;
-    };
-}}
-
-// We need to tell fusion about our rexpr struct
-// to make it a first-class fusion citizen
-BOOST_FUSION_ADAPT_STRUCT(client::ast::rexpr,
-    entries
-)
-
-///////////////////////////////////////////////////////////////////////////////
-//  AST processing
-///////////////////////////////////////////////////////////////////////////////
-namespace client { namespace ast
-{
-    ///////////////////////////////////////////////////////////////////////////
-    //  Print out the rexpr tree
-    ///////////////////////////////////////////////////////////////////////////
-    int const tabsize = 4;
-
-    struct rexpr_printer
-    {
-        typedef void result_type;
-
-        rexpr_printer(int indent = 0)
-          : indent(indent) {}
-
-        void operator()(rexpr const& ast) const
-        {
-            Serial << '{' << endl;
-            for (auto const& entry : ast.entries)
-            {
-                tab(indent+tabsize);
-                Serial << '"' << entry.first << "\" = ";
-                boost::apply_visitor(rexpr_printer(indent+tabsize), entry.second);
-            }
-            tab(indent);
-            //Serial << "    " << endl;
-            Serial << '}' << endl;
-        }
-
-        void operator()(std::string const& text) const
-        {
-            Serial << '"' << text << '"' << endl;
-        }
-
-        void tab(int spaces) const
-        {
-            for (int i = 0; i < spaces; ++i)
-                Serial << ' ';
-        }
-
-        int indent;
-    };
-}}
-
-///////////////////////////////////////////////////////////////////////////////
-//  Our rexpr grammar
-///////////////////////////////////////////////////////////////////////////////
-namespace client { namespace parser
-{
-    namespace x3 = boost::spirit::x3;
-    namespace ascii = boost::spirit::x3::ascii;
-
-    using x3::lit;
-    using x3::lexeme;
-
-    using ascii::char_;
-    using ascii::string;
-
-    x3::rule<class rexpr_value, ast::rexpr_value>
-        rexpr_value = "rexpr_value";
-
-    x3::rule<class rexpr, ast::rexpr>
-        rexpr = "rexpr";
-
-    x3::rule<class rexpr_key_value, ast::rexpr_key_value>
-        rexpr_key_value = "rexpr_key_value";
-
-    auto const quoted_string =
-        lexeme['"' >> *(char_ - '"') >> '"'];
-
-    auto const rexpr_value_def =
-        quoted_string | rexpr;
-
-    auto const rexpr_key_value_def =
-        quoted_string >> '=' >> rexpr_value;
-
-    auto const rexpr_def =
-        '{' >> *rexpr_key_value >> '}';
-
-    BOOST_SPIRIT_DEFINE(rexpr_value, rexpr, rexpr_key_value);
-}}
 
 /// Sample input:
 ///
@@ -211,12 +99,12 @@ void setup() {
   Serial.print(t2);
   Serial.println(" millis");
   while (!delay_without_delaying(10000) ) { };
-  Serial << "ArduinoRP2040BoostSpiritX3Rexpr ** " << endl << __FILE__ << endl;
+  Serial << "ArduinoRP2040BoostSpiritX3RexprFull ** " << endl << __FILE__ << endl;
   Serial << "Boost Spirit X3 parsing" << endl;
 
     // I need to sort out the input here.
     std::string storage = input; // We will read the contents here.
-
+/*
     using client::parser::rexpr; // Our grammar
     client::ast::rexpr ast; // Our tree
 
@@ -242,7 +130,7 @@ void setup() {
         Serial << "stopped at: \": " << context << "...\"\n";
         Serial << "-------------------------\n";
     }
-
+*/
 
   Serial << "------------------------------" << endl;
   while (!delay_without_delaying(10000) ) { };
