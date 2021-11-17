27d88f71e578c5409159e5b5c5fbe936fa3ac024
John Fletcher <john@bunbury28.plus.com>
2021-11-17 17:25:23 +0000

    Create ArduinoRP2040BoostSpiritX3Variant.ino

---
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3Variant/ArduinoRP2040BoostSpiritX3Variant.ino
@@ -0,0 +1,123 @@
+/// @file ArduinoRP2040BoostSpiritX3Variant.ino
+/// @brief Example of Boost Spirit X3 Variant
+///
+/// Boost Spirit 3.0.0 is included from Arduino Boost (Boost 1.66.0)
+/// This is taken from spirit/test/x3/x3_variant
+/// and adapted to run on the Arduino NANO RP2040 connect.
+///
+/// This has involved a number of adaptions to the Arduino environment.
+///
+/// X3 Variant is based on Boost Variant
+
+
+// 3rd party libraries
+#include <Streaming.h>
+
+#undef F
+#undef min
+
+#include <exception>
+#include <stdexcept>
+#include <iostream> 
+#include <string>
+#include <vector>
+#include <cstdio>
+
+#include "ArduinoCode.h"
+
+#include <boost_spirit_home_x3.hpp>
+#include <boost/spirit/home/x3/support/ast/variant.hpp>
+
+//////////////////////////////////////////////////////////
+/// x3 variant example
+//////////////////////////////////////////////////////////
+
+namespace x3 = boost::spirit::x3;
+
+struct none {};
+
+using variant = x3::variant<
+        none
+      , bool
+      , std::string
+      , int
+      , double
+    >;
+
+struct ast : variant
+{
+    using variant::variant;
+    using variant::operator=;
+
+    ast(char const* s)
+      : variant(std::string{s})
+    {}
+
+    ast& operator=(char const* s)
+    {
+        variant::operator=(std::string{s});
+        return *this;
+    }
+};
+
+
+//////////////////////////////////////////////////////////
+
+// This comes from the cdc_multi example
+/// Helper: non-blocking "delay" alternative.
+boolean delay_without_delaying(unsigned long time) {
+  /// return false if we're still "delaying", true if time ms has passed.
+  /// this should look a lot like "blink without delay"
+  static unsigned long previousmillis = 0;
+  unsigned long currentmillis = millis();
+  if (currentmillis - previousmillis >= time) {
+    previousmillis = currentmillis;
+    return true;
+  }
+  return false;
+}
+void setup() {
+  /// put your setup code here, to run once:
+  Serial.begin (115200);
+  unsigned long t1 = millis();
+  unsigned long t2;
+  while (!Serial && ((millis() - t1) <= 10000));
+  t2 = millis() - t1;
+  Serial.print("Waited for ");
+  Serial.print(t2);
+  Serial.println(" millis");
+  while (!delay_without_delaying(10000) ) { };
+  Serial << "ArduinoRP2040BoostSpiritX3Variant ** " << endl << __FILE__ << endl;
+  Serial << "Some simple Boost Spirit X3 variant operations" << endl;
+ 
+  {
+ 
+     ast v{123};
+     Serial << "ast v{123};" << endl;
+     Serial << boost::get<int>(v) << endl;               
+
+  }
+  Serial << "------------------------------" << endl;
+  while (!delay_without_delaying(10000) ) { };
+  Serial << "------------------------------" << endl;
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+//////////////////////////////////////////////////////////
+
+int LEDstate = 0;
+
+void loop() {
+  /// put your main code here, to run repeatedly:
+  if (delay_without_delaying(1000)) {
+    LEDstate = !LEDstate;
+    digitalWrite(LED_BUILTIN, LEDstate);
+    //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    if (LEDstate) {
+      Serial.println("Arduino blink ON");
+    } else {
+      Serial.println("Arduino blink OFF");
+    }
+  }
+
+}
