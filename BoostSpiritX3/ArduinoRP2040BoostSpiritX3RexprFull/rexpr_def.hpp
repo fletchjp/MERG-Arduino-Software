e7a1be5dfbc1ce3d2eb5ec3f9317bed69174d54d
John Fletcher <john@bunbury28.plus.com>
2021-11-17 13:11:09 +0000

    Update rexpr_def.hpp
    Removed rexpr_inner to match minimal example

--- BoostSpiritX3/ArduinoRP2040BoostSpiritX3RexprFull/rexpr_def.hpp
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3RexprFull/rexpr_def.hpp
@@ -39,7 +39,8 @@ namespace rexpr { namespace parser
 
     struct rexpr_value_class;
     struct rexpr_key_value_class;
-    struct rexpr_inner_class;
+    //struct rexpr_inner_class;
+    struct rexpr_class;
 
     ///////////////////////////////////////////////////////////////////////////
     // Rules
@@ -51,10 +52,13 @@ namespace rexpr { namespace parser
     x3::rule<rexpr_key_value_class, ast::rexpr_key_value> const
         rexpr_key_value = "rexpr_key_value";
 
-    x3::rule<rexpr_inner_class, ast::rexpr> const
+/*    x3::rule<rexpr_inner_class, ast::rexpr> const
         rexpr_inner = "rexpr";
 
     rexpr_type const rexpr = "rexpr";
+*/
+    x3::rule<rexpr_class, ast::rexpr> const
+        rexpr = "rexpr";
 
     ///////////////////////////////////////////////////////////////////////////
     // Grammar
@@ -64,17 +68,21 @@ namespace rexpr { namespace parser
         lexeme['"' >> *(char_ - '"') >> '"'];
 
     auto const rexpr_value_def =
-        quoted_string | rexpr_inner;
+        quoted_string | rexpr;
 
     auto const rexpr_key_value_def =
         quoted_string >> '=' >> rexpr_value;
 
-    auto const rexpr_inner_def =
+    /*auto const rexpr_inner_def =
+        '{' >> *rexpr_key_value >> '}';
+    */
+    auto const rexpr_def =
         '{' >> *rexpr_key_value >> '}';
 
-    auto const rexpr_def = rexpr_inner_def;
+    //auto const rexpr_def = rexpr_inner_def;
 
-    BOOST_SPIRIT_DEFINE(rexpr_value, rexpr, rexpr_inner, rexpr_key_value);
+    BOOST_SPIRIT_DEFINE(rexpr_value, rexpr, rexpr_key_value);
+    //BOOST_SPIRIT_DEFINE(rexpr_value, rexpr, rexpr_inner, rexpr_key_value);
 
     ///////////////////////////////////////////////////////////////////////////
     // Annotation and Error handling
@@ -102,7 +110,7 @@ namespace rexpr { namespace parser
     // We want these to be annotated with the iterator position.
     struct rexpr_value_class /* : x3::annotate_position /*_on_success*/ {};
     struct rexpr_key_value_class /*: x3::annotate_position /*_on_success*/ {};
-    struct rexpr_inner_class /*: x3::annotate_position /*_on_success*/ {};
+    //struct rexpr_inner_class /*: x3::annotate_position /*_on_success*/ {};
 
     // We want error-handling only for the start (outermost) rexpr
     // rexpr is the same as rexpr_inner but without error-handling (see error_handler.hpp)
