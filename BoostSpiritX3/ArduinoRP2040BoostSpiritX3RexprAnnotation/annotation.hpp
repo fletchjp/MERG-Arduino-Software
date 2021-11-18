ffd894a3406b05f80ea6e0beb22f345ee5d36ed9
John Fletcher <john@bunbury28.plus.com>
2021-11-18 00:11:14 +0000

    Create annotation.hpp
    Copied and adapted example from Annotation example.

---
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3RexprAnnotation/annotation.hpp
@@ -0,0 +1,42 @@
+/// @file annotation.hpp
+/// @brief Rexpr AST annotation
+///
+/// This defines the annotation for the parsed data.
+
+/*=============================================================================
+    Copyright (c) 2001-2015 Joel de Guzman
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#if !defined(BOOST_SPIRIT_X3_REPR_ANNOTATION_HPP)
+#define BOOST_SPIRIT_X3_REPR_ANNOTATION_HPP
+
+namespace rexpr
+{
+    namespace parser
+    {
+        namespace x3 = boost::spirit::x3;
+        namespace ascii = boost::spirit::x3::ascii;
+
+        ///////////////////////////////////////////////////////////////////////
+        //  Our annotation handler
+        ///////////////////////////////////////////////////////////////////////
+
+        // tag used to get the position cache from the context
+        struct position_cache_tag;
+
+        struct annotate_position
+        {
+            template <typename T, typename Iterator, typename Context>
+            inline void on_success(Iterator const& first, Iterator const& last
+            , T& ast, Context const& context)
+            {
+                auto& position_cache = x3::get<position_cache_tag>(context).get();
+                position_cache.annotate(ast, first, last);
+            }
+        };
+
+    }
+}
+#endif
