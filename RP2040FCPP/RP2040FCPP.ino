386dc0abb66c1e52f5cc51a2c7d45317415fb6f2
John Fletcher <john@bunbury28.plus.com>
2021-11-28 23:16:49 +0000

    Update RP2040FCPP.ino
    Changed to use Serial for output

--- RP2040FCPP/RP2040FCPP.ino
+++ RP2040FCPP/RP2040FCPP.ino
@@ -2,7 +2,14 @@
 // Demo of FC++ Maybe operation
 // This is modified from the version which will work on ARDUINO DUE and also on AVR boards.
 
+// 3rd party libraries
+#include <Streaming.h>
+
+#undef F
+#undef min
+
 #include <iostream>
+
 
 /// Solution to the sync_synchronize problem
 /// https://www.vexforum.com/t/i-am-learning-arm-none-eabi-compiler-recently-i-want-to-ask-some-questions/73973
@@ -12,42 +19,63 @@ extern "C" void __sync_synchronize() {}
 #include "fcpp_prelude.h"
 
 
-using namespace std;
+//using namespace std;
 
 using namespace fcpp;
 
+// This comes from the cdc_multi example
+/// Helper: non-blocking "delay" alternative.
+boolean delay_without_delaying(unsigned long time) {
+  /// return false if we're still "delaying", true if time ms has passed.
+  /// this should look a lot like "blink without delay"
+  static unsigned long previousmillis = 0;
+  unsigned long currentmillis = millis();
+  if (currentmillis - previousmillis >= time) {
+    previousmillis = currentmillis;
+    return true;
+  }
+  return false;
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (115200);
-  cout << "> RP2040FCPP ** " << __FILE__ << endl;
-  cout << "Some simple FC++ operations" << endl;
-  cout << "plus(1,2) = " << fcpp::plus(1,2) << endl;
-  cout << "plus(1.5,2.3) = " << fcpp::plus(1.5,2.3) << endl;
-  cout << "plus(1)(2) = " << fcpp::plus(1)(2) << endl;
-  cout << "inc(1) = " << inc(1) << endl;
-  cout << "--------------------------" << endl;
+  unsigned long t1 = millis();
+  unsigned long t2;
+  while (!Serial && ((millis() - t1) <= 10000));
+  t2 = millis() - t1;
+  Serial.print("Waited for ");
+  Serial.print(t2);
+  Serial.println(" millis");
+  while (!delay_without_delaying(10000) ) { };
+  Serial << "> RP2040FCPP ** " << __FILE__ << endl;
+  Serial << "Some simple FC++ operations" << endl;
+  Serial << "plus(1,2) = " << fcpp::plus(1,2) << endl;
+  Serial << "plus(1.5,2.3) = " << fcpp::plus(1.5,2.3) << endl;
+  Serial << "plus(1)(2) = " << fcpp::plus(1)(2) << endl;
+  Serial << "inc(1) = " << inc(1) << endl;
+  Serial << "--------------------------" << endl;
 
   Maybe<int> mx(2), my(3);
   Maybe<int> mz;
-  if (mz.is_nothing()) cout << "mz is nothing" << endl;
+  if (mz.is_nothing()) Serial << "mz is nothing" << endl;
   mz = just(fcpp::plus(mx.value(),my.value()));
   if (mz.is_nothing()) { 
-     cout << "mz is nothing" << endl;
+     Serial << "mz is nothing" << endl;
   } else {
-     cout << "mz has " << mz.value() << endl;
+     Serial << "mz has " << mz.value() << endl;
   }
-  cout << "--------------------------" << endl;
+  Serial << "--------------------------" << endl;
 
   List<double>::iterator idx1, idx2;
   List<double> x1 = list_with(0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0);
   List<double> x2 = fcpp::map(fcpp::minus(1.0),x1);
   double sumx1 = foldl(fcpp::plus,0.,x1);
   for (idx1 = x1.begin(), idx2 = x2.begin(); idx1 != x1.end(); ++idx1, ++idx2) {
-     cout << *idx1 << " " << *idx2 << endl;
+     Serial << *idx1 << " " << *idx2 << endl;
   }
-  cout << "Sum of x1 = " << sumx1 << endl;
-  cout << "--------------------------" << endl;
+  Serial << "Sum of x1 = " << sumx1 << endl;
+  Serial << "--------------------------" << endl;
 
   List<int> ix1 = enumFromTo(0,10);
   List<bool> bodds = fcpp::map(odd,ix1);
@@ -57,13 +85,13 @@ void setup() {
   int sum_odds2 = foldl(fcpp::plus,0,filter(odd,ix1));
   List<int>::iterator iix1;
   List<bool>::iterator ib1, ib2;
-  cout << "Length of bodds is " << length(bodds) << endl;
+  Serial << "Length of bodds is " << length(bodds) << endl;
   for (iix1 = ix1.begin(), ib1 = bodds.begin(), ib2 = bevens.begin(); iix1 != ix1.end(); ++iix1, ++ib1, ++ib2) {
-     cout << *iix1 << " " << *ib1 << " " << *ib2 << endl;
+     Serial << *iix1 << " " << *ib1 << " " << *ib2 << endl;
   }
 
-  cout << "Length of odds is " << length(odds) << endl;
-  cout << "sum of the odds is " << sum_odds << endl;
+  Serial << "Length of odds is " << length(odds) << endl;
+  Serial << "sum of the odds is " << sum_odds << endl;
 
 }
 
