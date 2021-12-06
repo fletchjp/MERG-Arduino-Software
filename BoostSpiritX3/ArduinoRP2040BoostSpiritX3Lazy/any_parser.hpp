2e08822cbe1044943896ec3d0ddb86df4b85e8d8
John Fletcher <john@bunbury28.plus.com>
2021-12-06 22:50:32 +0000

    Create any_parser.hpp
    Code from lazy example

---
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3Lazy/any_parser.hpp
@@ -0,0 +1,62 @@
+/// @file any_parser.hpp
+/// @brief Arduino RP2040 code for Boost Spirit X3 any_parser example
+///
+/// This is based on an example: 
+///
+/// https://stackoverflow.com/questions/60171119/boost-spirit-x3-lazy-parser
+///
+
+#ifndef ANY_PARSER_HPP
+#define ANY_PARSER_HPP
+
+#include <boost_spirit_home_x3.hpp>
+
+namespace x3 = boost::spirit::x3;
+
+/// namespace named any_parser
+namespace any_parser {
+    template <typename T>
+    struct as_type {
+        template <typename...> struct Tag{};
+
+        template <typename P>
+        auto operator[](P p) const {
+            return x3::rule<Tag<T, P>, T> {"as"} = x3::as_parser(p);
+        }
+    };
+
+    template <typename Tag>
+    struct set_context_type {
+        template <typename P>
+        auto operator[](P p) const {
+            auto action = [](auto& ctx) {
+                x3::get<Tag>(ctx) = x3::_attr(ctx);
+            };
+            return x3::omit [ p [ action ] ];
+        }
+    };
+
+    template <typename Tag>
+    struct lazy_type : x3::parser<lazy_type<Tag>> {
+        using attribute_type = typename Tag::attribute_type; // TODO FIXME?
+
+        template<typename It, typename Ctx, typename RCtx, typename Attr>
+        bool parse(It& first, It last, Ctx& ctx, RCtx& rctx, Attr& attr) const {
+            auto& subject = x3::get<Tag>(ctx);
+
+            return x3::as_parser(subject)
+                .parse(
+                    first, last, 
+                    std::forward<Ctx>(ctx),
+                    std::forward<RCtx>(rctx),
+                    attr);
+        }
+    };
+
+    template <typename T> static const as_type<T>          as{};
+    template <typename T> static const set_context_type<T> set_context{};
+    template <typename T> static const lazy_type<T>        lazy{};
+}
+
+
+#endif
