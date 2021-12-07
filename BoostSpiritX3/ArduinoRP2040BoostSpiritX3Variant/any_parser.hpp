f6f782b95682c157fc01ec2d831084c208685b08
John Fletcher <john@bunbury28.plus.com>
2021-12-07 22:57:46 +0000

    Update any_parser.hpp
    Minor changes

--- BoostSpiritX3/ArduinoRP2040BoostSpiritX3Variant/any_parser.hpp
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3Variant/any_parser.hpp
@@ -18,7 +18,10 @@ using boost::spirit::x3::phrase_parse;
 using boost::spirit::x3::skip_flag;
 using boost::spirit::x3::skipper_tag;
 using boost::spirit::x3::_attr;
+//using boost::spirit::x3::char_;
 
+typedef char const* iterator_type;
+typedef decltype(make_context<skipper_tag>(space)) context_type;
 
 /// simple test of any_parser
 bool basic_test()
@@ -29,15 +32,39 @@ bool basic_test()
         auto c = lit('c');
 
         {
-            //typedef std::string::const_iterator iterator_type;
-            typedef const char* iterator_type;
-            x3::any_parser<iterator_type> p =
+            any_parser<iterator_type> p =
                 *(a | b | c);
             char const* in("abcabcacb");
             char const* last = in;
             while (*last) last++;
             return x3::parse(in, last, p);   
             //BOOST_TEST(test("abcabcacb", start));
+        }
+        return true;
+}
+
+/// use lambda function to get the output
+bool another_test()
+{ 
+        std::string s;
+        auto f = [&](auto& ctx){ s = _attr(ctx); };
+
+        using x3::lit;
+        auto a = lit('a');
+        auto b = lit('b');
+        auto c = lit('c');
+
+        {
+          // this does not compile although it is a copy from a test file.
+          // It is the rhs of the parser which causes the problem.
+             x3::any_parser<iterator_type, std::string> p = char_ >> *(',' >> char_);
+          //  x3::any_parser<iterator_type, unused_type> p; //= char_ >> *(',' >> char_);
+            char const* in("a,b,c,d,e,f");
+            char const* last = in;
+            while (*last) last++;
+            bool res =  x3::parse(in, last, p[f]);
+            Serial << "output : " << s << endl; // output is blank!
+            return res;
         }
         return true;
 }
