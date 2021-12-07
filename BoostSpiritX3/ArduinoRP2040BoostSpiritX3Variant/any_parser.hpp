ed2482aaa43a9be0a303f441e74b9e4ca61c6de4
John Fletcher <john@bunbury28.plus.com>
2021-12-07 18:09:36 +0000

    Update any_parser.hpp
    Adapt test code

--- BoostSpiritX3/ArduinoRP2040BoostSpiritX3Variant/any_parser.hpp
+++ BoostSpiritX3/ArduinoRP2040BoostSpiritX3Variant/any_parser.hpp
@@ -1,6 +1,7 @@
 /// @file any_parser.hpp
 /// @brief Arduino RP2040 code for Boost Spirit X3 any_parser simple example
 ///
+/// test adapted from Boost X3 test of any_parser.
 
 #ifndef ANY_PARSER_HPP
 #define ANY_PARSER_HPP
@@ -8,11 +9,18 @@
 #include <boost_spirit_home_x3.hpp>
 
 namespace x3 = boost::spirit::x3;
+using namespace boost::spirit::x3::ascii;
+using boost::spirit::x3::any_parser;
+using boost::spirit::x3::make_context;
+using boost::spirit::x3::lit;
+using boost::spirit::x3::unused_type;
+using boost::spirit::x3::phrase_parse;
+using boost::spirit::x3::skip_flag;
+using boost::spirit::x3::skipper_tag;
+using boost::spirit::x3::_attr;
 
-/// namespace named any_parser
 
-
-/// somehow the pares here messes with the defintion of Variant_struct.
+/// simple test of any_parser
 bool basic_test()
 { // basic tests
         using x3::lit;
@@ -21,7 +29,8 @@ bool basic_test()
         auto c = lit('c');
 
         {
-            typedef std::string::const_iterator iterator_type;
+            //typedef std::string::const_iterator iterator_type;
+            typedef const char* iterator_type;
             x3::any_parser<iterator_type> p =
                 *(a | b | c);
             char const* in("abcabcacb");
