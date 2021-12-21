245f18fa6bbe6ce4b54edd10bdd4c4a7500b5acd
John Fletcher <john@bunbury28.plus.com>
2021-12-21 23:01:23 +0000

    Create TestSwitch.ino
    Create switch statement example

---
+++ TestSwitch/TestSwitch.ino
@@ -0,0 +1,44 @@
+/// @file TestSwitch.ino
+/// @brief Test ideas about switch statements.
+
+// 3rd party libraries
+#include <Streaming.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  while(!Serial);
+  Serial.begin (115200);
+  Serial << endl << endl << F("> ** TestSwitch ** ") << __FILE__ << endl;
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial << F("enter a number");
+  int cast = 0;
+  Serial >> cast;
+  // This will need to be input
+  int value = 0;
+  Serial << F("cast = ") << cast << endl;
+
+  switch(cast) {
+
+     case 0:  
+        Serial << F("simple case") << endl;
+        break;
+
+     case 1:
+        Serial << F("This case sets value") << endl;
+        value = cast;
+        break;
+
+     case 2:
+        int another = cast;
+        break;
+
+     case 3:
+        Serial << another << endl;
+  }
+  Serial << F("value = ") << value << endl;
+
+}
