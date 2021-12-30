522b50de075cf9b5e7e1e39dd62158b1d80bfd23
John Fletcher <john@bunbury28.plus.com>
2021-12-30 23:59:30 +0000

    Create TestLittleFS.ino
    New example

---
+++ TestLittleFS/TestLittleFS.ino
@@ -0,0 +1,14 @@
+/// @file TestLittleFS.ino
+/// @brief First experiment with LittleFS
+///
+/// https://arduino-pico.readthedocs.io/en/latest/fs.html
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
