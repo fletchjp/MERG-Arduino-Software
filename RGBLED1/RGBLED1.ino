f71e613a815f0d4efc4f870ecfe714fe14e59a43
John Fletcher <john@bunbury28.plus.com>
2021-12-30 22:48:59 +0000

    Create RGBLED1.ino
    New example

---
+++ RGBLED1/RGBLED1.ino
@@ -0,0 +1,21 @@
+/// @file RGBLED1.ino
+/// @brief First experiment with RGB LED
+///
+/// https://www.youtube.com/watch?v=mZxJxPZuhMs
+
+//#include <SPI.h>
+//#include <WiFiNINA_Generic.h>
+//#include <WiFiNINA.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode(LEDR, OUTPUT);
+  //pinMode(LEDG, OUTPUT);
+  //pinMode(LEDB, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
