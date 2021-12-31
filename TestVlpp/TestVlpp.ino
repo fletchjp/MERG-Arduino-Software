024e71ded2e92d05cbb4443d20cd6494b7bd19ea
John Fletcher <john@bunbury28.plus.com>
2021-12-31 17:36:44 +0000

    Create TestVlpp.ino
    New example

---
+++ TestVlpp/TestVlpp.ino
@@ -0,0 +1,19 @@
+/// @file TestVlpp.ino
+/// @brief First experiment with Functional-Vlpp library
+///
+/// https://github.com/khoih-prog/functional-vlpp
+
+// For this functional-vlpp library
+#include <functional-vlpp.h>
+
+
+//
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
