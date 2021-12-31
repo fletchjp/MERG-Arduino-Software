6e5aadb4f3e222f903f91210e246214cfaa922f9
John Fletcher <john@bunbury28.plus.com>
2021-12-31 16:21:09 +0000

    Update RGBLED1.ino
    Using WiFiManager_NINA_Lite

--- RGBLED1/RGBLED1.ino
+++ RGBLED1/RGBLED1.ino
@@ -3,10 +3,23 @@
 ///
 /// https://www.youtube.com/watch?v=mZxJxPZuhMs
 
-#include <SPI.h>
+
+//#include <WiFiManager_NINA_Lite.h>
+//#include <WiFiManager_NINA_Lite_Debug.h>
+//#include <WiFiManager_NINA_Lite_DUE.h>
+//#include <WiFiManager_NINA_Lite_nRF52.h>
+#include <WiFiManager_NINA_Lite_RP2040.h>
+//#include <WiFiManager_NINA_Lite_SAMD.h>
+//#include <WiFiManager_NINA_Lite_SAMD_CP.h>
+//#include <WiFiManager_NINA_Lite_STM32.h>
+//#include <WiFiManager_NINA_Lite_Teensy.h>
+
+
+//#include <SPI.h>
 // Warning when used with arduino-pico core.
 // It includes SPI.h if it has not been included already.
-#include <WiFiNINA_Generic.h>
+//#include <WiFiNINA_Generic.h>
+
 //#include <WiFiNINA.h>
 
 void setup() {
