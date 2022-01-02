6bece6f1e89a323d3ed7b31286fed8e9f25241b5
John Fletcher <john@bunbury28.plus.com>
2022-01-02 22:15:09 +0000

    Update CAN1602LONG2.ino
    Bring in sendLongMessage routine

--- CAN1602LONG2/CAN1602LONG2.ino
+++ CAN1602LONG2/CAN1602LONG2.ino
@@ -578,6 +578,32 @@ void processSwitches(void)
   }
 }
 
+bool sendLongMessage(void)
+{
+  int string_length; // Returned by snprintf. This may exceed the actual length.
+  unsigned int message_length;
+  while (cbus_long_message.is_sending()) { } //wait for previous message to finish.
+  string_length = snprintf(long_message_output_buffer, output_buffer_size,
+                           "This is a Long Message");
+  message_length = strlen(long_message_output_buffer);
+  if (message_length > 0) {
+    if (cbus_long_message.sendLongMessage(long_message_output_buffer,
+                                          message_length, stream_id) )
+    {
+      Serial << F("long message ") << long_message_output_buffer << F(" sent to ")
+             << stream_id << endl;
+    } else {
+      Serial << F("long message sending ")
+             << long_message_output_buffer << F(" to ")
+             << stream_id << F(" failed with message length ") << message_length
+             << endl;
+    }
+  } else {
+    Serial << "long message preparation failed with message length "
+           << message_length << endl;
+  }
+}
+
 // Send an event routine according to Module Switch
 bool sendEvent(byte opCode, unsigned int eventNo)
 {
