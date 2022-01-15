92eefdfcc5ccaa319d297184b78eeeb188d11911
John Fletcher <john@bunbury28.plus.com>
2022-01-15 23:06:37 +0000

    Add Doxygen output

---
+++ TestPrograms/TestWarnings/README.md
@@ -0,0 +1,14 @@
+# TestWarnings
+
+@mainpage TestWarnings
+
+This is a test programme looking at some warnings turned off in the Arduino IDE.
+
+### Anonymous namespace
+
+I have managed to get Doxygen to document the anonymous namespace.
+
+This required a change in the configuration file to set 
+
+EXTRACT_ANON_NSPACES   = YES
+
--- TestPrograms/TestWarnings/TestWarnings.ino
+++ TestPrograms/TestWarnings/TestWarnings.ino
@@ -1,5 +1,13 @@
 /// @file TestWarnings.ino
 /// @brief Test code warnings.
+///
+/// There are two problems.
+///
+/// One is that the Arduino IDE normal setting turns off all the warnings.
+///
+/// The other is that the Arduino IDE uses -fpermissive which means that some errors become warnings.
+/// 
+/// As warnings are turned off there are then no warnings of some errors.
 
 // 3rd party libraries
 #include <Streaming.h>
