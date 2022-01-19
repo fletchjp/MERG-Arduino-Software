27205be02872a65e6f9a29382d21cf73f3a65bf5
John Fletcher <john@bunbury28.plus.com>
2022-01-19 12:30:08 +0000

    Update matrixKeyboardMEGA44.ino
    minor change

--- TestKeyPad/matrixKeyboardMEGA44/matrixKeyboardMEGA44.ino
+++ TestKeyPad/matrixKeyboardMEGA44/matrixKeyboardMEGA44.ino
@@ -1,4 +1,4 @@
-/// @file matrixKeyboard44.ino
+/// @file matrixKeyboardMEGA44.ino
 /// @brief Example of matrix keyboard support for MEGA 4 by 4 Keypad built into IoAbstraction
 ///
 /// This example shows how to use the matrix keyboard support that's built into IoAbstraction,
