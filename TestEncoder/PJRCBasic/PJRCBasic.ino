3b7f8da69c54a1a917070c7d3d443d4f5ae3d0b1
John Fletcher <john@bunbury28.plus.com>
2022-01-26 22:21:05 +0000

    Update PJRCBasic.ino
    This is now working code - encoder only.

--- TestEncoder/PJRCBasic/PJRCBasic.ino
+++ TestEncoder/PJRCBasic/PJRCBasic.ino
@@ -4,6 +4,8 @@
 /// https://www.pjrc.com/teensy/td_libs_Encoder.html
 ///
 /// This is configured as an Arduino library. However, there is another one of the same name in the system.
+///
+/// This example is now working - encoder only, no button.
 
 /* Encoder Library - Basic Example
  * http://www.pjrc.com/teensy/td_libs_Encoder.html
@@ -13,11 +15,14 @@
 
 #include "Encoder.h"
 
+const int PinCLK=2;   // Generating interrupts using CLK signal
+const int PinDT=3;    // Reading DT signal
+
 // Change these two numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
 //   Low Performance:  neither pin has interrupt capability
-Encoder myEnc(5, 6);
+Encoder myEnc(PinCLK, PinDT);
 //   avoid using pins with LEDs attached
 
 void setup() {
