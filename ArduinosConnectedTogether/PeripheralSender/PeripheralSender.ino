6aa9c19be381f9f53faebc65774c64066903e940
John Fletcher <john@bunbury28.plus.com>
2022-03-13 20:30:27 +0000

    Update PeripheralSender.ino
    Add Doxygen comments

--- ArduinosConnectedTogether/PeripheralSender/PeripheralSender.ino
+++ ArduinosConnectedTogether/PeripheralSender/PeripheralSender.ino
@@ -1,13 +1,14 @@
-// Wire Peripheral Sender
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Sends data as an I2C/TWI peripheral device
-// Refer to the "Wire Master Reader" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
+/// @file PeripheralSender.ino
+/// @brief Wire Peripheral Sender
+/// by Nicholas Zambetti <http://www.zambetti.com>
+///
+/// Demonstrates use of the Wire library
+/// Sends data as an I2C/TWI peripheral device
+/// Refer to the "Wire Master Reader" example for use with this
+///
+/// Created 29 March 2006
+///
+/// This example code is in the public domain.
 
 
 #include <Wire.h>
