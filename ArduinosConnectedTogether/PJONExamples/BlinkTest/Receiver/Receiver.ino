716fdee38843dc92f42926faed83ee1e0f7bf949
John Fletcher <john@bunbury28.plus.com>
2022-04-04 14:52:41 +0100

    Add new examples

---
+++ ArduinosConnectedTogether/PJONExamples/BlinkTest/Receiver/Receiver.ino
@@ -0,0 +1,27 @@
+
+#include <PJONThroughSerial.h>
+
+PJONThroughSerial bus(44);
+
+void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+ /* Make use of the payload before sending something, the buffer where payload points to is
+    overwritten when a new message is dispatched */
+  if(payload[0] == 'B') {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(30);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+};
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // Initialize LED 13 to be off
+  Serial.begin(9600);
+  bus.strategy.set_serial(&Serial);
+  bus.set_receiver(receiver_function);
+  bus.begin();
+};
+
+void loop() {
+  bus.receive(1000);
+};
