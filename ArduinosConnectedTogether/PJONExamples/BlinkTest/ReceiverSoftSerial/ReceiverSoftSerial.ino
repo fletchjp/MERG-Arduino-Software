d442454ef5bea230c5c1629f178c05b13570e836
John Fletcher <john@bunbury28.plus.com>
2022-04-04 18:17:29 +0100

    Update ReceiverSoftSerial.ino
    Ammend for Softserial use and add setup output

--- ArduinosConnectedTogether/PJONExamples/BlinkTest/ReceiverSoftSerial/ReceiverSoftSerial.ino
+++ ArduinosConnectedTogether/PJONExamples/BlinkTest/ReceiverSoftSerial/ReceiverSoftSerial.ino
@@ -1,5 +1,12 @@
 /// @file ReceiverSoftSerial.ino
 /// @brief BlinkTestReceiver using a Soft Serial port
+/// Using https://arduinojson.org/v6/how-to/do-serial-communication-between-two-boards/
+
+#include <SoftwareSerial.h>
+
+// Declare the "link" serial port
+// Please see SoftwareSerial library for detail
+SoftwareSerial linkSerial(10, 11); // RX, TX
 
 #include <PJONThroughSerial.h>
 
@@ -16,12 +23,18 @@ void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info
 };
 
 void setup() {
+   // Initialize "debug" serial port
+  // The data rate must be much higher than the "link" serial port
+  Serial.begin(115200);
+  while (!Serial) continue;
+  Serial.println("ReceiverSoftSerial running");
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW); // Initialize LED 13 to be off
-  Serial.begin(9600);
-  bus.strategy.set_serial(&Serial);
+  linkSerial.begin(4800);
+  bus.strategy.set_serial(&linkSerial);
   bus.set_receiver(receiver_function);
   bus.begin();
+  Serial.println("PJON bus running");
 };
 
 void loop() {
