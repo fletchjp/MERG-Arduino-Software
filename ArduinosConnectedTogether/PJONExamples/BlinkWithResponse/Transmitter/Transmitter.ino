c8b156b8281af2993dc1f3ca5059f5a44cf4be42
John Fletcher <john@bunbury28.plus.com>
2022-04-04 15:40:34 +0100

    Add more examples

---
+++ ArduinosConnectedTogether/PJONExamples/BlinkWithResponse/Transmitter/Transmitter.ino
@@ -0,0 +1,29 @@
+
+#include <PJONThroughSerial.h>
+
+PJONThroughSerial bus(45);
+
+void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  /* Make use of the payload before sending something, the buffer where payload points to is
+     overwritten when a new message is dispatched */
+  if(payload[0] == 'B') {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(30);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+};
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // Initialize LED 13 to be off
+  Serial.begin(9600);
+  bus.strategy.set_serial(&Serial); // Pass the Serial object you want to use for communication
+  bus.set_receiver(receiver_function);
+  bus.begin();
+  bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
+};
+
+void loop() {
+  bus.update();
+  bus.receive();
+};
