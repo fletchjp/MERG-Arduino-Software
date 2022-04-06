363fc12effc5dd5f9a96f0622da3996cad4d68f7
John Fletcher <john@bunbury28.plus.com>
2022-04-06 16:19:05 +0100

    Update TransmitterWithErrorHandler.ino
    Adding error_handler code

--- ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkWithResponse/TransmitterWithErrorHandler/TransmitterWithErrorHandler.ino
+++ ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkWithResponse/TransmitterWithErrorHandler/TransmitterWithErrorHandler.ino
@@ -3,8 +3,15 @@
 
 #include <PJONSoftwareBitBang.h>
 
-
 PJONSoftwareBitBang bus(45);
+
+/// error_handler
+void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
+  if(code == PJON_CONNECTION_LOST) {
+    Serial.print("Connection lost with device id ");
+    Serial.println(bus.packets[data].content[0], DEC);
+  }
+};
 
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
   /* Make use of the payload before sending something, the buffer where payload points to is
@@ -23,6 +30,7 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW); // Initialize LED 13 to be off
 
   bus.strategy.set_pin(12);
+  bus.set_error(error_handler);
   bus.set_receiver(receiver_function);
   bus.begin();
   bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
