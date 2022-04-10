aa0e5e98a1b3815b56005b7bd260058a26f57b24
John Fletcher <john@bunbury28.plus.com>
2022-04-10 20:31:45 +0100

    Update Transmitter.ino
    Add error_handler

--- ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/RP2040BlinkTest/Transmitter/Transmitter.ino
+++ ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/RP2040BlinkTest/Transmitter/Transmitter.ino
@@ -7,13 +7,34 @@
 
 PJONSoftwareBitBang bus(45);
 
-void setup() {
-  bus.strategy.set_pin(BITBANG_PIN);
-  bus.begin();
-  bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
+/// error_handler
+void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
+  if(code == PJON_CONNECTION_LOST) {
+    Serial.print("Connection lost with device id ");
+    Serial.println(bus.packets[data].content[0], DEC);
+  }
+  if(code == PJON_PACKETS_BUFFER_FULL) {
+    Serial.print("Packet buffer is full, has now a length of ");
+    Serial.println(data);
+    Serial.println("Possible wrong bus configuration!");
+    Serial.println("higher PJON_MAX_PACKETS if necessary.");
+  }
+  if(code == PJON_CONTENT_TOO_LONG) {
+    Serial.print("Content is too long, length: ");
+    Serial.println(data);
+  }
+};
 
+void setup() {
+  while (!Serial);
   Serial.begin(115200);
   Serial.println("Raspberry Pi Pico Bitbang Test: Transmitter");
+  bus.strategy.set_pin(BITBANG_PIN);
+  bus.set_error(error_handler);
+  bus.begin();
+  bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
+  Serial.println("PJON bus is running");
+
 }
 
 void loop() {
