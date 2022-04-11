51133c36fb741619ea0718a1ee87bc96a47b480e
John Fletcher <john@bunbury28.plus.com>
2022-04-11 13:02:04 +0100

    Create TransmitterWithErrorHandler.ino
    New example

---
+++ ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkTest/TransmitterWithErrorHandler/TransmitterWithErrorHandler.ino
@@ -0,0 +1,29 @@
+/// @file TransmitterWithErrorHandler.ino
+/// @brief Transmitter with error handler from Transmitter1 in Any/MultiStrategyLink
+
+#include <PJONSoftwareBitBang.h>
+
+PJONSoftwareBitBang bus(45);
+
+/// error_handler
+void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
+  if(code == PJON_CONNECTION_LOST) {
+    Serial.print("Connection lost with device id ");
+    Serial.println(bus.packets[data].content[0], DEC);
+  }
+};
+
+void setup() {
+  while (!Serial);
+  Serial.begin(115200);
+  Serial.println("Bitbang Test: Transmitter with error handler");
+  bus.strategy.set_pin(12);
+  bus.set_error(error_handler);
+  bus.begin();
+  bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
+  Serial.println("PJON bus is running");
+}
+
+void loop() {
+  bus.update();
+};
