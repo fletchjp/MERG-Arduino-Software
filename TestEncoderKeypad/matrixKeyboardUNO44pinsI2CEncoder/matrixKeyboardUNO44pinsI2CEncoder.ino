5250b21507bca3499dec3bb74a286740f9cbee9b
John Fletcher <john@bunbury28.plus.com>
2022-05-10 15:45:24 +0100

    Create matrixKeyboardUNO44pinsI2CEncoder.ino
    New example with the keypad on I2C

---
+++ TestEncoderKeypad/matrixKeyboardUNO44pinsI2CEncoder/matrixKeyboardUNO44pinsI2CEncoder.ino
@@ -0,0 +1,179 @@
+/// @file matrixKeyboardUNO44pinsI2CEncoder.ino
+/// @brief Example of matrix keyboard support for UNO 4 by 4 Keypad built into IoAbstraction
+///
+/// Adding encoder from MartinsEncoder
+///
+/// Move keypad to I2C.
+///
+/// This example shows how to use the matrix keyboard support that's built into IoAbstraction,
+/// it can be used out the box with either a 3x4 or 4x4 keypad, but you can modify it to use
+/// any matrix keyboard quite easily.
+/// It just sends the characters that are typed on the keyboard to Serial. The keyboard in This
+/// example is connected directly to Arduino pins, but could just as easily be connected over
+/// a PCF8574, MCP23017 or other IoAbstraction.
+/// This version sets up the keyboard adapting the custom_keyboard example.
+ 
+#include <Wire.h>
+#include <IoAbstraction.h>
+#include <IoAbstractionWire.h>
+#include <TaskManagerIO.h>
+#include <KeyboardManager.h>
+
+/// I am going to configure this for a UNO or NANO
+///
+/// Swap the pins to get the opposite action
+#define SWAP_PINS 0
+
+#include "EncoderMD.h"
+
+boolean TurnDetected;
+
+const int PinCLK=2;   // Generating interrupts using CLK signal
+const int PinDT=3;    // Reading DT signal
+const int PinSW=4;     // Reading Push Button switch
+// Also connect +5V and ground.
+
+#if SWAP_PINS
+EncoderMD encoder(PinDT,PinCLK);
+#else
+EncoderMD encoder(PinCLK,PinDT);
+#endif
+
+int RotaryPosition=0;    // To store Stepper Motor Position
+
+int PrevPosition;     // Previous Rotary position Value to check accuracy
+
+void setupPCI()
+{
+  cli();
+  PCICR  |= 0b00000100;  //Set Pin Change Interrupt on Register D
+  PCMSK2 |= 0b00001100;  //Set pins 2 & 3 for interrupt
+  sei();
+}
+
+//
+// We need to make a keyboard layout that the manager can use. choose one of the below.
+// The parameter in brackets is the variable name.
+//
+//MAKE_KEYBOARD_LAYOUT_3X4(keyLayout)
+//MAKE_KEYBOARD_LAYOUT_4X4(keyLayout)
+//
+// I want to use the alternative based on the custom_keyboard example.
+//const char pgmLayout[] PROGMEM = "charsColByRow";
+//KeyboardLayout layout(rows, cols, const char* pgmLayout)
+
+/// Declare that the first 100 pins are reserved for the Arduino.
+MultiIoAbstractionRef multiIo = multiIoExpander(100);
+
+
+const byte ROWS = 4; /// four rows
+const byte COLS = 4; /// four columns
+/// define the symbols on the buttons of the keypads
+/// PROGMEM is important.
+const char layout[] PROGMEM = "123A456B789C*0#D"; // Chars have to be in a string.
+//  '1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'
+//};
+/// These are in order of Keypad pins from 1 to 8.
+/// Pin 1 is on the left with the pad face up.
+byte rowPins[ROWS] = {100, 101, 102, 103}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {104, 105, 106, 107}; //connect to the column pinouts of the keypad
+//const byte PIN_START = 3; // ONE less than the first pin.
+//byte rowPins[ROWS] = {9+PIN_START, 8+PIN_START, 7+PIN_START, 6+PIN_START}; //connect to the row pinouts of the keypad
+//byte colPins[COLS] = {5+PIN_START, 4+PIN_START, 3+PIN_START, 2+PIN_START}; //connect to the column pinouts of the keypad
+/// This seems fussy. ROWS and COLS will not work here.
+uint8_t rows = ROWS;
+uint8_t cols = COLS;
+/// Set up the keyLayout
+KeyboardLayout keyLayout(rows, cols, layout);
+
+///
+/// We need a keyboard manager class too
+///
+MatrixKeyboardManager keyboard;
+
+/// this example connects the pins directly to an arduino but you could use
+/// IoExpanders or shift registers instead.
+IoAbstractionRef arduinoIo = ioUsingArduino();
+
+///
+/// We need a class that extends from KeyboardListener. This gets notified when
+/// there are changes in the keyboard state.
+///
+class MyKeyboardListener : public KeyboardListener {
+public:
+    void keyPressed(char key, bool held) override {
+        Serial.print("Key ");
+        Serial.print(key);
+        Serial.print(" is pressed, held = ");
+        Serial.println(held);
+    }
+
+    void keyReleased(char key) override {
+        Serial.print("Released ");
+        Serial.println(key);
+    }
+} myListener;
+
+void setup() {
+/// setup 
+    setupPCI();
+  // These are done in the encoder constructor
+  //pinMode(PinCLK,INPUT);
+  //pinMode(PinDT,INPUT);  
+    pinMode(PinSW,INPUT);
+    digitalWrite(PinSW, HIGH); // Pull-Up resistor for switch
+    encoder.setLimits(0,100);
+    while(!Serial);
+    Serial.begin(115200);
+    Wire.begin();
+    /// Add 10 pins from 100 up.
+    multiIoAddExpander(multiIo, ioFrom8574(0x20), 10);
+
+    // Converted to copy the arrays.
+    for (byte i = 0; i < ROWS; i++)
+      keyLayout.setRowPin(i, rowPins[i]);
+    for (byte i = 0; i < COLS; i++)
+      keyLayout.setColPin(i, colPins[i]);
+
+    /// create the keyboard mapped to arduino pins and with the layout chosen above.
+    /// It will callback our listener
+    keyboard.initialise(multiIo, &keyLayout, &myListener);
+    //keyboard.initialise(arduinoIo, &keyLayout, &myListener);
+
+    /// start repeating at 850 millis then repeat every 350ms
+    keyboard.setRepeatKeyMillis(850, 350);
+
+    Serial.println("Keyboard and encoder are initialised!");
+}
+
+ISR(PCINT2_vect)  // Pin 2 & 3 interrupt vector
+{
+  encoder.encoderISR();
+}
+
+void loop() {
+/** as this indirectly uses taskmanager, we must include taskManager.runLoop(); in loop. */
+    taskManager.runLoop();
+      if (!(digitalRead(PinSW))) {   // check if button is pressed
+    if (RotaryPosition == 0) {  // check if button was already pressed
+        //Serial.println("XXX ");
+       } else {
+        //small_stepper.step(-(RotaryPosition*50));
+        RotaryPosition=0; // Reset position to ZERO
+        encoder.setPosition(RotaryPosition);
+        Serial.print("X ");
+        Serial.println(RotaryPosition);
+        PrevPosition = RotaryPosition;
+      }
+      
+  }
+
+  // Runs if rotation was detected
+  RotaryPosition = encoder.getPosition();
+  TurnDetected = (RotaryPosition != PrevPosition);
+  if (TurnDetected)  {
+    PrevPosition = RotaryPosition; // Save previous position in variable
+    Serial.println(RotaryPosition);
+  }
+
+}
