7af98a2a1393b679d4342a91ac81bf6f82ed3683
John Fletcher <john@bunbury28.plus.com>
2022-05-12 18:29:43 +0100

    Update Receiver.ino
    Change response character

--- ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkWithResponse/Receiver/Receiver.ino
+++ ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkWithResponse/Receiver/Receiver.ino
@@ -6,12 +6,12 @@ PJONSoftwareBitBang bus(44);
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
   /* Make use of the payload before sending something, the buffer where payload points to is
      overwritten when a new message is dispatched */
-  if(payload[0] == 'B') {
+  if(payload[0] == 'X') {
     Serial.println("BLINK");
     digitalWrite(LED_BUILTIN, HIGH);
     delay(30);
     digitalWrite(LED_BUILTIN, LOW);
-    bus.reply("B", 1);
+    bus.reply("X", 1);
   }
 };
 
