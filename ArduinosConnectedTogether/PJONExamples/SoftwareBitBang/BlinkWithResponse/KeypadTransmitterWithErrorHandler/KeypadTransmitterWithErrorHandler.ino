5fd40d65ec42697b463c96441fefe96fb75aac3d
John Fletcher <john@bunbury28.plus.com>
2022-05-12 18:29:07 +0100

    Update KeypadTransmitterWithErrorHandler.ino
    Add keypad code

--- ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkWithResponse/KeypadTransmitterWithErrorHandler/KeypadTransmitterWithErrorHandler.ino
+++ ArduinosConnectedTogether/PJONExamples/SoftwareBitBang/BlinkWithResponse/KeypadTransmitterWithErrorHandler/KeypadTransmitterWithErrorHandler.ino
@@ -16,7 +16,7 @@ void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
   /* Make use of the payload before sending something, the buffer where payload points to is
      overwritten when a new message is dispatched */
-  if(payload[0] == 'B') {
+  if(payload[0] == 'X') {
     Serial.println("BLINK");
     digitalWrite(LED_BUILTIN, HIGH);
     delay(30);
@@ -35,19 +35,77 @@ const byte COLS = 4; /// four columns
 const char layout[] PROGMEM = "123A456B789C*0#D"; // Chars have to be in a string.
 //  '1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'
 
+/// These are in order of Keypad pins from 1 to 8.
+/// Pin 1 is on the left with the pad face up.
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+/// This seems fussy. ROWS and COLS will not work here.
+uint8_t rows = ROWS;
+uint8_t cols = COLS;
+/// Set up the keyLayout
+KeyboardLayout keyLayout(rows, cols, layout);
+
+///
+/// We need a keyboard manager class too
+///
+MatrixKeyboardManager keyboard;
+
+/// this example connects the pins directly to an arduino but you could use
+/// IoExpanders or shift registers instead.
+IoAbstractionRef arduinoIo = ioUsingArduino();
+
+
+///
+/// We need a class that extends from KeyboardListener. This gets notified when
+/// there are changes in the keyboard state.
+///
+uint8_t payload[10];
+
+class MyKeyboardListener : public KeyboardListener {
+public:
+    void keyPressed(char key, bool held) override {
+        Serial.print("Key ");
+        Serial.print(key);
+        Serial.print(" is pressed, held = ");
+        Serial.println(held);
+        // Note conversion of the 
+        payload[0] = key;
+        bus.send(44, payload, 1); // Send key to device 44
+    }
+    void keyReleased(char key) override {
+        Serial.print("Released ");
+        Serial.println(key);
+    }
+} myListener;
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW); // Initialize LED 13 to be off
+    // Converted to copy the arrays.
+    for (byte i = 0; i < ROWS; i++)
+      keyLayout.setRowPin(i, rowPins[i]);
+    for (byte i = 0; i < COLS; i++)
+      keyLayout.setColPin(i, colPins[i]);
+
+    /// create the keyboard mapped to arduino pins and with the layout chosen above.
+    /// It will callback our listener
+    keyboard.initialise(arduinoIo, &keyLayout, &myListener);
+    /// start repeating at 850 millis then repeat every 350ms
+    keyboard.setRepeatKeyMillis(850, 350);
+
+    Serial.println("Keyboard is initialised!");
 
   bus.strategy.set_pin(12);
   bus.set_error(error_handler);
   bus.set_receiver(receiver_function);
   bus.begin();
-  bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
+  bus.send_repeatedly(44, "X", 1, 1000000); // Send B to device 44 every second
+  Serial.println("PJON bus running");
 }
 
 void loop() {
+  taskManager.runLoop();
   bus.update();
   bus.receive(1000);
 };
