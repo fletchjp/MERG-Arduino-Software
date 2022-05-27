4b342ebc2183c554f6650d7fb821de2fcd293e09
John Fletcher <john@bunbury28.plus.com>
2022-05-27 22:23:39 +0100

    Update PeripheralReceiver.ino
    Add a print

--- ArduinosConnectedTogether/I2CExamples/PeripheralReceiver/PeripheralReceiver.ino
+++ ArduinosConnectedTogether/I2CExamples/PeripheralReceiver/PeripheralReceiver.ino
@@ -30,6 +30,7 @@ void loop()
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
+  Serial.println("receiveEvent");
   while(1 < Wire.available()) // loop through all but the last
   {
     char c = Wire.read(); // receive byte as a character
