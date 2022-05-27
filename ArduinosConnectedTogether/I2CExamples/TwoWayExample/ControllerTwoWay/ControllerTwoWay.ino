40dc3fdebaa16c8b9fe1cb59efa0024dda9b8736
John Fletcher <john@bunbury28.plus.com>
2022-05-27 17:44:49 +0100

    Update ControllerTwoWay.ino
    Add more code

--- ArduinosConnectedTogether/I2CExamples/TwoWayExample/ControllerTwoWay/ControllerTwoWay.ino
+++ ArduinosConnectedTogether/I2CExamples/TwoWayExample/ControllerTwoWay/ControllerTwoWay.ino
@@ -18,18 +18,63 @@
 
 #include <Wire.h>
 
+#define TO_CONTROLLER_SIZE 3
+#define TO_PERIPHERAL_SIZE  4
+
+#define START_NODE   8 // The starting I2C address of slave nodes
+#define END_NODE     9 // last node to probe +1
+
+#define NODE_READ_DELAY 100 // Some delay between I2C node reads
+
+byte messageToController[TO_CONTROLLER_SIZE];
+byte messageToPeripheral[TO_PERIPHERAL_SIZE];
+
+void  sendToPeripheral(int);
+void  readFromPeripheral();
+
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Serial.begin(115200);  // start serial for output
+  Serial.println("Controller starting");
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from peripheral device #8
+  //Wire.requestFrom(8, 6);    // request 6 bytes from peripheral device #8
 
-  while (Wire.available()) { // peripheral may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
+  //while (Wire.available()) { // peripheral may send less than requested
+  //  char c = Wire.read(); // receive a byte as character
+  //  Serial.print(c);         // print the character
+  //}
+
+   for (int address = START_NODE; address < END_NODE; address++) {
+      sendToPeripheral(address);
+      readFromPeripheral();
+   }
+   delay(NODE_READ_DELAY);
+}
+
+void sendToPeripheral(int address) {
+  // message is 0123
+  for(int i = 0; i < TO_PERIPHERAL_SIZE; i++) {
+    messageToPeripheral[i] = (byte)i;  
   }
 
-  delay(500);
+  Wire.beginTransmission(address);
+  Wire.write(messageToPeripheral, TO_PERIPHERAL_SIZE);
+  Wire.endTransmission();
+}
+
+void readFromPeripheral() {
+  // if data size is available from nodes
+  if(Wire.available() == TO_CONTROLLER_SIZE) {
+    for (int i = 0; i < TO_CONTROLLER_SIZE; i++) {
+      messageToController[i] = Wire.read();  // get data
+    }
+    int fromAddress = messageToController[0];
+    int value = ((int)messageToController[1] << 8 ) | (int)messageToController[2];
+    Serial.print("Peripheral ");
+    Serial.print(fromAddress);
+    Serial.print(" says ");
+    Serial.print(value);
+  }
 }
