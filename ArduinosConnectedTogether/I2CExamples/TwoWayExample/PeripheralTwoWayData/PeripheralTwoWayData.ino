8167fc5512e0884f86c42b93e226fcda272207de
John Fletcher <john@bunbury28.plus.com>
2022-06-06 17:58:52 +0100

    Update PeripheralTwoWayData.ino
    Change read to char

--- ArduinosConnectedTogether/I2CExamples/TwoWayExample/PeripheralTwoWayData/PeripheralTwoWayData.ino
+++ ArduinosConnectedTogether/I2CExamples/TwoWayExample/PeripheralTwoWayData/PeripheralTwoWayData.ino
@@ -59,7 +59,7 @@ void receiveEvent(int howMany)
   Serial.println("receiveEvent");
   while(1 < Wire.available()) // loop through all but the last
   { // Now reading as byte.
-    byte c = Wire.read(); // receive byte as a character
+    char c = Wire.read(); // receive byte as a character
     Serial.print(c);         // print the character
   }
   int x = Wire.read();    // receive byte as an integer
