c7407eec7e8522f9c7f34aca40e8211d475c5bfa
John Fletcher <john@bunbury28.plus.com>
2022-07-22 21:14:24 +0100

    Update PicoRP2040FCPPParser.ino
    Change to use Pico output and run only parser example.

--- ArduinoSTL/PicoRP2040FCPPParser/PicoRP2040FCPPParser.ino
+++ ArduinoSTL/PicoRP2040FCPPParser/PicoRP2040FCPPParser.ino
@@ -29,7 +29,7 @@ parser              Monadic parser combinators.  A bit of a mess, but
 #define FCPP_ENABLE_LAMBDA
 #include "fcpp_prelude.h"
 
-#include <Streaming.h>
+//#include <Streaming.h>
 
 using namespace fcpp;
 
@@ -529,17 +529,17 @@ void parser_example()
    group      = bracket( charP('('), expression, charP(')') );
 //////////////////////////////////////////////////////////////////////
 
-   Serial << "Test of ParserM monad" << endl;
+   Serial.printf("Test of ParserM monad\n");
    std::string ss("1 + 2");
    //Serial << "string is " << ss << endl;
    StringL s( ss.begin(), ss.end() );
    StringL::iterator si;
    for (si = s.begin(); si != s.end(); ++si) {
-      Serial << *si << " ";
+      Serial.printf(" %c ",*si);
    }
-   Serial << endl;
-
-   Serial << "Length of s is " << length(s) << endl;  // force evaluation
+   Serial.printf("\n");
+   int l = length(s); // force evaluation
+   Serial.printf("Length of s is %d \n",l);// force evaluation
 
 
    typedef ParserM P;
@@ -548,82 +548,19 @@ void parser_example()
       [ X | X <= item ] ]()(s);
    List<std::pair<char,StringL> >::iterator lpi;
    for (lpi = lpcs.begin(); lpi != lpcs.end(); ++lpi) {
-      Serial << (*lpi).first << " " << (*lpi).second << endl;
+      Serial.printf("%c \n",(*lpi).first);
    }
 
 }
 
 //////////////////////////////////////////////////////////
-// This comes from the cdc_multi example
-// Helper: non-blocking "delay" alternative.
-boolean delay_without_delaying(unsigned long time) {
-  // return false if we're still "delaying", true if time ms has passed.
-  // this should look a lot like "blink without delay"
-  static unsigned long previousmillis = 0;
-  unsigned long currentmillis = millis();
-  if (currentmillis - previousmillis >= time) {
-    previousmillis = currentmillis;
-    return true;
-  }
-  return false;
-}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (115200);
-  unsigned long t1 = millis();
-  unsigned long t2;
-  while (!Serial && ((millis() - t1) <= 10000));
-  t2 = millis() - t1;
-  Serial.print("Waited for ");
-  Serial.print(t2);
-  Serial.println(" millis");
-  while (!delay_without_delaying(5000) ) { };
-  Serial << endl << endl << " Arduino RP2040 FC++ " << __FILE__ << endl;
-  Serial << "Some simple FC++ operations" << endl;
-  Serial << "plus(1,2) = " << plus(1,2) << endl;
-  Serial << "plus(1.5,2.3) = " << plus(1.5,2.3) << endl;
-  Serial << "plus(1)(2) = " << plus(1)(2) << endl;
-  Serial << "inc(1) = " << inc(1) << endl;
-  Serial << "--------------------------" << endl;
-
-  Maybe<int> mx(2), my(3);
-  Maybe<int> mz;
-  if (mz.is_nothing()) Serial << "mz is nothing" << endl;
-  mz = just(plus(mx.value(),my.value()));
-  if (mz.is_nothing()) { 
-     Serial << "mz is nothing" << endl;
-  } else {
-     Serial << "mz has " << mz.value() << endl;
-  }
-  Serial << "--------------------------" << endl;
-
-  List<double>::iterator idx1, idx2;
-  List<double> x1 = list_with(0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0);
-  List<double> x2 = fcpp::map(fcpp::minus(1.0),x1);
-  double sumx1 = foldl(plus,0.,x1);
-  for (idx1 = x1.begin(), idx2 = x2.begin(); idx1 != x1.end(); ++idx1, ++idx2) {
-     Serial << *idx1 << " " << *idx2 << endl;
-  }
-  Serial << "Sum of x1 = " << sumx1 << endl;
-  Serial << "--------------------------" << endl;
-
-  List<int> ix1 = enumFromTo(0,10);
-  List<bool> bodds = fcpp::map(odd,ix1);
-  List<bool> bevens = fcpp::map(even,ix1);
-  List<int> odds = filter(odd,ix1);
-  int sum_odds = foldl(plus,0,odds);
-  int sum_odds2 = foldl(plus,0,filter(odd,ix1));
-  List<int>::iterator iix1;
-  List<bool>::iterator ib1, ib2;
-  Serial << "Length of bodds is " << length(bodds) << endl;
-  for (iix1 = ix1.begin(), ib1 = bodds.begin(), ib2 = bevens.begin(); iix1 != ix1.end(); ++iix1, ++ib1, ++ib2) {
-     Serial << *iix1 << " " << *ib1 << " " << *ib2 << endl;
-  }
-
-  Serial << "Length of odds is " << length(odds) << endl;
-  Serial << "sum of the odds is " << sum_odds << endl;
-  while (!delay_without_delaying(5000) ) { };
+  while (!Serial) { }
+  //::delay(5000);
+  Serial.printf("Pico RP2040 FC++ operations\n");
   parser_example();
 }
 
