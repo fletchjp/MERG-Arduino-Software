f5d83cb1dd91ba8a3000ed945658938f2b7a1a9a
John Fletcher <john@bunbury28.plus.com>
2022-08-03 17:16:27 +0100

    Update fcpp_prelude.h
    Add copyright and explanation to this file.

--- fcpp/src/fcpp_prelude.h
+++ fcpp/src/fcpp_prelude.h
@@ -1,1 +1,14 @@
+//
+// Copyright (c) 2000-2003 Brian McNamara and Yannis Smaragdakis
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is granted without fee,
+// provided that the above copyright notice and this permission notice
+// appear in all source code copies and supporting documentation. The
+// software is provided "as is" without any express or implied
+// warranty.
+
 #include <fcpp/prelude.h>
+
+// This file is needed to enable access to the other files from the Arduino IDE.
+
