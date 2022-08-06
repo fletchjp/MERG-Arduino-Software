e010b7d3451b6d31cd9d674c75df5a257d79964c
John Fletcher <john@bunbury28.plus.com>
2022-08-06 18:54:14 +0100

    Update PicoRP2040FCPPCategory.ino
    Add some more header includes

--- PicoRP2040/PicoRP2040FCPPCategory/PicoRP2040FCPPCategory.ino
+++ PicoRP2040/PicoRP2040FCPPCategory/PicoRP2040FCPPCategory.ino
@@ -16,8 +16,17 @@
 #include <string>
 #define FCPP152
 #define FCPP_ENABLE_LAMBDA
-//#define FCPP_USE_EXCEPTIONS
+#define FCPP_EXTRA_STRONG_CURRY
+#define FCPP_PATTERN // define this to include pattern.h
+//#define FCPP_ARDUINO // Take out arguement traits - defined in prelude for Arduino etc
+// All headers modified to work in the Arduino/Pico environment.
+// Some exceptions guarding in pattern.h have had to be removed.
+#define FCPP_UNIFY // Unification of Monads and Functors in functors.h
 #include "fcpp_prelude.h"
+#include "fcpp/functors.h"
+#include "fcpp/comonad.h"
+#include "fcpp/arrow.h"
+
 
 #include <Streaming.h>
 
