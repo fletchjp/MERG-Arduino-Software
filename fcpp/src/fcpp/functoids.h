070728295cca52be52bf5c93b102bddf22b15e25
John Fletcher <john@bunbury28.plus.com>
2022-08-13 13:37:13 +0100

    Create functoids.h
    New file for functoids code

---
+++ fcpp/src/fcpp/functoids.h
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2000-2003 Brian McNamara and Yannis Smaragdakis
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is granted without fee,
+// provided that the above copyright notice and this permission notice
+// appear in all source code copies and supporting documentation. The
+// software is provided "as is" without any express or implied
+// warranty.
+
+#ifndef FCPP_FUNCTOIDS_DOT_H
+#define FCPP_FUNCTOIDS_DOT_H
+
+
+
+
+
+
+#endif
