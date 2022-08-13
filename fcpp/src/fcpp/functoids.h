988f9294b5843833bd765e16eb3edc2795226d87
John Fletcher <john@bunbury28.plus.com>
2022-08-13 13:41:26 +0100

    Update functoids.h
    Start to add comments and code.

--- fcpp/src/fcpp/functoids.h
+++ fcpp/src/fcpp/functoids.h
@@ -11,9 +11,58 @@
 #ifndef FCPP_FUNCTOIDS_DOT_H
 #define FCPP_FUNCTOIDS_DOT_H
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Monoid operations based on ideas from https://bartoszmilewski.com/2014/12/05/categories-great-and-small/
+/// and also from Learn You a Haskell for Great Good! p.252.
+///
+/// Haskell code:
+///
+/// class Monoid m where
+///    mempty  :: m
+///    mappend :: m -> m -> m
+///    mconcat :: [m] -> m
+///    mconcat = foldr mappend mempty
+///
+/// Note: mconcat is only in Learn You a Haskell for Great Good! p.252.
+///
+/// Following Monads in monad.h what I need first are the free functions.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+namespace fcpp {
+	
+/// Infer structure from the equivalent for Monad in monad.h
+template <class Rep> struct ThisTypeIsNotAnInstanceOfAnInferrableMonoid {};
+template <class T> struct MonoidError { inline static void error() {} };
+template <class T>
+struct MonoidError<ThisTypeIsNotAnInstanceOfAnInferrableMonoid<T> > {};
+template <class Rep> struct MonoidTraitsSpecializer {
+   typedef ThisTypeIsNotAnInstanceOfAnInferrableMonoid<Rep> Monoid;
+};
+template <class Rep> struct MonoidTraits {
+   typedef typename MonoidTraitsSpecializer<Rep>::Monoid Monoid;
+   inline static void ensure_is_instance_of_monoid() {
+      MonoidError<Monoid>::error();
+   }
+};
+
+//////////////////////////////////////////////////////////////////////
+/// Free functions for Monoids
+///    mempty  :: m
+///    mappend :: m -> m -> m
+///    mconcat :: [m] -> m
+//////////////////////////////////////////////////////////////////////
+/// mempty<Monoid>()();
+///
+/// mappend<Monoid>()(m1,m2);
+/// mmappend(m1,m2) - now inferrable for Mstring.
+///
 
 
 
+
+
+}
 
 
 #endif
