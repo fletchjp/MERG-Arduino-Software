4d5dd7897420f453466414bfdcd213055daf743a
John Fletcher <john@bunbury28.plus.com>
2022-08-13 13:48:59 +0100

    Update functoids.h
    Add monoid codes.

--- fcpp/src/fcpp/functoids.h
+++ fcpp/src/fcpp/functoids.h
@@ -57,12 +57,138 @@ template <class Rep> struct MonoidTraits {
 /// mappend<Monoid>()(m1,m2);
 /// mmappend(m1,m2) - now inferrable for Mstring.
 ///
+///
+/// mconcat<Monoid>()(lm) where lm has the type List<Monoid>
+/// mmconcat(lm)        - now inferrable for Mstring.
+//////////////////////////////////////////////////////////////////////
+
+namespace impl {
+  // I have used the example of IntZero in prelude.h line 3947 as a basis.
+   template <class Monoid>
+   struct XMempty : public CFunType<typename Monoid::Rep::Type> {
+    using MonoidResultType = typename Monoid::Rep::Type;
+       MonoidResultType
+      operator()() const {
+         return Monoid::mempty()();
+      }
+
+   };
+
+   template <class Monoid>
+   struct XMappend {
+
+      template <class A, class B> struct Sig;
+      // Force the monoids to have the same type
+      template <class A> struct Sig<A,A>
+      : public FunType<A,A,typename Monoid::Mappend::template Sig<A,A>::ResultType> {};
+
+      template <class A>
+      typename Sig<A,A>::ResultType
+      operator()( const A& a1 , const A& a2) const {
+         return Monoid::mappend()(a1,a2);
+      }
+
+   };
+
+   template <class Monoid>
+   struct XMconcat {
+      template <class L> struct Sig
+      : public FunType<L,typename Monoid::Mconcat::template Sig<L>::ResultType> {};
+
+      template <class L>
+      typename Sig<L>::ResultType
+      operator()( const L& l ) const {
+         return Monoid::mconcat()(l);
+      }
+
+   };
 
 
 
+  }
+
+template <class Monoid> Full0<impl::XMempty<Monoid> > mempty()
+{ return makeFull0( impl::XMempty<Monoid>() ); }
+template <class Monoid> struct Mempty
+{ typedef Full0<impl::XMempty<Monoid> > Type; };
+
+template <class Monoid> Full2<impl::XMappend<Monoid> > mappend()
+{ return makeFull2( impl::XMappend<Monoid>() ); }
+template <class Monoid> struct Mappend
+{ typedef Full2<impl::XMappend<Monoid> > Type; };
 
 
+template <class Monoid> Full1<impl::XMconcat<Monoid> > mconcat()
+{ return makeFull1( impl::XMconcat<Monoid>() ); }
+template <class Monoid> struct Mconcat
+{ typedef Full1<impl::XMconcat<Monoid> > Type; };
+
+////////////////////////////////////////////////////////////////////
+// The next thing is to infer mmappend(m1,m2)
+// based on bind in monad.h from line 206
+// I have now done mmconcat(lm) as well.
+////////////////////////////////////////////////////////////////////
+
+namespace impl {
+   template <bool b> struct AppendError {};
+   template <> struct AppendError<true> { static inline void error() {} };
+   template <class M, class K>
+   struct AppendHelper {
+      typedef typename MonoidTraits<M>::Monoid MonoidA;
+      //typedef typename MonadA::template UnRep<M>::Type A;
+      typedef typename MonoidA::Mappend::template Sig<M,K>::ResultType ResultType;
+      typedef typename MonoidTraits<ResultType>::Monoid MonoidB;
+      inline static void ensure_m_and_k_are_in_the_same_monoid_instance() {
+         AppendError<Conversion<MonoidA,MonoidB>::sameType>::error();
+      }
+   };
+   struct XMMappend {
+      template <class M, class K> struct Sig
+      : public FunType<M,K,typename AppendHelper<M,K>::ResultType> {};
+
+      template <class M, class K>
+      typename Sig<M,K>::ResultType
+      operator()( const M& m, const K& k ) const {
+         AppendHelper<M,K>::ensure_m_and_k_are_in_the_same_monoid_instance();
+         typedef typename AppendHelper<M,K>::MonoidA MM;
+         return MM::mappend()(m,k);
+      }
+
+   };
+   template <class L>
+   struct ConcatHelper {
+      //EnsureListLike<L>();
+      typedef typename L::ElementType M;
+      typedef typename MonoidTraits<M>::Monoid MonoidA;
+      //typedef typename MonadA::template UnRep<M>::Type A;
+      typedef typename MonoidA::Mconcat::template Sig<M>::ResultType ResultType;
+      typedef typename MonoidTraits<ResultType>::Monoid MonoidB;
+      inline static void ensure_m_and_k_are_in_the_same_monoid_instance() {
+         AppendError<Conversion<MonoidA,MonoidB>::sameType>::error();
+      }
+   };
+   struct XMMconcat {
+      template <class L> struct Sig
+      : public FunType<L,typename ConcatHelper<L>::ResultType> {};
+
+      template <class L>
+      typename Sig<L>::ResultType
+      operator()( const L& l ) const {
+         //AppendHelper<M,K>::ensure_m_and_k_are_in_the_same_monoid_instance();
+         typedef typename ConcatHelper<L>::MonoidA MM;
+         return MM::mconcat()(l);
+      }
+
+   };
 }
 
+typedef Full2<impl::XMMappend> MMappend;
+typedef Full1<impl::XMMconcat> MMconcat;
+FCPP_MAYBE_NAMESPACE_OPEN
+FCPP_MAYBE_EXTERN MMappend mmappend;
+FCPP_MAYBE_EXTERN MMconcat mmconcat;
+FCPP_MAYBE_NAMESPACE_CLOSE
+
+}
 
 #endif
