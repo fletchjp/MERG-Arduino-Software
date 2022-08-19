870610ebcf8d0f15e98d7aa66fb29568552645c5
John Fletcher <john@bunbury28.plus.com>
2022-08-19 23:14:45 +0100

    Update PicoRP2040Glucat.ino
    This code uses exceptions which is not available.

--- PicoRP2040/PicoRP2040Glucat/PicoRP2040Glucat.ino
+++ PicoRP2040/PicoRP2040Glucat/PicoRP2040Glucat.ino
@@ -4,16 +4,51 @@
 
 #include <string>
 
+
+//#define throw Serial<<
+
 #include "glucat.h"
 #include "glucat/glucat_imp.h"
+#include "glucat/tuning.h"
+typedef glucat::tuning<> Tune_P;
 
 #include <Streaming.h>
 
+using namespace glucat;
 
 //////////////////////////////////////////////////////////
 
+//template< class M_T , class T>
+void do_test1_double()
+{
+    typedef double T;
+    typedef framed_multi<T> number;
+    //typedef Multivector_T number;
+    Serial << "in do_test1() at start" << endl;
+    T x, y;
+    x = T(1.);
+    y = sqrt(x);
+    Serial << "in do_test1() after sqrt with y = " << y << endl;
+    index_set<DEFAULT_LO,DEFAULT_HI> i1(1);
+    index_set<DEFAULT_LO,DEFAULT_HI> i2(2);
+    //number z1(i1,1.);
+    number z2(i2);
+    //Serial<< "in do_test1() after sqrt with i1 = " << i1 << endl;
+    //Serial << "in do_test1() after sqrt with z1 = " << z1 << endl;
+    //Serial << "in do_test1() after sqrt with z2 = " << z2 << endl;
+    //number PO = 4.*z1 + 3.*z2;
+    //number QO = 2.*z1 + 3.*z2;
+
+}
+
+
 void glucat_examples()
 {
+  Serial << "framed_multi<double>" << endl;
+  Serial << endl;
+  //do_test1< framed_multi<double>, double >();
+  Serial << "matrix_multi<double>" << endl;
+  //do_test1< matrix_multi<double>, double >();
 }
  
 
@@ -27,7 +62,7 @@ void setup() {
   Serial << "Glucat Version " << _GLUCAT_VERSION << endl;
 #endif
   glucat_examples();
-  Serial.println("after fcpp_examples");
+  Serial.println("after glucat_examples");
 }
 
  
