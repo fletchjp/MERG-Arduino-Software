86e7ddb3e2ab4ca8893c7e59f1477c1c5e94a186
John Fletcher <john@bunbury28.plus.com>
2022-08-20 15:16:47 +0100

    Update functors.h
    Add bimap2 functoid

--- fcpp/src/fcpp/functors.h
+++ fcpp/src/fcpp/functors.h
@@ -344,6 +344,50 @@ FCPP_MAYBE_NAMESPACE_CLOSE
 
 #ifdef FCPP152
 
+  /////////////////////////////////////////////////////////////////////////
+  //
+  // I need a version of bimap which works on two argument functoids  bimap2(f,g,p1,p2)
+  // This is a four argument functoid, supported in FC++ 1.5.2
+  //
+  // This returns the pair  (f(p1.first,p2.first),g(p1.second,p2.second)
+  //
+  // I don't know how to write the Haskell version of this.
+  //
+  // I am using this with to make monoids work on pairs.
+  //
+  /////////////////////////////////////////////////////////////////////////
+
+  namespace impl {
+
+    struct XBimap2 {
+#ifdef FCPP_DEBUG
+       std::string name() const
+       {
+           return std::string("Bimap2");
+       }
+#endif
+      template <class F, class G,class P,class Q> struct Sig; // force the two pairs to have the same type.
+      
+      template <class F, class G,class Q>
+      struct Sig<F,G,Q,Q> : public FunType<F,G,Q,Q,
+       std::pair<typename RT<F,typename Q::first_type,typename Q::first_type>::ResultType,
+           typename RT<G,typename Q::second_type,typename Q::second_type>::ResultType > > { };
+
+      template <class F, class G,class Q>
+      typename Sig<F,G,Q,Q>::ResultType operator()
+      (const F& f,const G& g,const Q &p,const Q &q) const
+      {
+         return std::make_pair(f(p.first,q.first),g(p.second,q.second));
+      }
+    };
+
+  }
+ typedef Full4<impl::XBimap2> Bimap2;
+ FCPP_MAYBE_NAMESPACE_OPEN
+ FCPP_MAYBE_EXTERN Bimap2 bimap2;
+ FCPP_MAYBE_NAMESPACE_CLOSE
+
+
  namespace impl {
 
   /////////////////////////////////////////////////////////////////////////
