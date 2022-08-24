2947b014ea20782a01874f094db04a75c22cede6
John Fletcher <john@bunbury28.plus.com>
2022-08-24 22:32:08 +0100

    Update PicoRP2040FCPPMonoids.ino
    Add code to suppress warnings

--- PicoRP2040/PicoRP2040FCPPcodes/PicoRP2040FCPPMonoids/PicoRP2040FCPPMonoids.ino
+++ PicoRP2040/PicoRP2040FCPPcodes/PicoRP2040FCPPMonoids/PicoRP2040FCPPMonoids.ino
@@ -319,6 +319,36 @@ Print &operator << ( Print &obj, const MonoidType<T,Op> &arg)
 
 }
 
+// I have put these examples here to show how some things work and also to avoid some warnings
+// that things are defined and not used.
+auto bar() {
+   LambdaVar<1> X;
+   LambdaVar<2> Y;
+   return lambda()[
+      doM[ X <= list_with(1,2), Y <= list_with(3,4),
+           unitM<ListM>()[ makePair[X,Y] ] ] ];
+}
+
+auto qux() {
+   LambdaVar<1> X;
+   LambdaVar<2> Y;
+   return lambda()[ compM<ListM>()[ makePair[X,Y] | 
+          X<=list_with(1,2), guard[true], Y<=list_with(3,4), 
+          guard[equal[divides[Y,X],3] ] ]
+      ];
+}
+
+auto whoa() {
+   LambdaVar<1> even;
+   LambdaVar<2> odd;
+   LambdaVar<3> X;
+   return lambda()[
+      letrec[ even==lambda(X)[ if1[fcpp::equal[X,0],true,odd[fcpp::minus[X,1]]] ],
+              odd ==lambda(X)[ if2[notEqual[X,0],even[fcpp::minus[X,1]],false] ]
+      ].in[ even[3] ] ];
+}
+
+
 void monoid_examples()
 {
   Serial << "Monoid examples"
