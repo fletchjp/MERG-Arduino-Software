1e09a77c0628bd5e2599846c15a65cf2f62699eb
John Fletcher <john@bunbury28.plus.com>
2022-08-30 23:57:17 +0100

    Update PicoRP2040FCPPMonoids.ino
    New test cases for apply2a

--- PicoRP2040/PicoRP2040FCPPcodes/PicoRP2040FCPPMonoids/PicoRP2040FCPPMonoids.ino
+++ PicoRP2040/PicoRP2040FCPPcodes/PicoRP2040FCPPMonoids/PicoRP2040FCPPMonoids.ino
@@ -815,6 +815,10 @@ void monoid_examples()
   Serial << "apply(inc,3) = " << apply(inc,3) << endl;
   // It is really nice that this works without any extra code.
   Serial << "apply(endoinc,3) = " << apply(endoinc,3) << endl;
+  Serial << "apply2a(fcpp::plus,3,4) = " << apply2a(fcpp::plus,3,4) << endl;
+  Serial << "apply2a(fcpp::plus,3)(5) = " << apply2a(fcpp::plus,3)(5) << endl;
+  // This is not working yet.
+  //Serial << "apply2a(endo2plus,3,4) = " << apply2a(endo2plus,3,4) << endl;
 }
 
 void setup() {
