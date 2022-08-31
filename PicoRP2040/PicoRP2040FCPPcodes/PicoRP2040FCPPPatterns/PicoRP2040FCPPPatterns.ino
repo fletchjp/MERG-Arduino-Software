8e5a0f3b418a3f9f993ab32881fb0f70a9638f6d
John Fletcher <john@bunbury28.plus.com>
2022-08-31 22:33:25 +0100

    Update PicoRP2040FCPPPatterns.ino
    Add a lot more code

--- PicoRP2040/PicoRP2040FCPPcodes/PicoRP2040FCPPPatterns/PicoRP2040FCPPPatterns.ino
+++ PicoRP2040/PicoRP2040FCPPcodes/PicoRP2040FCPPPatterns/PicoRP2040FCPPPatterns.ino
@@ -318,6 +318,235 @@ void testing_patterns()
   x = hUncurry(plus,fork_(inc,dec,1));
   Serial << "hUncurry(plus,fork_(inc,dec,1)) = " << x << endl;
   Serial << "==================================" << endl;
+  Serial << "Experiments with lists" << endl;
+  Serial << "==================================" << endl;
+  List<int> l = enumFrom(1);
+  Serial << "List<int> l = enumFrom(1);" << endl;
+  std::pair<int,List<int> > il = fork_(head,tail,l);
+  Serial << "fork_(head,tail,a list(1,2,3,...))" << endl;
+  Serial << "( first: " << il.first
+            << ", head of second: " << head(il.second) << ")" << endl;
+  std::pair<int,List<int> > il2 = fork_(inc ^dot^ head,tail,l);
+  Serial << "fork_(inc ^dot^ head,tail,a list(1,2,3,...))" << endl;
+  Serial << "( first: " << il2.first
+            << ", head of second: " << head(il2.second) << ")" << endl;
+  Serial << "---------------" << endl;
+  std::pair<int,List<int> > il3 = out(l);
+  Serial << "std::pair<int,List<int> > il3 = out(l);" << endl;
+  Serial << "out(a list(1,2,3,...))" << endl;
+  Serial << "( first: " << il3.first
+            << ", head of second: " << head(il3.second) << ")" << endl;
+  Serial << "unfold1 takes a std::pair<T,List<T> >" << endl;
+  List<int> l2 = unfold1(il3);
+  Serial << "unfold1(1,a list(2,3,...)) makes original list" << endl;
+  Serial << "( first: " << head(l2)
+            << ", second: " << head(tail(l2)) << ")" << endl;
+  List<int> l3 = unfold1(first(inc,il3));
+  Serial << "unfold1(first(inc,pair(1,list))) makes new list" << endl;
+  Serial << "( first: " << head(l3)
+            << ", second: " << head(tail(l3)) << ")" << endl;
+
+  List<int> l4 = unfold1(first(inc)(il3)); // The same as l3.
+  Serial << "==================================" << endl;
+  Serial << "The Stream monad" << endl;
+  Serial << "==================================" << endl;
+  // This is 'double' now implemented as 'double_' see below.
+  std::pair<int,int> pi = forkp(makePair(id,id))(1);
+  Serial << "pi = forkp(makePair(id,id))(1);" << endl;
+  Serial << "pi.first = " << pi.first
+            << ", pi.second = " << pi.second << endl;
+  // My first take on double is more general
+  // There is no need to describe the functoid f twice.
+  std::pair<int,int> pi2 = doublef(id)(1);
+  Serial << "pi2 = doublef(id)(1);" << endl;
+  Serial << "pi2.first = " << pi2.first
+            << ", pi2.second = " << pi2.second << endl;
+  std::pair<int,int> pi3 = doublef(inc)(1);
+  Serial << "pi3 = doublef(inc)(1);" << endl;
+  Serial << "pi3.first = " << pi3.first
+            << ", pi3.second = " << pi3.second << endl;
+  std::pair<int,int> pi4 = double1(1);
+  Serial << "pi4 = double1(1);" << endl;
+  Serial << "pi4.first = " << pi4.first
+            << ", pi4.second = " << pi4.second << endl;
+  // This is supposed to be 'unfold double'
+  // but it has the wrong type.
+  //List<int> l5 = unfold1(pi);
+  // I had the wrong idea about this. This is what is needed.
+  // It won't compile unless defined as out(repeat)!!
+  Serial << "-------------------------------------" << endl;
+  std::pair<int,List<int> > il4 = double_(1);
+  std::pair<int,List<int> > il5 = out(repeat(1));
+  // Now I need diag for the bind of StreamM.
+  Serial << "-------------------------------------" << endl;
+  List<List<int> > lli = repeatS(enumFrom(1));
+  Serial << "List<List<int> > lli = repeatS(enumFrom(1));" << endl;
+  List<int> ild = diag(lli);
+  Serial << "List<int> ild = diag(lli);" << endl;
+  Serial << "head(ild) = " << head(ild) << ","
+            << " head(tail(ild)) = " << head(tail(ild)) << endl;
+  //List<List<int> > lli2 = map(repeatS,ild);
+  List<int> ild2 = diag(repeatS(enumFrom(1)));
+  Serial << "List<int> ild2 = diag(repeatS(enumFrom(1)));" << endl;
+  Serial << "head(ild2) = " << head(ild2) << ","
+            << " head(tail(ild2)) = " << head(tail(ild2)) << endl;
+  Serial << "head(tail(tail(ild2))) = " << head(tail(tail(ild2))) << endl;
+  Serial << "-------------------------------------" << endl;
+  List<int> ild3 = fcpp::map(inc,enumFrom(1));
+  Serial << "List<int> ild3 = map(inc,enumFrom(1));" << endl;
+  Serial << "head(ild3) = " << head(ild3) << ","
+            << " head(tail(ild3)) = " << head(tail(ild3)) << endl;
+  Serial << "head(tail(tail(ild3))) = " << head(tail(tail(ild3))) << endl;
+  List<int> ild4 = diag(repeatS(mapS(inc,enumFrom(1))));
+  Serial << "List<int> ild4 = diag(repeatS(mapS(inc,enumFrom(1))));"
+            << endl;
+  Serial << "head(ild4) = " << head(ild4) << ","
+            << " head(tail(ild4)) = " << head(tail(ild4)) << endl;
+  Serial << "head(tail(tail(ild4))) = " << head(tail(tail(ild4))) << endl;
+  Serial << "-------------------------------------" << endl;
+  List<int> ild5 = diag(mapS(repeatS,enumFrom(1)));
+  Serial << "List<int> ild5 = diag(mapS(repeatS,enumFrom(1)));"
+            << endl;
+  Serial << "head(ild5) = " << head(ild5) << ","
+            << " head(tail(ild5)) = " << head(tail(ild5)) << endl;
+  Serial << "head(tail(tail(ild5))) = " << head(tail(tail(ild5))) << endl;
+  Serial << "-------------------------------------" << endl;
+  Serial << "FC++ ListM monad " << endl;
+  Serial << "-------------------------------------" << endl;
+  LambdaVar<1> X;
+  //LambdaVar<2> Y;
+
+  List<int> l5 = lambda(X) [unitM<ListM>() [ X ] ](1);
+  Serial << "List<int> l5 = lambda(X) [unitM<ListM>() [ X ] ](1);"
+      << endl;
+  Serial << "head(l5) = " << head(l5)
+            << ", length(l5) = " << length(l5) << endl;
+  List<List<int> > ll1 = lambda(X) [unitM<ListM>() [ X ] ](l5);
+  Serial << "List<List<int> > ll1 = lambda(X) [unitM<ListM>() [ X ] ](l5);"
+      << endl;
+  Serial << "head(head(ll1)) = " << head(head(ll1))
+            << ", length(ll1) = " << length(ll1) << endl;
+  List<int> l6 = repeat(1);
+  Serial << "List<int> l6 = repeat(1);" << endl;
+  Serial << "head(l6) = " << head(l6)
+            << ", head(tail(l6)) = " << head(tail(l6)) << endl;
+  // I cannot find any examples of bindM<ListM> !!
+  // It needs a functiod of the type a -> [a].
+  Serial << "-------------------------------------" << endl;
+  List<int> l6a = lambda(X) [bindM<ListM>() [ X, repeat ] ](enumFromTo(1,2));
+  Serial << "l6a = lambda(X) [bindM<ListM>() [ X , repeat ] ](enumFromTo(1,2));"
+            << endl;
+  List<int>::iterator i;
+  int j = 0;
+  for (i = l6a.begin(); i != l6a.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+ Serial << "head(l6a) = " << head(l6a)
+            << ", head(tail(l6a)) = " << head(tail(l6a)) << endl;
+  Serial << "head(tail(tail(l6a))) = " << head(tail(tail(l6a))) << endl;
+  Serial << "-------------------------------------" << endl;
+  Serial << "This is the body of the previous one and they are the same"
+            << endl;
+  List<int> l6b = concat(mapS(repeat,enumFromTo(1,2)));
+  Serial << "List<int> l6b = concat(mapS(repeat,enumFromTo(1,2)));" << endl;
+  Serial << "head(l6b) = " << head(l6b)
+            << ", head(tail(l6b)) = " << head(tail(l6b)) << endl;
+  Serial << "head(tail(tail(l6b))) = " << head(tail(tail(l6b))) << endl;
+  j = 0;
+  for (i = l6b.begin(); i != l6b.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+  Serial << "-------------------------------------" << endl;
+  Serial << "This is the body of the previous one modified to do what I want."
+            << endl;
+  List<int> l6bb = concat(repeat(fcpp::map(id,enumFromTo(1,2))));
+  Serial << "List<int> l6bb = concat(repeat(mapS(id,enumFromTo(1,2))));"
+            << endl;
+  Serial << "head(l6bb) = " << head(l6bb)
+            << ", head(tail(l6bb)) = " << head(tail(l6bb)) << endl;
+  Serial << "head(tail(tail(l6bb))) = " << head(tail(tail(l6bb))) << endl;
+  j = 0;
+  for (i = l6bb.begin(); i != l6bb.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+  Serial << "-------------------------------------" << endl;
+  List<int> l6bc = concat(mapS(id,repeat(enumFromTo(1,2))));
+  Serial << "List<int> l6bc = concat(mapS(id,repeat(enumFromTo(1,2))));"
+            << endl;
+  j = 0;
+  for (i = l6bc.begin(); i != l6bc.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+  Serial << "-------------------------------------" << endl;
+  List<int> l6bd = lambda(X) [bindM<ListM>() [ X, id ] ](repeat(enumFromTo(1,2)));
+  Serial << "l6a = lambda(X) [bindM<ListM>() [ X , id ] ](repeat(enumFromTo(1,2)));"
+            << endl;
+  j = 0;
+  for (i = l6bd.begin(); i != l6bd.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+  Serial << "head(l6bd) = " << head(l6bd)
+            << ", head(tail(l6bd)) = " << head(tail(l6bd)) << endl;
+  Serial << "head(tail(tail(l6bd))) = " << head(tail(tail(l6bd))) << endl;
+  Serial << "-------------------------------------" << endl;
+  List<int> l6c = fcpp::map(inc,enumFromTo(1,2));
+  Serial << "List<int> l6c = map(inc,enumFromTo(1,2));" << endl;
+  j = 0;
+  for (i = l6c.begin(); i != l6c.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+  Serial << "-------------------------------------" << endl;
+  List<int> l6d = concat(repeat(l6c));
+  Serial << "List<int> l6d = concat(repeat(l6c));" << endl;
+  j = 0;
+  for (i = l6d.begin(); i != l6d.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
+  Serial << "-------------------------------------" << endl;
+  Serial << "New FC++ StreamM monad " << endl;
+  Serial << "-------------------------------------" << endl;
+  Serial << "Correction to bug in diag functoid means that results are correct now."
+      << endl;
+  List<int> l7 = lambda(X) [unitM<StreamM>() [ X ] ](1);
+  Serial << "List<int> l7 = lambda(X) [unitM<StreamM>() [ X ] ](1);"
+      << endl;
+  Serial << "head(l7) = " << head(l7)
+            << ", head(tail(l7)) = " << head(tail(l7)) << endl;
+  List<int> l8 = lambda(X) [bindM<StreamM>() [ X , id] ](l7);
+  Serial << "List<int> l8 = lambda(X) [bindM<StreamM>() [ X , id] ](l7);"
+            << endl;
+  Serial << "head(l8) = " << head(l8)
+            << ", head(tail(l8)) = " << head(tail(l8)) << endl;
+  List<int> l8a = lambda(X) [bindM<StreamM>() [ X , id] ](enumFrom(1));
+  Serial << "l8a = lambda(X) [bindM<StreamM>() [ X , id ] ](enumFrom(1));"
+            << endl;
+  Serial << "head(l8a) = " << head(l8a)
+            << ", head(tail(l8a)) = " << head(tail(l8a)) << endl;
+  List<int> l8b = lambda(X) [bindM<StreamM>() [ X , inc ] ](enumFrom(1));
+  Serial << "List<int> l8b = lambda(X) [bindM<StreamM>() [ X , inc ] ](enumFrom(1));"
+            << endl;
+  Serial << "head(l8b) = " << head(l8b)
+            << ", head(tail(l8b)) = " << head(tail(l8b)) << endl;
+  j = 0;
+  for (i = l8b.begin(); i != l8b.end(); ++i) {
+    ++j;
+    Serial << j << " " << *i << endl;
+    if (j > 5) break;
+  }
 
 }
 
