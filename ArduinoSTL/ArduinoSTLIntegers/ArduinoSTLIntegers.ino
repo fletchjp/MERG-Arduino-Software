22f243127b6a77408e1acaf29fdf5aafba8509eb
John Fletcher <john@bunbury28.plus.com>
2022-09-02 19:28:01 +0100

    Update ArduinoSTLIntegers.ino
    Add note on solution to problem

--- ArduinoSTL/ArduinoSTLIntegers/ArduinoSTLIntegers.ino
+++ ArduinoSTL/ArduinoSTLIntegers/ArduinoSTLIntegers.ino
@@ -1,6 +1,8 @@
 // ArduinoSTLIntegers
 // This example now works as long as the AVR Boards are 1.8.2 not 1.8.3
 // It also will not work with AVR 1.8.5 - multiple definition of std::nothrow.
+// I have deleted line 22 in new_handler.cpp and it now works on the UNO and DUE.
+// printf will not work on a DUE.
 #include <ArduinoSTL.h>
 
 using namespace std;
