1459d5fc426b82fec20a0dcd6d9ad1e200c0ca7a
John Fletcher <john@bunbury28.plus.com>
2022-09-20 11:27:07 +0100

    Update CAN1602PINIOTM.ino
    Correct error messages and increment version to 4.0b

--- CAN1602PINIOTM/CAN1602PINIOTM.ino
+++ CAN1602PINIOTM/CAN1602PINIOTM.ino
@@ -35,7 +35,7 @@
 ///////////////////////////////////////////////////////////////////////
 // LiquidCrystalIO versions
 // Version 4.0a beta 1 simple change of library.
-// Version 4.1a beta 1 proper task integration for the display.
+// Version 4.0b beta 1 proper task integration for the display. Correct error message lengths.
 // Long messages disabled for now (there is not enough memory on a UNO - 102%)
 // Memory usage without long messages is 95% (30830).
 // #define CBUS_LONG_MESSAGE
@@ -268,7 +268,7 @@ unsigned char mname[7] = { '1', '6', '0', '2', 'P', 'I', 'N' };
 
 // constants
 const byte VER_MAJ = 4;         // code major version
-const char VER_MIN = 'a';       // code minor version
+const char VER_MIN = 'b  ';       // code minor version
 const byte VER_BETA = 1;        // code beta sub-version
 const byte MODULE_ID = 99;      // CBUS module type
 
@@ -352,11 +352,12 @@ enum eventTypes {
 // This is following the methods in EzyBus_master to provide error messages.
 // These have been limited to 16 chars to go on an LCD 2 by 16 display.
 // blank_string is used to cancel an error message.
-const char blank_string[]   PROGMEM  = "                ";
-const char error_string_0[] PROGMEM  = "no error";
-const char error_string_1[] PROGMEM  = "Test message";
-const char error_string_2[] PROGMEM  = "Emergency Stop";
-const char error_string_3[] PROGMEM  = "CANbus error";
+// Adjust length of error messages
+const char blank_string[]   PROGMEM  = "             ";
+const char error_string_0[] PROGMEM  = "no error     ";
+const char error_string_1[] PROGMEM  = "Test message ";
+const char error_string_2[] PROGMEM  = "EmergencyStop";
+const char error_string_3[] PROGMEM  = "CANbus error ";
 const char error_string_4[] PROGMEM  = "invalid error";
 
 const char* const error_string_table[] PROGMEM = {
