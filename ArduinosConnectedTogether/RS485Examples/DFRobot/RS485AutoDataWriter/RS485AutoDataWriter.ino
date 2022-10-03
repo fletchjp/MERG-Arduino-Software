72108eed1af058fc479774d24e9b1bd8032506be
John Fletcher <john@bunbury28.plus.com>
2022-10-03 22:40:40 +0100

    Update RS485AutoDataWriter.ino
    Building code

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485AutoDataWriter/RS485AutoDataWriter.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485AutoDataWriter/RS485AutoDataWriter.ino
@@ -1,10 +1,19 @@
-// DFR0219 example code to transmit and receive data using Auto485
+/// RS485AutoDataWriter.ino
 
+// DFR0219 example code to transmit and receive data using Auto485 and send output to I2C.
+// Make I2C optional to take it out for production code.
+#define WRITE_I2C
+
+#ifdef WRITE_I2C
+#include <Wire.h>
+#define I2C_DEVICE 4  // Chose a device no.
+#endif
 #include <Auto485.h>
 // I have made a small change to the header file to avoid a warning from the Arduino compiler.
 
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
+
 
 Auto485  bus(EN); // Single pin version
 
@@ -12,6 +21,9 @@ void setup() {
   // put your setup code here, to run once:
   //pinMode(EN, OUTPUT);
   //Serial.begin(19200);
+#ifdef WRITE_I2C
+  Wire.begin(); // join i2c bus (address optional for master)
+#endif
   bus.begin(19200);
 }
 
@@ -20,8 +32,13 @@ void loop() {
   //digitalWrite(EN, HIGH); // Enable data transmit
   //Serial.print('A');
   if (bus.available() > 0 ) {
-    while (bus.available() > 0 )
+#ifdef WRITE_I2C
+     Wire.beginTransmission(I2C_DEVICE); // transmit to device #I2C_DEVICE
+     Wire.write("Reading ");
+#endif
+    while (bus.available() > 0 ) {
       bus.write(bus.read()); // mode -> transmit
+    }
     bus.println(); // mode -> receive.
   }
   delay(1000);
