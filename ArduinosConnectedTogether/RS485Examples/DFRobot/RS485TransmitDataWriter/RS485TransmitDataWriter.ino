8f9eee0333fdd6dbcc688c055fef3912c3902479
John Fletcher <john@bunbury28.plus.com>
2022-10-03 22:48:46 +0100

    Update RS485TransmitDataWriter.ino
    Building code

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataWriter/RS485TransmitDataWriter.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataWriter/RS485TransmitDataWriter.ino
@@ -1,6 +1,15 @@
 /// RS485TransmitDataWriter.ino
 
-// DFR0219 example code to transmit data
+// DFR0219 example code to transmit data and send output to I2C.
+
+// Make I2C optional to take it out for production code.
+#define WRITE_I2C
+
+#ifdef WRITE_I2C
+#include <Wire.h>
+#define I2C_DEVICE 4  // Chose a device no.
+#endif
+
 
 // With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
 
@@ -10,6 +19,9 @@ int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 
 void setup() {
   // put your setup code here, to run once:
+#ifdef WRITE_I2C
+  Wire.begin(); // join i2c bus (address optional for master)
+#endif
   pinMode(EN, OUTPUT);
   Serial.begin(19200);
 }
