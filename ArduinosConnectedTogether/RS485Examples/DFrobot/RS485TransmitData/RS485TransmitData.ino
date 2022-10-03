5e1f5d87c708bebdb310a7157c88adf5a162dede
John Fletcher <john@bunbury28.plus.com>
2022-10-03 18:31:36 +0100

    Update RS485TransmitData.ino
    Add a comment

--- ArduinosConnectedTogether/RS485Examples/DFrobot/RS485TransmitData/RS485TransmitData.ino
+++ ArduinosConnectedTogether/RS485Examples/DFrobot/RS485TransmitData/RS485TransmitData.ino
@@ -1,5 +1,7 @@
 // DFR0219 example code to transmit data
 
+// With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
+
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
 
