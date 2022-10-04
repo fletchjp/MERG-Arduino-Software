da274317d8899fd12db9486d038cd40ac765ee34
John Fletcher <john@bunbury28.plus.com>
2022-10-04 11:23:35 +0100

    Update RS485ReceivingDataWriter.ino
    Build code

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485ReceivingDataWriter/RS485ReceivingDataWriter.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485ReceivingDataWriter/RS485ReceivingDataWriter.ino
@@ -1,4 +1,14 @@
-// DFR0219 example code to receive data
+/// RS485ReceivingDataWriter.ino
+
+// DFR0219 example code to receive data and send output to I2C.
+
+// Make I2C optional to take it out for production code.
+#define WRITE_I2C
+
+#ifdef WRITE_I2C
+#include <Wire.h>
+#define I2C_DEVICE 4  // Chose a device no.
+#endif
 
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
@@ -7,6 +17,10 @@ int val;
 
 void setup() {
   // put your setup code here, to run once:
+#ifdef WRITE_I2C
+  Wire.begin(); // join i2c bus (address optional for master)
+  Wire.write("RS485 Transmit Data Writer active\n");
+#endif
   pinMode(ledPin, OUTPUT);
   pinMode(EN, OUTPUT);
   Serial.begin(19200);
@@ -14,6 +28,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+#ifdef WRITE_I2C
+  Wire.beginTransmission(I2C_DEVICE); // transmit to device #I2C_DEVICE
+#endif
   digitalWrite(EN, LOW); // Enable receiving data
   val = Serial.read();
   if (-1 != val) {
@@ -22,6 +39,10 @@ void loop() {
       delay(500);
       digitalWrite(ledPin, LOW);
       delay(500);
+#ifdef WRITE_I2C
+      Wire.write('A');
+      Wire.endTransmission();    // stop transmitting
+#endif 
     }
   }
 }
