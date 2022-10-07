f037459abd69154ec98b56b6771f9b6ede0e4946
John Fletcher <john@bunbury28.plus.com>
2022-10-07 23:09:34 +0100

    Update RS485AutoDataWriter.ino
    Add flashing of the LED pin

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485AutoDataWriter/RS485AutoDataWriter.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485AutoDataWriter/RS485AutoDataWriter.ino
@@ -14,6 +14,8 @@
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
 
+// Add flashing of LED pin.
+int ledPin = 13;
 
 Auto485  bus(EN); // Single pin version
 
@@ -41,5 +43,9 @@ void loop() {
     }
     bus.println(); // mode -> receive.
   }
-  delay(1000);
+  //delay(1000);
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
 }
