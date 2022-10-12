050ae622d7efa1af7da5f3b4ca2aa72db266d15b
John Fletcher <john@bunbury28.plus.com>
2022-10-12 23:34:25 +0100

    Update RS485TransmitDataTM.ino
    Start to build code

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
@@ -4,18 +4,40 @@
 
 // With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
 
+#include <TaskManagerIO.h>
+
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
 
 // Add flashing of LED pin.
-int ledPin = 13;
+const int ledPin = 13;
 
+enum { LED_off, LED_on } Led_State;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(EN, OUTPUT);
   Serial.begin(19200);
 }
+
+class RS485_transmit : public BaseEvent {
+  int blinker_pin;
+  public:
+    int taskId;
+    RS485_transmit(int pin) : blinker_pin(pin)
+    { 
+        taskId = TASKMGR_INVALIDID;
+    }
+    uint32_t timeOfNextCheck() override {  
+       return 100UL * 1000UL; // every 15 milliseconds we increment
+    }  
+    void exec() override {
+      
+    }
+    ~RS485_transmit() override = default;
+};
+
+RS485_transmit transmit(ledPin);
 
 void loop() {
   // put your main code here, to run repeatedly:
