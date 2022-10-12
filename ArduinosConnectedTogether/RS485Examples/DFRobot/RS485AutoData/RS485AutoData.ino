f4a9c577139e378a284bb1c07ae08b9cf39eb5fe
John Fletcher <john@bunbury28.plus.com>
2022-10-12 15:49:11 +0100

    Update RS485AutoData.ino
    Modified to separate TRANSMIT and receive versions.

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485AutoData/RS485AutoData.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485AutoData/RS485AutoData.ino
@@ -1,6 +1,7 @@
 // DFR0219 example code to transmit and receive data using Auto485
 
 // This is running as a transmitter.
+//#define TRANSMIT
 
 #include <Auto485.h>
 // I have made a small change to the header file to avoid a warning from the Arduino compiler.
@@ -10,6 +11,7 @@ int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 
 // Add flashing of LED pin.
 int ledPin = 13;
+int val;
 
 Auto485  bus(EN); // Single pin version
 
@@ -23,15 +25,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //digitalWrite(EN, HIGH); // Enable data transmit
+#ifdef TRANSMIT
   bus.print('A');
-  if (bus.available() > 0 ) {
-    while (bus.available() > 0 )
-      bus.write(bus.read()); // mode -> transmit
-    bus.println(); // mode -> receive.
-  }
-  //delay(1000);
   digitalWrite(ledPin, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
   delay(500);
+#else
+  if (bus.available() > 0 ) {
+     val = bus.read();
+     if (-1 != val) {
+        if ('A' == val) {
+          digitalWrite(ledPin, HIGH);
+          delay(500);
+          digitalWrite(ledPin, LOW);
+          delay(500);
+        }
+     }
+  }
+#endif
+//  if (bus.available() > 0 ) {
+//    while (bus.available() > 0 ) {
+//      bus.write(bus.read()); // mode -> transmit
+//    }  
+//    bus.println(); // mode -> receive.
+//  }
 }
