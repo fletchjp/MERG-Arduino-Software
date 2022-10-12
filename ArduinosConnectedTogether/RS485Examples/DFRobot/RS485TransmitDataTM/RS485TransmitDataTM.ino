3e465f767441fb2b26d77604301f66a11de7fcbc
John Fletcher <john@bunbury28.plus.com>
2022-10-12 23:21:33 +0100

    Create RS485TransmitDataTM.ino
    New example for task management

---
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
@@ -0,0 +1,29 @@
+// RS485TransmitDataTM
+// Add task management using TaskManagerIO
+// DFR0219 example code to transmit data
+
+// With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
+
+int EN = 2;  // RS485 enable/disable pin for Rx/Tx
+// High to transmit, low to receive.
+
+// Add flashing of LED pin.
+int ledPin = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(EN, OUTPUT);
+  Serial.begin(19200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(EN, HIGH); // Enable data transmit
+  Serial.print('A');
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  //delay(1000);
+}
