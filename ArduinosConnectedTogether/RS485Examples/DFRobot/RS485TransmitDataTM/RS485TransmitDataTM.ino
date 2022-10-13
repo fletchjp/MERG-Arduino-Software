c45a994189dbe659af6f324157ed56099074b55e
John Fletcher <john@bunbury28.plus.com>
2022-10-13 12:16:29 +0100

    Update RS485TransmitDataTM.ino
    Version with task as a callback

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
@@ -1,5 +1,7 @@
 // RS485TransmitDataTM
 // Add task management using TaskManagerIO
+// I do not need the event class for this case.
+// See example RS485TransmitDataTMC for an event class
 // DFR0219 example code to transmit data
 
 // With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
@@ -14,38 +16,35 @@ const int ledPin = 13;
 
 enum { LED_off, LED_on } Led_State;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(EN, OUTPUT);
-  Serial.begin(19200);
-}
-
-class RS485_transmit : public BaseEvent {
-  int blinker_pin;
-  public:
-    int taskId;
-    RS485_transmit(int pin) : blinker_pin(pin)
-    { 
-        taskId = TASKMGR_INVALIDID;
-    }
-    uint32_t timeOfNextCheck() override {  
-       return 100UL * 1000UL; // every 15 milliseconds we increment
-    }  
-    void exec() override {
-      
-    }
-    ~RS485_transmit() override = default;
-};
-
-RS485_transmit transmit(ledPin);
-
-void loop() {
+void transmit()
+{
   // put your main code here, to run repeatedly:
   digitalWrite(EN, HIGH); // Enable data transmit
   Serial.print('A');
   digitalWrite(ledPin, HIGH);
-  delay(500);
+  delay(500); // I don't like the delay in here - think of something better.
   digitalWrite(ledPin, LOW);
-  delay(500);
+  digitalWrite(EN, LOW);// Enable data receive
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(EN, OUTPUT);
+  Serial.begin(19200);
+
+  // This is at the end of setup()
+  taskManager.scheduleFixedRate(1000,transmit);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  taskManager.runLoop();
+  //digitalWrite(EN, HIGH); // Enable data transmit
+  //Serial.print('A');
+  //digitalWrite(ledPin, HIGH);
+  //delay(500);
+  //digitalWrite(ledPin, LOW);
+  //delay(500);
   //delay(1000);
 }
