2a9c4d65e15307f9d6a12426c2990b4bc39cd8e5
John Fletcher <john@bunbury28.plus.com>
2022-10-13 18:23:14 +0100

    Update RS485ReceivingDataTM.ino
    Version in progress

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485ReceivingDataTM/RS485ReceivingDataTM.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485ReceivingDataTM/RS485ReceivingDataTM.ino
@@ -2,15 +2,25 @@
 // Add task management using TaskManagerIO
 // This needs to be an event driven task using a class
 // DFR0219 example code to receive data
+//////////////////////////////////////////////////////////////////////////////////// 
+// PSEUDOCODE
+// Set TASK_off
+// WHILE TASK_off IF data_available THEN START receive_data and set TASK_on
+// WHEN task is finished THEN set TASK_off
+// WHILE TASK_on THEN any more tasks. (not needed)
+
+///////////////////////////////////////////////////////////////////////////////////
 
 #include <TaskManagerIO.h>
 
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
 int ledPin = 13;
-int val;
+int value, save_value;
 
 enum { LED_off, LED_on } Led_State;
+
+enum { TASK_off, TASK_on} Task_State;
 
 class RS485_receive : public BaseEvent {
   int blinker_pin;
@@ -24,10 +34,21 @@ class RS485_receive : public BaseEvent {
        return 100UL * 1000UL; // every 15 milliseconds we increment
     }  
     void exec() override {
-      
+        if ('A' == saved_value) {
+           if (Led_State == LED_off) {
+              digitalWrite(ledPin, HIGH);
+              Led_State = LED_on;
+              Task_State = TASK_running;
+           } else {
+              digitalWrite(ledPin, LOW);
+              Led_State = LED_off;
+              Task_State = TASK_off;
+           }
+           Task_State = TASK_off;
+       }
     }
-    ~RS485_transmit() override = default;
-};
+    ~RS485_receive() override = default;
+};  
 
 RS485_receive receive_data(ledPin);
 
@@ -38,19 +59,20 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   Led_State = LED_off;
+  Task_State = TASK_off;
+  saved_value = 'X';
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   taskManager.runLoop();
   digitalWrite(EN, LOW); // Enable receiving data
-  val = Serial.read();
-  if (-1 != val) {
-    if ('A' == val) {
-      digitalWrite(ledPin, HIGH);
-      delay(500);
-      digitalWrite(ledPin, LOW);
-      delay(500);
-    }
+  if (Task_State == TASK_off) {
+      value = Serial.read();
+      if (-1 != value) {
+        saved_value = value;
+        Task_State = TASK_on;
+        taskManager.registerEvent(&receive_data);
+      } 
   }
 }
