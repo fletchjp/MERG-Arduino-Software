aab5827832023a60cec8f502fa7eb46ce4ce8f25
John Fletcher <john@bunbury28.plus.com>
2022-10-31 21:58:27 +0000

    Update ArduinoSTLVector.ino
    This test works

--- ArduinoSTL/ArduinoSTLVector/ArduinoSTLVector.ino
+++ ArduinoSTL/ArduinoSTLVector/ArduinoSTLVector.ino
@@ -12,6 +12,9 @@ void setup() {
   Serial.begin(115200); 
   printf("STL Vector\n");
   for (int i = 0; i < 5; i++) trial.push_back(i);
+  // range-based for loop
+  for (auto i : trial) cout << i << " ";
+  cout << endl;
 }
 
 void loop() {
