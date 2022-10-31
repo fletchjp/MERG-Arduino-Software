4edca96b0587b2f03e4b9751c727b29420af44d1
John Fletcher <john@bunbury28.plus.com>
2022-10-31 23:33:23 +0000

    Update ArduinoSTLComposite.ino
    working code

--- ArduinoSTL/ArduinoSTLComposite/ArduinoSTLComposite.ino
+++ ArduinoSTL/ArduinoSTLComposite/ArduinoSTLComposite.ino
@@ -12,24 +12,38 @@ vector<int> trial;
 
 class Component {
 public:
-   virtual void print() const = 0;
+   virtual void print_() const = 0;
    virtual ~Component() { }  
 };
 
 class Composite : public Component {
   vector<const Component*> children;
-  const string& component_name;
+  const string component_name;
 public:
   explicit Composite(const string& n) : component_name(n) { }
-  void print() const override {
+  void print_() const override {
     cout << component_name << " ";
-    for (auto c: children) c->print();
+    for (auto c: children) c->print_(); 
   }
   void add(const Component* component) {
     children.push_back(component);
   }
   void remove(const Component* component) {
-    children.erase(component);
+    // erase(children, component); Not available in C++11
+    // There is children.erase(location) which needs to know where the component is.
+    for (int i = 0; i < children.size(); i++) {
+      if (children.at(i) == component) { children.erase(children.begin() + i); break; }
+    } 
+  }
+};
+
+class Leaf : public Component {
+private:
+  const string leaf_name;
+public:
+  explicit Leaf(const string& n) : leaf_name(n) { }
+  void print_() const override {
+    cout << leaf_name << " ";
   }
 };
 
@@ -40,6 +54,28 @@ void setup() {
   // range-based for loop
   for (auto i : trial) cout << i << " ";
   cout << endl;
+  cout << "Test of Composite Pattern" << endl;
+
+  Composite composite("composite");
+  Composite composite1("composite1");
+  composite.print_();
+  Leaf leaf1("leaf1");
+  Leaf leaf2("leaf2");
+  Leaf leaf3("leaf3");
+  leaf1.print_();
+  cout << endl;
+  composite1.add(&leaf1);
+  composite1.add(&leaf2);
+
+  composite.add(&composite1);
+  composite.add(&leaf3);
+
+  composite1.print_();
+  cout << endl;
+
+  composite.print_();
+  cout << endl;
+  
 }
 
 void loop() {
