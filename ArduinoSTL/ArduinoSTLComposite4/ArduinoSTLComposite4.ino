830459a7e4d90bfc071db93b5993dac577c620c0
John Fletcher <john@bunbury28.plus.com>
2022-11-01 14:31:26 +0000

    Another new example

---
+++ ArduinoSTL/ArduinoSTLComposite4/ArduinoSTLComposite4.ino
@@ -0,0 +1,58 @@
+// Arduino STL Composite4
+// Modified from modernescpp.com example.
+// This example is now working.
+// I think there is quite a lot of promise in having this structure available.
+// Adding some things from refactoring.guru design
+
+#include <ArduinoSTL.h>
+
+#include <vector>
+//#include <algorithm>
+#include <string>
+
+#include "composite.h"
+
+using namespace std;
+
+vector<int> trial;
+
+
+
+void setup() {
+  Serial.begin(115200); 
+  printf("STL Composite Pattern\n");
+  for (int i = 0; i < 5; i++) trial.push_back(i);
+  // range-based for loop
+  for (auto i : trial) cout << i << " ";
+  cout << endl;
+  cout << "Test of Composite Pattern" << endl;
+
+  Composite composite("composite");
+  Composite composite1("composite1");
+  composite.print_();
+  Leaf leaf1("leaf1");
+  Leaf leaf2("leaf2");
+  Leaf leaf3("leaf3");
+  leaf1.print_();
+  cout << endl;
+  composite1.add(&leaf1);
+  composite1.add(&leaf2);
+
+  composite.add(&composite1);
+  composite.add(&leaf3);
+
+  composite1.print_();
+  cout << endl;
+
+  composite.print_();
+  cout << endl;
+
+  composite.remove(&composite1);
+  
+  composite.print_();
+  cout << endl;
+}
+
+void loop() {
+  
+}
---
+++ ArduinoSTL/ArduinoSTLComposite4/composite.h
@@ -0,0 +1,48 @@
+// composite.h
+// First try at a composite header to go in a library
+// Change vector to list when remove does work.
+
+
+#ifndef COMPOSITE_H
+#define COMPOSITE_H
+
+#include <list>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Component {
+public:
+   virtual void print_() const = 0;
+   virtual ~Component() { }  
+};
+
+class Composite : public Component {
+  list<const Component*> children;
+  const string component_name;
+public:
+  explicit Composite(const string& n) : component_name(n) { }
+  void print_() const override {
+    cout << component_name << " ";
+    for (auto c: children) c->print_(); 
+  }
+  void add(const Component* component) {
+    children.push_back(component);
+  }
+  void remove(const Component* component) {
+    children.remove(component);
+  }
+};
+
+class Leaf : public Component {
+private:
+  const string leaf_name;
+public:
+  explicit Leaf(const string& n) : leaf_name(n) { }
+  void print_() const override {
+    cout << leaf_name << " ";
+  }
+};
+
+#endif
