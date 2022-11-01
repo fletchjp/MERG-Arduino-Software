0c0ed694ba0c6f35c52ab559dd2745511b251d27
John Fletcher <john@bunbury28.plus.com>
2022-11-01 21:26:10 +0000

    Signal Composite
    Based on Arduino STL Composite5

---
+++ Signals/SignalComposite/SignalComposite/SignalComposite.ino
@@ -0,0 +1,74 @@
+// SignalComposite
+// Based on Arduino STL Composite5
+// Modified from modernescpp.com example.
+// This example is now working.
+// I think there is quite a lot of promise in having this structure available.
+// Adding some things from refactoring.guru design
+// Adding support for multiple parents.
+
+#include <ArduinoSTL.h>
+
+#include <vector>
+//#include <algorithm>
+#include <string>
+
+#include "composite.h"
+
+using namespace std;
+
+vector<int> trial;
+
+
+
+void setup() {
+  Serial.begin(115200); 
+  printf("STL Composite Pattern Version 5 - refactoring.guru\n");
+  for (int i = 0; i < 5; i++) trial.push_back(i);
+  // range-based for loop
+  for (auto i : trial) cout << i << " ";
+  cout << endl;
+  cout << "Test of Composite Pattern with multiple parents" << endl;
+
+  Composite composite("composite");
+  Composite composite1("composite1");
+  composite.print_();
+  Leaf leaf1("leaf1");
+  Leaf leaf2("leaf2");
+  Leaf leaf3("leaf3");
+  Leaf leaf4("leaf4");
+  leaf1.print_();
+  cout << endl;
+  composite1.Add(&leaf1);
+  composite1.Add(&leaf2);
+  composite.Add(&composite1);
+  composite.Add(&leaf3);
+  composite1.Add(&leaf3);
+
+  composite1.print_();
+  cout << endl;
+
+  composite.print_();
+  cout << endl;
+
+  cout << composite.Operation() << endl;
+
+  cout << leaf3.GetName() << " has parent(s) " << leaf3.GetParentNames() << endl;
+
+  composite.Remove(&composite1);
+  
+  composite.print_();
+  cout << endl;
+
+  
+  composite1.Remove(&leaf3);
+  cout << leaf3.GetName() << " has parent(s) " << leaf3.GetParentNames() << endl;
+
+  cout << leaf4.GetName() << " has parent(s) " << leaf4.GetParentNames() << endl;
+
+  cout << composite.GetName() << " has parent(s) " << composite.GetParentNames() << endl;
+
+}
+
+void loop() {
+  
+}
