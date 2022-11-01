fa013253dd9272894c49abb3132df0dc81519e08
John Fletcher <john@bunbury28.plus.com>
2022-11-01 16:16:09 +0000

    Another new example

---
+++ ArduinoSTL/ArduinoSTLComposite5/ArduinoSTLComposite5.ino
@@ -0,0 +1,61 @@
+// Arduino STL Composite4
+// Modified from modernescpp.com example.
+// This example is now working.
+// I think there is quite a lot of promise in having this structure available.
+// Adding some things from refactoring.guru design
+
+#include <ArduinoSTL.h>
+
+#include <vector>
+//#include <algorithm>
+#include <string>
+
+#include "composite.h"
+
+using namespace std;
+
+vector<int> trial;
+
+
+
+void setup() {
+  Serial.begin(115200); 
+  printf("STL Composite Pattern Version 4 - refactoring.guru\n");
+  for (int i = 0; i < 5; i++) trial.push_back(i);
+  // range-based for loop
+  for (auto i : trial) cout << i << " ";
+  cout << endl;
+  cout << "Test of Composite Pattern " << endl;
+
+  Composite composite("composite");
+  Composite composite1("composite1");
+  composite.print_();
+  Leaf leaf1("leaf1");
+  Leaf leaf2("leaf2");
+  Leaf leaf3("leaf3");
+  leaf1.print_();
+  cout << endl;
+  composite1.Add(&leaf1);
+  composite1.Add(&leaf2);
+  composite.Add(&composite1);
+  composite.Add(&leaf3);
+
+  composite1.print_();
+  cout << endl;
+
+  composite.print_();
+  cout << endl;
+
+  cout << composite.Operation() << endl;
+
+  cout << leaf3.GetName() << " has parent " << leaf3.GetParent()->GetName() << endl;
+
+  composite.Remove(&composite1);
+  
+  composite.print_();
+  cout << endl;
+}
+
+void loop() {
+  
+}
---
+++ ArduinoSTL/ArduinoSTLComposite5/composite.h
@@ -0,0 +1,94 @@
+// composite.h
+// First try at a composite header to go in a library
+// Change vector to list when remove does work.
+// Adding some things from refactoring.guru design
+
+// This allows the setting of information on one parent.
+// There are other differences.
+// I have kept the print_ operations although this also allows for the return of a string.
+
+
+#ifndef COMPOSITE_H
+#define COMPOSITE_H
+
+#include <list>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Component {
+protected:
+   Component *parent_;
+public:
+   virtual ~Component() { }
+   void SetParent(Component *parent) {
+    this->parent_ = parent;
+   }
+   Component *GetParent() const {
+    return this->parent_;
+   }
+   virtual void Add(/* const */ Component* component) { }
+   virtual void Remove(/* const */ Component* component) { }
+   virtual bool IsComposite() const {
+    return false;
+   }
+   virtual string Operation() const = 0;
+   virtual string GetName() const = 0;
+   virtual void print_() const = 0;
+};
+
+class Composite : public Component {
+  const string component_name;
+protected:
+  list<const Component*> children_;
+public:
+  explicit Composite(const string& n) : component_name(n) { }
+  void print_() const override {
+    cout << component_name << " ";
+    for (auto c: children_) c->print_(); 
+  }
+  void Add(/* const */ Component* component) {
+    this->children_.push_back(component);
+    component->SetParent(this);
+  }
+  void Remove(/* const */ Component* component) {
+    children_.remove(component);
+    component->SetParent(nullptr);
+  }
+   virtual bool IsComposite() const {
+    return true;
+   }
+  string GetName() const {
+    return component_name;
+  }
+  string Operation() const override {
+    string result;
+    for (const Component *c: children_) {
+      if (c == children_.back() ) {
+        result += c->Operation();
+      } else {
+        result += c->Operation() + "+";       
+      }
+    }
+    return "Branch(" + result + ")";
+  }
+};
+
+class Leaf : public Component {
+private:
+  const string leaf_name;
+public:
+  explicit Leaf(const string& n) : leaf_name(n) { }
+  string Operation() const override {
+    return leaf_name;
+  }
+  string GetName() const {
+    return leaf_name;
+  }
+  void print_() const override {
+    cout << leaf_name << " ";
+  }
+};
+
+#endif
