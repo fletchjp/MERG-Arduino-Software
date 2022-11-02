88d204339c53432003c98696139820663fa3e5a6
John Fletcher <john@bunbury28.plus.com>
2022-11-02 23:47:47 +0000

    Create ArduinoSTLObserver2.ino
    New example

---
+++ ArduinoSTL/ArduinoSTLObserver2/ArduinoSTLObserver2.ino
@@ -0,0 +1,14 @@
+// Arduino STL Observer2
+// Example from codereview.stackexchange.com
+
+
+#include <ArduinoSTL.h>
+
+void setup() {
+  Serial.begin(9600); 
+  printf("Hello World\n");
+}
+
+void loop() {
+  
+}
