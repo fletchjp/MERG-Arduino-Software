47caabb367384e851018e8c0d7ee2f7bd224b98c
John Fletcher <john@bunbury28.plus.com>
2022-11-02 16:35:47 +0000

    Update ArduinoSTLObserver.ino
    Announce intention

--- ArduinoSTL/ArduinoSTLObserver/ArduinoSTLObserver.ino
+++ ArduinoSTL/ArduinoSTLObserver/ArduinoSTLObserver.ino
@@ -1,9 +1,11 @@
-// Arduino STL Hello
+// Arduino STL Observer
+// Bringing in previous code for the Observer Pattern
+// Starting from observer2_callable.cpp NOT using the FC++ version.
 
 #include <ArduinoSTL.h>
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(115200); 
   printf("Hello World\n");
 }
 
