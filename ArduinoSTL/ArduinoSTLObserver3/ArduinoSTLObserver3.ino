303ed8e59c907b7ded6d812cae81774559a475a8
John Fletcher <john@bunbury28.plus.com>
2022-11-03 11:51:46 +0000

    Create ArduinoSTLObserver3.ino
    New example

---
+++ ArduinoSTL/ArduinoSTLObserver3/ArduinoSTLObserver3.ino
@@ -0,0 +1,122 @@
+// Arduino STL Observer2
+// Example from coderevi3w.stackexchange.com
+// Also see further down in the comments.
+// I am going to make Observer2 the original version
+// This is the modification.
+
+
+#include <ArduinoSTL.h>
+
+#include <list>
+#include <queue>
+#include <map>
+
+// Example code was missing this.
+class Subject;
+// and this.
+class Event { };
+
+class Observer 
+{
+public:
+    virtual ~Observer() {}
+    virtual void onNotify(Subject *entity, Event event) = 0;
+private:
+
+};
+
+class Subject
+{
+private:
+    std::list<Observer*> _observers;
+    std::queue<std::list<Observer*>::iterator> _eraseQueue;
+public:
+    Subject() {}
+    virtual ~Subject() {}
+    // Return true if observer is added.
+    bool addObserver(Observer* observer)
+    {
+      if (std::find(_observers.begin(), _observers.end(), observer) ==  _observers.end())
+      {
+        _observers.push_back(observer);
+        return true;
+      } else return false;
+    }
+    void removeObserver(Observer* observer)
+    {
+      std::list<Observer*>::iterator it = std::find(_observers.begin(), _observers.end(), observer);
+      if (it != _observers.end())
+      {
+        *it = NULL;
+        _eraseQueue.push(it);
+      }
+    }
+protected:
+    void notify(Subject *entity, Event event)
+    {
+      for (std::list<Observer*>::iterator it = _observers.begin(); it != _observers.end(); it++)
+      {
+       if (*it != NULL)
+         (*it)->onNotify(entity,event);
+      }
+      while (!_eraseQueue.empty()) {
+        _observers.erase(_eraseQueue.front());
+        _eraseQueue.pop();
+      }
+    }
+    void notify(Subject *entity, Event event, Observer* observer)
+    {
+      if (observer != NULL)
+         observer->onNotify(entity,event);
+    }
+    
+};
+
+template <typename T>
+class EventHandler : public Observer
+{
+public:
+    virtual ~EventHandler() {}
+    virtual void onNotify(Subject *entity, Event event)
+    {
+      if (dynamic_cast<T*>(this))
+      {
+         auto it = _actions.find(event);
+         if (it != _actions.end())
+         {
+             (dynamic_cast<T*>(this)->*(it->second))(entity);
+         }
+      }
+    }
+protected:
+    template <typename U>
+    U safe_cast(Subject* entity)
+    {
+        if (dynamic_cast<U>(entity))
+        {
+            return (dynamic_cast<U>(entity));
+        } else {
+          std::cout <<"Non matching entity" << std::endl;
+        }
+    }
+    std::map<const Event, void (T::*)(Subject *)> _actions;
+};
+
+// His example is incomplete and not relevant.
+
+// What is useful is that he can use the map to store different actions by defining events.
+// 
+// _actions [EVENT_TYPE] = & member function to be called
+//
+// see the example code.
+
+
+void setup() {
+  Serial.begin(115200); 
+  printf("Hello Observer2 World\n");
+  printf("no working example yet\n");
+}
+
+void loop() {
+  
+}
