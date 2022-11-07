82837191e5a41a74f087fae00f4e63405f1a868a
John Fletcher <john@bunbury28.plus.com>
2022-11-07 17:43:12 +0000

    Create i2c_scanner_DUE.ino
    New example

---
+++ TestPrograms/i2c_scanner_DUE/i2c_scanner_DUE.ino
@@ -0,0 +1,65 @@
+// --------------------------------------
+// i2c_scanner
+//
+// Modified from https://playground.arduino.cc/Main/I2cScanner/
+// --------------------------------------
+
+#include <Wire.h>
+
+// Set I2C bus to use: Wire, Wire1, etc.
+// Wire1 on DUE
+#define WIRE Wire1
+
+void setup() {
+  WIRE.begin();
+
+  Serial.begin(115200);
+  while (!Serial)
+     delay(10);
+  Serial.println("\nI2C Scanner");
+  //Wire.setClock(50000);
+}
+
+
+void loop() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmission to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else {
+    Serial.print(nDevices);
+    Serial.println(" devices found - done\n");
+  }
+
+  delay(5000);           // wait 5 seconds for next scan
+}
