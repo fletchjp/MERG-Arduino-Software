f1399ce67923f8710abba729659ee35220a8fcaa
John Fletcher <john@bunbury28.plus.com>
2022-11-10 14:25:17 +0000

    Create AT24C256_read_write_Wire1.ino
    New example

---
+++ TestPrograms/AT24C256_read_write_Wire1/AT24C256_read_write_Wire1.ino
@@ -0,0 +1,33 @@
+// AT24C256 read write example
+// copied from the AT24C256 library example
+
+#include <AT24C256.h>
+
+AT24C256 eeprom(0x50);
+
+
+
+void setup() {
+  
+  char message[30];
+  char writemessage[] = "Hello AT24C256 World!";
+  
+  Serial.begin(115200);
+  Serial.println("AT24C256 library example");
+  eeprom.begin();
+  Serial.println("reading eeprom");
+  eeprom.read(0, (uint8_t*) message, sizeof(message));
+  Serial.println(message);
+
+  eeprom.write(0, (uint8_t*)writemessage, sizeof(message));
+  Serial.println("wrote eeprom");
+  
+  Serial.println("reading eeprom");
+  eeprom.read(0, (uint8_t*) message, sizeof(message));
+  Serial.println(message);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
