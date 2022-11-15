903fab2e5677c4328a993e6ce4432d9afe3095fe
John Fletcher <john@bunbury28.plus.com>
2022-11-15 23:08:52 +0000

    Update DUESignalCombined2.ino
    Correct typos

--- DUESignalCombined2/DUESignalCombined2.ino
+++ DUESignalCombined2/DUESignalCombined2.ino
@@ -7,9 +7,9 @@
 // This combines the Composite and Observer Patterns.
 // All objects declared are Components, Subjects and Observers.
 // Components can be either Composite or Leaf.
-// Composite components can have other items (Composite or Leaf) as chilren using Add(&child)
+// Composite components can have other items (Composite or Leaf) as children using Add(&child)
 // An composite can have more than one child and any object can have more than one parent.
-// Optionslly child.RegisterParents() makes the parents of the child into observers as well.
+// Optionally child.RegisterParents() makes the parents of the child into observers as well.
 // Any observer has to define handlers which will respond to particular integer values from 
 // the subjects being observed. The handlers are member functions and they are indexed using the integer value.
 // See the examples in classes Track and Section below.
