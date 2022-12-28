8666b5364ac9f94a3c3dccbdd2469865c590ef5a
John Fletcher <john@bunbury28.plus.com>
2022-12-28 15:54:48 +0000

    Update RS485ReceivingDataFreeRTOS.ino
    Update code for FreeRTOS

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485ReceivingDataFreeRTOS/RS485ReceivingDataFreeRTOS.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485ReceivingDataFreeRTOS/RS485ReceivingDataFreeRTOS.ino
@@ -17,6 +17,9 @@
 // This works for the first three or four switches and then the LED switches
 // on and off more quickly. Using a mutex around Task_State does not change things.
 ///////////////////////////////////////////////////////////////////////////////////
+// This is now translated to use the FreeRTOS interface
+// As a first case this always flashes.
+////////////////////////////////////////////////////////////////////////////////// 
 
 #include <Arduino_FreeRTOS.h>
 
@@ -32,129 +35,16 @@ enum { TASK_off, TASK_on} Task_State;
 void TaskSwitch  (void *pvParameters);
 void TaskReceive (void *pvParameters);
 
-/*
-namespace
-{
-	// An anonymous namespace tells the linker that this code is only
-	// referenced in this very compilation unit. That's good for larger
-	// projects but not necessary for this sketch.
-
-	// Define the tasks and implement run() for each.
-	// - final is optional but good practice
-	// - run() must be public
-	// - sleeping with remainder is optional
-	// - inlining run() is optional
-
-  //struct Data {
-  //   bool task_on;
-  //   bool led_on;     
-  //   unsigned long timestamp;
-  //};
-
-  //frt::Queue<Data,10> queue;
-
-  frt::Mutex task_mutex, led_mutex;
-  // There is a very strange relationship between the stack size and crashing.
-  // The default is 192U and it crashes after about 4 iterations.
-  // The crash is delayed for smaller stack size and earlier for larger stack size.
-  // After a crash the LED blinks regardless of tasks.
-  // This does not happen when the task is run from the loop().
-  // My guess is that the memory used for the stack is not being freed
-  // and the Arduino is running out of memory!!!
-  // I am looking into how I could free the memory.
-  class SwitchTask final :
-	public frt::Task<SwitchTask,32U> 
-  {
-    public:
-    bool run() {
-       //Data data;
-       //if(Led_State == LED_off) {
-         switch_on();
-  		   msleep(500, remainder);
-         switch_off();
-      //} else {
-        // switch_off();
-   		  // msleep(500, remainder);
-        // switch_on();
-      //}
-      //task_mutex.lock();
-      //Task_State = TASK_off;
-      //task_mutex.unlock();
-      //data.task_on = false;
-      //data.led_on = (Led_State == LED_on);
-      //data.timestamp = millis();
-      //queue.push(data);
-      return false; // Run once only.
-    }
-
-    void switch_off() {
-      digitalWrite(ledPin, LOW);
-      Led_State = LED_off;   
-    }
-
-    void switch_on() {
-      digitalWrite(ledPin, HIGH);
-      Led_State = LED_on;
-    }
-
-  private:
-		unsigned int remainder = 0;
-
-  };
-
-  SwitchTask switch_task;
-
-  class ReceiveTask final :
-	public frt::Task<ReceiveTask>
-  {
-    public:
-    bool run() {
-      digitalWrite(EN, LOW); // Enable receiving data
-      //task_mutex.lock();
-      if (!switch_task.isRunning()) {
-      //if (Task_State == TASK_off) {
-        value = Serial.read();
-        if (-1 != value) {
-          if ('A' == value) {
-           //task_mutex.unlock();
-            if (!switch_task.isRunning()) {
-              switch_task.start(2);
-              Task_State = TASK_on;
-            }
-          }
-        }
-      }
-      if (!switch_task.isRunning())
-        Task_State = TASK_off;
-      //task_mutex.unlock();
-      return true;
-    }
-  private:
-		unsigned int remainder = 0;
-  };
-
-  ReceiveTask receive_task;
-
-}
-*/
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(EN, OUTPUT);
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
-  //digitalWrite(ledPin, HIGH);
-  //delay(1000);
-  //digitalWrite(ledPin, LOW);
-  //delay(500);
-  //digitalWrite(ledPin, HIGH);
-  //delay(500);
   digitalWrite(ledPin, LOW);
   Led_State = LED_off;
   Task_State = TASK_off;
-  //receive_task.start(1);
   xTaskCreate(TaskSwitch, "Switch", 128, NULL, 2, NULL);
-  xTaskCreate(TaskReceive. "Receive", 128, NULL, 1, NULL);
+  xTaskCreate(TaskReceive, "Receive", 128, NULL, 1, NULL);
 }
 
 void loop() {
