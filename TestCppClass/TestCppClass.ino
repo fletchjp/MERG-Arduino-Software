7a23cbfee652b391347df0fdee1c8cb3cbe8ef9c
John Fletcher <john@bunbury28.plus.com>
2023-01-25 23:40:13 +0000

    Create TestCppClass.ino
    New example

---
+++ TestCppClass/TestCppClass.ino
@@ -0,0 +1,12 @@
+/// TestCppClass.ino Testing of C++ class rules for copying etc
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
