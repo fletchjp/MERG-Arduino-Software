89c09d96adfe5a8b09218f6af494d6383b1e0f5e
John Fletcher <john@bunbury28.plus.com>
2023-01-25 23:47:41 +0000

    Update TestCppClass.ino
    Add some information

--- TestCppClass/TestCppClass.ino
+++ TestCppClass/TestCppClass.ino
@@ -1,5 +1,15 @@
 /// TestCppClass.ino Testing of C++ class rules for copying etc
 
+/// Rule of Zero or Six
+
+/* Default operations for class X
+  default constructor  X()
+  copy constructor X(const X&)
+  copy assignment operator=(const X&)
+  move constructor  X(X&&)
+  move assignment operator=(X&&)
+  destructor ~X()
+*/
 
 void setup() {
   // put your setup code here, to run once:
