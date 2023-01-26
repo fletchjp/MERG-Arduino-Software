fa1da010f9b930d6fb6b90f22bef0a64a9ef4f6e
John Fletcher <john@bunbury28.plus.com>
2023-01-26 22:59:47 +0000

    Create Schroedinger.ino
    New example

---
+++ QuantumMechanics/Schroedinger/Schroedinger.ino
@@ -0,0 +1,18 @@
+/// Schroedinger.ino
+/// Using the Numerov method.
+/// Buffalo dept of physics.
+
+/// Tridiagonal may come later.
+/// The references do not seem to use this method.
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
