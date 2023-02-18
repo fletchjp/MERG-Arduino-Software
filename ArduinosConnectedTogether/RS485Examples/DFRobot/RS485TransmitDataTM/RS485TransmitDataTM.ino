e804c54d00e09b3cf11ee0ec3a052ae6c1862bff
John Fletcher <john@bunbury28.plus.com>
2023-02-18 15:04:00 +0000

    Update RS485TransmitDataTM.ino
    Add heading code

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTM/RS485TransmitDataTM.ino
@@ -10,7 +10,20 @@
 
 // With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
 
+// 3rd party libraries
+#include <Streaming.h>
 #include <TaskManagerIO.h>
+
+const byte VER_MAJ  = 1;
+const byte VER_MIN  = 0;
+const byte VER_DETAIL = 0;
+
+void heading()
+{
+  Serial << endl << endl << __FILE__ << endl;
+  Serial << F("Ver: ") << VER_MAJ << F(".") << VER_MIN << F(".") << VER_DETAIL;
+  Serial << F(" compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(" using compiler ") << __cplusplus << endl;
+}
 
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
@@ -41,8 +54,10 @@ void transmit()
 
 void setup() {
   // put your setup code here, to run once:
+  while (!Serial);
   pinMode(EN, OUTPUT);
   Serial.begin(115200);
+  heading();
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   Led_State = LED_off;
