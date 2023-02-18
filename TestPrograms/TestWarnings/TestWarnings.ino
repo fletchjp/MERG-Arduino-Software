3f90d354a510b6fd2ed9bcf48ab3f4f0ffe620a5
John Fletcher <john@bunbury28.plus.com>
2023-02-18 13:00:04 +0000

    Update TestWarnings.ino
    Add heading code

--- TestPrograms/TestWarnings/TestWarnings.ino
+++ TestPrograms/TestWarnings/TestWarnings.ino
@@ -12,11 +12,22 @@
 // 3rd party libraries
 #include <Streaming.h>
 
+const byte VER_MAJ  = 1;
+const byte VER_MIN  = 0;
+const byte VER_DETAIL = 0;
+
+void heading()
+{
+ Serial << endl << endl << __FILE__ << endl;
+  Serial << F("Ver: ") << VER_MAJ << F(".") << VER_MIN << F(".") << VER_DETAIL;
+  Serial << F(" compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(" using compiler ") << __cplusplus << endl;
+}
+
 void setup() {
   // put your setup code here, to run once:
   while(!Serial);
   Serial.begin (115200);
-  Serial << endl << endl << F("> ** TestWarnings ** ") << __FILE__ << endl;
+  heading();
 }
 
 bool return_value_missing()
