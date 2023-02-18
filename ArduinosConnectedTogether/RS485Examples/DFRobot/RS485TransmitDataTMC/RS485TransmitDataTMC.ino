0f2f3f10ae852ae14ab0c4d0253fade19ac97c1a
John Fletcher <john@bunbury28.plus.com>
2023-02-18 15:12:54 +0000

    Update RS485TransmitDataTMC.ino
    Add header code

--- ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTMC/RS485TransmitDataTMC.ino
+++ ArduinosConnectedTogether/RS485Examples/DFRobot/RS485TransmitDataTMC/RS485TransmitDataTMC.ino
@@ -4,7 +4,20 @@
 
 // With the board set to PROG the output goes to the Serial Monitor if the speed is set correctly.
 
+// 3rd party libraries
+#include <Streaming.h>
 #include <TaskManagerIO.h>
+
+const byte VER_MAJ  = 0;
+const byte VER_MIN  = 1;
+const byte VER_DETAIL = 0;
+
+void heading()
+{
+  Serial << endl << endl << __FILE__ << endl;
+  Serial << F("Ver: ") << VER_MAJ << F(".") << VER_MIN << F(".") << VER_DETAIL;
+  Serial << F(" compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(" using compiler ") << __cplusplus << endl;
+}
 
 int EN = 2;  // RS485 enable/disable pin for Rx/Tx
 // High to transmit, low to receive.
