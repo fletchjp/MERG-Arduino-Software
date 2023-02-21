408572cd55ea2f184669b70e34a1f851b688ad11
John Fletcher <john@bunbury28.plus.com>
2023-02-21 15:36:11 +0000

    Update FileHeading.ino
    Change file name in comments

--- TestPrograms/FileHeading/FileHeading.ino
+++ TestPrograms/FileHeading/FileHeading.ino
@@ -1,13 +1,6 @@
-/// @file TestWarnings.ino
-/// @brief Test code warnings.
+/// @file FileHeading.ino
+/// @brief File heading code to put in other codes.
 ///
-/// There are two problems.
-///
-/// One is that the Arduino IDE normal setting turns off all the warnings.
-///
-/// The other is that the Arduino IDE uses -fpermissive which means that some errors become warnings.
-/// 
-/// As warnings are turned off there are then no warnings of some errors.
 
 // 3rd party libraries
 #include <Streaming.h>
