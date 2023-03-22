52d04bf176fbdc2a0eddeac838e33c3f07a6f8ab
John Fletcher <john@bunbury28.plus.com>
2023-03-22 18:13:03 +0000

    Create FileHeadingBlink.ino
    New example - add blink to FileHeading

---
+++ TestPrograms/FileHeadingBlink/FileHeadingBlink.ino
@@ -0,0 +1,33 @@
+/// @file FileHeadingBlink.ino
+/// @brief File heading code to put in other codes with blink
+///
+
+// 3rd party libraries
+#include <Streaming.h>
+
+const byte VER_MAJ  = 1;
+const byte VER_MIN  = 0;
+const byte VER_DETAIL = 0;
+
+void heading()
+{
+  Serial << endl << endl << __FILE__ << endl;
+  Serial << F("Ver: ") << VER_MAJ << F(".") << VER_MIN << F(".") << VER_DETAIL;
+  Serial << F(" compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(" using compiler ") << __cplusplus << endl;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  while(!Serial);
+  Serial.begin (115200);
+  heading();
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second  
+}
