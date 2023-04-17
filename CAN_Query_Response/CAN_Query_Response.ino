d6c89281d88ceb51a2d85c130cc1c5579ace934c
John Fletcher <john@bunbury28.plus.com>
2023-04-17 11:52:09 +0100

    New CAN_Query_Response
    From CAN_Query

---
+++ CAN_Query_Response/CAN_Query_Response.ino
@@ -0,0 +1,589 @@
+// CAN_Query.ino
+// copied from CANWrapper
+// I am going to extend this to have a state machine
+// using the example in arduino_state_event_keypad as a basis.
+// That uses IoAbstraction and TaskManagerIO.
+// Code for responding is in CANASIGNAL 
+
+/*
+  Copyright (C) 2021 Martin Da Costa
+  Including copyrights from CBUS_1in1out and Arduino CBUS Libraries
+
+
+  This work is licensed under the:
+      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+   To view a copy of this license, visit:
+      http://creativecommons.org/licenses/by-nc-sa/4.0/
+   or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+   License summary:
+    You are free to:
+      Share, copy and redistribute the material in any medium or format
+      Adapt, remix, transform, and build upon the material
+
+    The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+    Attribution : You must give appropriate credit, provide a link to the license,
+                  and indicate if changes were made. You may do so in any reasonable manner,
+                  but not in any way that suggests the licensor endorses you or your use.
+
+    NonCommercial : You may not use the material for commercial purposes. **(see note below)
+
+    ShareAlike : If you remix, transform, or build upon the material, you must distribute
+                 your contributions under the same license as the original.
+
+    No additional restrictions : You may not apply legal terms or technological measures that
+                                 legally restrict others from doing anything the license permits.
+
+   ** For commercial use, please contact the original copyright holder(s) to agree licensing terms
+
+    This software is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+*/
+
+/*
+      3rd party libraries needed for compilation:
+
+      Streaming   -- C++ stream style output, v5, (http://arduiniana.org/libraries/streaming/)
+      ACAN2515    -- library to support the MCP2515/25625 CAN controller IC
+      CBUSSwitch  -- library access required by CBUS and CBUS Config
+      CBUSLED     -- library access required by CBUS and CBUS Config
+*/
+///////////////////////////////////////////////////////////////////////////////////
+// Pin Use map UNO:
+// Digital pin 2          Interupt CAN
+// Digital pin 3 (PWM)    Not Used
+// Digital pin 4          Not Used
+// Digital pin 5 (PWM)    Not Used
+// Digital pin 6 (PWM)    Not Used
+// Digital pin 7          Not Used
+// Digital pin 8          Not Used
+// Digital pin 9 (PWM)    Not Used
+// Digital pin 10 (SS)    CS    CAN
+// Digital pin 11 (MOSI)  SI    CAN
+// Digital pin 12 (MISO)  SO    CAN
+// Digital pin 13 (SCK)   Sck   CAN
+
+// Digital / Analog pin 0     Not Used
+// Digital / Analog pin 1     Not Used
+// Digital / Analog pin 2     Not Used
+// Digital / Analog pin 3     Not Used
+// Digital / Analog pin 4     Not Used
+// Digital / Analog pin 5     Not Used
+//////////////////////////////////////////////////////////////////////////
+
+
+#define ACCESSORY_REQUEST_EVENT 1
+
+#define DEBUG 1       // set to 0 for no serial debug
+
+#if DEBUG
+#define DEBUG_PRINT(S) Serial << S << endl
+#else
+#define DEBUG_PRINT(S)
+#endif
+
+// 3rd party libraries
+#include <Streaming.h>
+
+// CBUS library header files
+#include <CBUS2515.h>            // CAN controller and CBUS class
+#include <CBUSconfig.h>          // module configuration
+#include <cbusdefs.h>            // MERG CBUS constants
+#include <CBUSParams.h>
+
+////////////DEFINE MODULE/////////////////////////////////////////////////
+
+// module name - this must be 7 characters. It will be preceded by CAN in the FCU.
+unsigned char mname[7] = { '_', 'Q', 'U', 'E', 'R', 'Y', ' ' };
+
+// constants
+const byte VER_MAJ = 0;         // code major version
+const byte VER_MIN = 1;       // code minor version
+const byte VER_BETA = 0;        // code beta sub-version
+const byte MODULE_ID = 99;      // CBUS module type
+
+const unsigned long CAN_OSC_FREQ = 16000000;     // Oscillator frequency on the CAN2515 board
+
+// Forward Function declarations
+void framehandler(CANFrame *msg);
+
+// User list of opcodes.  This list is an example and can be changed as required. List is retained in program memory
+const byte opcodes[] PROGMEM = {OPC_DFNON, OPC_DFNOF, OPC_DSPD, OPC_GLOC, OPC_KLOC, OPC_PLOC,
+      OPC_ACOF,OPC_ACON,OPC_AREQ,OPC_ASRQ,OPC_AROF,OPC_ARON,OPC_ARSOF,OPC_ARSON};
+
+// Calculate number of opcodes in list
+const byte nopcodes = sizeof(opcodes) / sizeof(opcodes[0]);
+// This macro cannot be used outside a function e.g. setup() when DEBUG is 1
+//DEBUG_PRINT(F("> Number of op codes ") << nopcodes);
+
+//Define inputs here
+//Define outputs here
+
+//Allocate input pins here
+//Allocate output pins here
+
+// Define module objects here
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+//CBUS pins
+const byte CAN_INT_PIN = 2;  // Only pin 2 and 3 support interrupts
+const byte CAN_CS_PIN = 10;
+//const byte CAN_SI_PIN = 11;  Pin 50 on Mega. Cannot be changed. Here as a comment for information only.
+//const byte CAN_SO_PIN = 12;  Pin 51 on Mega. Cannot be changed. Here as a comment for information only. 
+//const byte CAN_SCK_PIN = 13;  Pin 52 on Mega. Cannot be changed. Here as a comment for information only.
+
+// CBUS objects
+CBUS2515 CBUS;                      // CBUS object
+CBUSConfig config;                  // configuration object
+
+//
+///  setup CBUS - runs once at power on called from setup()
+//
+void setupCBUS()
+{
+  // set config layout parameters
+  config.EE_NVS_START = 10;
+  config.EE_NUM_NVS = 10;
+  config.EE_EVENTS_START = 50;
+  config.EE_MAX_EVENTS = 64;
+  config.EE_NUM_EVS = 4;
+  config.EE_BYTES_PER_EVENT = (config.EE_NUM_EVS + 4);
+
+  // initialise and load configuration
+  config.setEEPROMtype(EEPROM_INTERNAL);
+  config.begin();
+
+  Serial << F("> mode = ") << ((config.FLiM) ? "FLiM" : "SLiM") << F(", CANID = ") << config.CANID;
+  Serial << F(", NN = ") << config.nodeNum << endl;
+
+  // show code version and copyright notice
+  printConfig();
+
+  // set module parameters
+  CBUSParams params(config);
+  params.setVersion(VER_MAJ, VER_MIN, VER_BETA);
+  params.setModuleId(MODULE_ID);
+  params.setFlags(PF_FLiM | PF_COMBI);
+
+  // assign to CBUS
+  CBUS.setParams(params.getParams());
+  CBUS.setName(mname);
+
+  // register our CBUS event handler, to receive event messages of learned events
+  CBUS.setEventHandler(eventhandler);
+  // register our CBUS frame handler to receive messages in user list
+  CBUS.setFrameHandler(framehandler, (byte *) opcodes, nopcodes);
+
+  // configure and start CAN bus and CBUS message processing
+  CBUS.setNumBuffers(2);         // more buffers = more memory used, fewer = less
+  CBUS.setOscFreq(CAN_OSC_FREQ);   // select the crystal frequency of the CAN module
+  CBUS.setPins(CAN_CS_PIN, CAN_INT_PIN);           // select pins for CAN bus CE and interrupt connections
+  CBUS.begin();
+}
+//
+///  setup Module - runs once at power on called from setup()
+//
+
+void setupModule()
+{
+  //Module setup code goes here for calling by setup()
+}
+
+
+void setup()
+{
+  Serial.begin (115200);
+  Serial << endl << endl << F("> ** CAN_Query ** ") << __FILE__ << endl;
+  Serial << F("> FCU name is CAN") << (char *)mname << endl;
+  //Serial.println((char *)mname);
+  setupCBUS();
+  setupModule();
+
+  DEBUG_PRINT(F("> Number of op codes ") << nopcodes);
+
+  // end of setup
+  DEBUG_PRINT(F("> ready"));
+}
+
+
+void loop()
+{
+  // do CBUS message, switch and LED processing
+  CBUS.process();
+
+  // process console commands
+  processSerialInput();
+
+  // Put module loop() code here
+
+
+
+}
+
+// Send an event
+bool sendEvent(byte opCode, unsigned int eventNo)
+{
+  CANFrame msg;
+  msg.id = config.CANID;
+  msg.len = 5;
+  msg.data[0] = opCode;
+  msg.data[1] = highByte(config.nodeNum);
+  msg.data[2] = lowByte(config.nodeNum);
+  msg.data[3] = highByte(eventNo);
+  msg.data[4] = lowByte(eventNo);
+
+  bool success = CBUS.sendMessage(&msg);
+  if (success) {
+    DEBUG_PRINT(F("> sent CBUS event with Event Number ") << eventNo);
+  } else {
+    DEBUG_PRINT(F("> error sending CBUS message"));
+  }
+  return success;
+}
+
+// Send an event with one data byte
+bool sendEvent(byte opCode, unsigned int eventNo, byte data1)
+{
+  CANFrame msg;
+  msg.id = config.CANID;
+  msg.len = 6;
+  msg.data[0] = opCode;
+  msg.data[1] = highByte(config.nodeNum);
+  msg.data[2] = lowByte(config.nodeNum);
+  msg.data[3] = highByte(eventNo);
+  msg.data[4] = lowByte(eventNo);
+  msg.data[5] = data1;
+
+  bool success = CBUS.sendMessage(&msg);
+  if (success) {
+    DEBUG_PRINT(F("> sent CBUS event with data byte ") << data1);
+  } else {
+    DEBUG_PRINT(F("> error sending CBUS message"));
+  }
+  return success;
+}
+
+// Send a frame with op code only (eg OPC_ARST)
+bool sendFrame(byte opCode)
+{
+	CANFrame msg;
+	msg.id = config.CANID;
+	msg.len = 1;
+	msg.data[0] = opCode;
+	
+	bool success = CBUS.sendMessage(&msg);
+	if (success) {
+    DEBUG_PRINT(F("> sent CBUS message op code only ") << _HEX(opCode));
+  } else {
+    DEBUG_PRINT(F("> error sending CBUS message"));
+  }
+  return success;
+}
+
+// Send a frame with op code and 2 data bytes (eg OPC_DFNON)
+bool sendFrame(byte opCode, byte data1, byte data2)
+{
+	CANFrame msg;
+	msg.id = config.CANID;
+	msg.len = 3;
+	msg.data[0] = opCode;
+	msg.data[1] = data1;
+	msg.data[2] = data2;
+	
+	bool success = CBUS.sendMessage(&msg);
+	if (success) {
+    DEBUG_PRINT(F("> sent CBUS message op Code ") << _HEX(opCode) << F("> data byte 1 ") << _HEX(data1) << F("> data byte 2 ") << _HEX(data2));
+  } else {
+    DEBUG_PRINT(F("> error sending CBUS message"));
+  }
+  return success;
+}
+
+//
+/// called from the CBUS library when a learned event is received
+//
+void eventhandler(byte index, CANFrame *msg)
+{
+
+  DEBUG_PRINT(F("> event handler: index = ") << index << F(", opcode = 0x") << _HEX(msg->data[0]));
+  DEBUG_PRINT(F("> event handler: length = ") << msg->len);
+
+  unsigned int node_number = (msg->data[1] << 8 ) + msg->data[2];
+  unsigned int event_number = (msg->data[3] << 8 ) + msg->data[4];
+  DEBUG_PRINT(F("> NN = ") << node_number << F(", EN = ") << event_number);
+  DEBUG_PRINT(F("> op_code = ") << msg->data[0]);
+
+  switch (msg->data[0])
+  {
+// Add a case for each taught event op code used
+    case OPC_ACON:
+    
+      // case code goes here
+
+      //break;
+
+    case OPC_ACOF: 
+   
+      // case code goes here
+#if DEBUG
+           Serial << F("Message handled with Opcode [ 0x") << _HEX(msg->data[0]) << F(" ]")<< endl;
+           Serial << F("Test code to see if a message is getting sent") << endl;
+#endif
+#if ACCESSORY_REQUEST_EVENT
+#if USE_SHORT_EVENTS
+      {
+         // Local variable definition needs to be in { } 
+         unsigned int device_number = 513;
+         Serial << F("Send request short event with device number ") << device_number << endl;
+         sendEvent(OPC_ASRQ,device_number); // Test of short event request.
+      }
+#else
+    //if (moduleSwitch.isPressed() ) { // Send when button is pressed.
+      byte requestEvent = 3; //For now - this is supplied in CANCMDDC by an enum.
+      Serial << F("Send request long event") << endl;
+      sendEvent(OPC_AREQ,requestEvent); // Test of long event request.
+    //}
+#endif
+#endif
+
+      break;
+  }
+}
+
+//
+/// called from the CBUS library when a message is received with an OpCode in the user list
+//
+void framehandler(CANFrame *msg)
+{
+
+  DEBUG_PRINT(F("> handling frame with opcode = 0x") << _HEX(msg->data[0]));
+
+  switch (msg->data[0])
+  {
+    // insert a case for each opcode in the user list
+
+    case OPC_DFNON:
+
+      // case code goes here
+
+      break;
+
+    case OPC_DFNOF:
+
+      //case code goes here
+      break;
+
+      // and so on for all user list op codes
+      // Copied from CANCMDDC from 2400
+      // This needs defines sorting out.
+      // It also needs to be moved to the event handler for taught events.
+      // -------------------------------------------------------------------
+    //case OPC_ACOF:
+    //case OPC_ACON:
+    //break;
+        // -------------------------------------------------------------------
+    case OPC_AROF:
+    case OPC_ARON:
+    case OPC_ARSOF:
+    case OPC_ARSON:
+        // Process responses to AREQ and ASRQ messages.
+        // I may want to set this up to handle only certain device nos.
+        // It will also be possible to send data or events based on this information.
+    {
+        byte local_opcode = msg->data[0];
+#if ACCESSORY_REQUEST_EVENT
+         unsigned int node_number = (msg->data[1] << 8 ) + msg->data[2];
+         unsigned int event_number = (msg->data[3] << 8 ) + msg->data[4];
+#if USE_SHORT_EVENTS
+        if (local_opcode == OPC_ARSON) {
+          Serial << F(" ON message from device ") << event_number << endl;
+        } else if (local_opcode == OPC_ARSOF) {
+          Serial << F(" OFF message from device ") << event_number << endl;
+        }
+#endif
+        if (local_opcode == OPC_ARON) {
+          Serial << F(" ON message from event ") << node_number << F(" event ") << event_number << endl;
+        } else if (local_opcode == OPC_AROF) {
+          Serial << F(" OFF message from node ") << node_number << F(" event ") << event_number << endl;
+        }
+#endif   
+        }     
+        break;
+        // -------------------------------------------------------------------
+        default:
+          // ignore any other CBus messages
+#if DEBUG
+           Serial << F("Message handled with Opcode [ 0x") << _HEX(msg->data[0]) << F(" ]")<< endl;
+#endif
+          break;
+   }
+}
+
+
+void printConfig(void)
+{
+  // code version
+  Serial << F("> code version = ") << VER_MAJ << VER_MIN << F(" beta ") << VER_BETA << endl;
+  Serial << F("> compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(", compiler ver = ") << __cplusplus << endl;
+
+  // copyright
+  Serial << F("> © Martin Da Costa (MERG M6223) 2021 & 2022") << endl;
+  Serial << F("> © Duncan Greenwood (MERG M5767) 2021") << endl;
+  Serial << F("> © John Fletcher (MERG M6777) 2021") << endl;
+  Serial << F("> © Sven Rosvall (MERG M3777) 2021") << endl;
+}
+
+//
+/// command interpreter for serial console input
+//
+
+void processSerialInput(void)
+{
+  byte uev = 0;
+  char msgstr[32];
+
+  if (Serial.available()) {
+    char c = Serial.read();
+
+    switch (c) {
+
+      case 'n':
+        // node config
+        printConfig();
+
+        // node identity
+        Serial << F("> CBUS node configuration") << endl;
+        Serial << F("> mode = ") << (config.FLiM ? "FLiM" : "SLiM") << F(", CANID = ") << config.CANID << F(", node number = ") << config.nodeNum << endl;
+        Serial << endl;
+        break;
+
+      case 'e':
+        // EEPROM learned event data table
+        Serial << F("> stored events ") << endl;
+        Serial << F("  max events = ") << config.EE_MAX_EVENTS << F(" EVs per event = ") << config.EE_NUM_EVS << F(" bytes per event = ") << config.EE_BYTES_PER_EVENT << endl;
+
+        for (byte j = 0; j < config.EE_MAX_EVENTS; j++) {
+          if (config.getEvTableEntry(j) != 0) {
+            ++uev;
+          }
+        }
+
+        Serial << F("  stored events = ") << uev << F(", free = ") << (config.EE_MAX_EVENTS - uev) << endl;
+        Serial << F("  using ") << (uev * config.EE_BYTES_PER_EVENT) << F(" of ") << (config.EE_MAX_EVENTS * config.EE_BYTES_PER_EVENT) << F(" bytes") << endl << endl;
+
+        Serial << F("  Ev#  |  NNhi |  NNlo |  ENhi |  ENlo | ");
+
+        for (byte j = 0; j < (config.EE_NUM_EVS); j++) {
+          sprintf(msgstr, "EV%03d | ", j + 1);
+          Serial << msgstr;
+        }
+
+        Serial << F("Hash |") << endl;
+
+        Serial << F(" --------------------------------------------------------------") << endl;
+
+        // for each event data line
+        for (byte j = 0; j < config.EE_MAX_EVENTS; j++) {
+          if (config.getEvTableEntry(j) != 0) {
+            sprintf(msgstr, "  %03d  | ", j);
+            Serial << msgstr;
+
+            // for each data byte of this event
+            for (byte e = 0; e < (config.EE_NUM_EVS + 4); e++) {
+              sprintf(msgstr, " 0x%02hx | ", config.readEEPROM(config.EE_EVENTS_START + (j * config.EE_BYTES_PER_EVENT) + e));
+              Serial << msgstr;
+            }
+
+            sprintf(msgstr, "%4d |", config.getEvTableEntry(j));
+            Serial << msgstr << endl;
+          }
+        }
+
+        Serial << endl;
+
+        break;
+
+      // NVs
+      case 'v':
+        // note NVs number from 1, not 0
+        Serial << "> Node variables" << endl;
+        Serial << F("   NV   Val") << endl;
+        Serial << F("  --------------------") << endl;
+
+        for (byte j = 1; j <= config.EE_NUM_NVS; j++) {
+          byte v = config.readNV(j);
+          sprintf(msgstr, " - %02d : %3hd | 0x%02hx", j, v, v);
+          Serial << msgstr << endl;
+        }
+
+        Serial << endl << endl;
+
+        break;
+
+      // CAN bus status
+      case 'c':
+        CBUS.printStatus();
+        break;
+
+      case 'h':
+        // event hash table
+        config.printEvHashTable(false);
+        break;
+
+      case 'y':
+        // reset CAN bus and CBUS message processing
+        CBUS.reset();
+        break;
+
+      case '*':
+        // reboot
+        config.reboot();
+        break;
+
+      case 'm':
+        // free memory
+        Serial << F("> free SRAM = ") << config.freeSRAM() << F(" bytes") << endl;
+        break;
+
+      case 'r':
+        // renegotiate
+        CBUS.renegotiate();
+        break;
+
+      case 'z':
+        // Reset module, clear EEPROM
+        static bool ResetRq = false;
+        static unsigned long ResWaitTime;
+        if (!ResetRq) {
+          // start timeout timer
+          Serial << F(">Reset & EEPROM wipe requested. Press 'z' again within 2 secs to confirm") << endl;
+          ResWaitTime = millis();
+          ResetRq = true;
+        }
+        else {
+          // This is a confirmed request
+          // 2 sec timeout
+          if (ResetRq && ((millis() - ResWaitTime) > 2000)) {
+            Serial << F(">timeout expired, reset not performed") << endl;
+            ResetRq = false;
+          }
+          else {
+            //Request confirmed within timeout
+            Serial << F(">RESETTING AND WIPING EEPROM") << endl;
+            config.resetModule();
+            ResetRq = false;
+          }
+        }
+        break;
+
+      default:
+        // Serial << F("> unknown command ") << c << endl;
+        break;
+    }
+  }
+}
---
+++ CAN_Query_Response/README.md
@@ -0,0 +1,189 @@
+<img align="right" src="arduino_cbus_logo.png"  width="150" height="75">
+
+# CAN_Query
+
+This is developed to demonstrate a query and response operation.
+
+# CAN_Wrapper
+
+An Arduino program that provides a MERG CBUS interface that can interface with the users
+application.  The program is derived from CANmINnOUT, which was originally developed as
+a technology demonstrator. CANmINnOUT thus serves as an exmaple of how to use this program
+and can be found here:
+
+https://github.com/MartinDaCosta53/CANmINnOUT
+
+Key Features:
+- MERG CBUS interface
+- Modular construction to ease adaptation to your application.
+
+## Overview
+
+The program does not allow for the inclusion of a CBUS setup switch or LEDs. Consequently,
+the following library versions are required:
+- CBUS Version 1.1.14 or later
+- CBUSConfig 1.1.10 or later
+Notwithstanding the fact that they are not used, CBUSSwitch and CBUSLED libraries must be 
+available for access by the CBUS and CBUSConfig libraries for reason of backwards library
+compatibility.
+
+It is possible that the CBUS setup switch could be reinstated for purposes of requesting a
+Node Number.  The LEDs remain superfluous as SLiM is not supported and the module is always
+in FLiM.
+
+## Setting up CAN_Wrapper for use
+
+The MCP2515 interface requires five Arduino pins to be allocated. Three of these are fixed
+in the architecture of the Arduino processor. One pin must be connected to an interrupt
+capable Arduino pin. Thus, the total number of pins available for input or output is:
+- UNO  13 pins
+- NANO 15 pins (Note that two of these, A6 & A7, are analogue inputs only)
+- MEGA 63 pins
+
+**It is the users responsibility that the total current that the Arduino is asked to supply 
+**stays within the capacity of the on board regulator.  Failure to do this will result in 
+**terminal damage to your Arduino.
+
+### Library Dependancies
+
+The following third party libraries are required:
+Library | Purpose
+---------------|-----------------
+Streaming.h  |*C++ stream style output, v5, (http://arduiniana.org/libraries/streaming/)*
+ACAN2515.h   |*library to support the MCP2515/25625 CAN controller IC*
+CBUS2515.h   |*CAN controller and CBUS class
+CBUSconfig.h |*module configuration*
+CBUS.h       |*CBUS Class*
+cbusdefs.h   |*Definition of CBUS codes*
+CBUSParams.h   |*Manage CBUS parameters*
+CBUSSwitch.h   |*library compatibility*
+CBUSLED.h      |*library compatibility*
+
+### Pre-Processor Options
+
+Various output reports can be made to the serial monitor for use in debugging by
+changing 0 to 1 for the DEBUG macro in the DEFINE MODULE section at line c.73.
+```
+#define DEBUG 0       // set to 0 for no serial debug
+```
+
+### Application Configuration
+
+#### Module Name
+The module name can be adjusted as required to reflect the module configuration.  Only one
+character is allowed between each pair of ' ' and the total number of characters must be seven.
+```
+// module name
+unsigned char mname[7] = { 'C', 'A', 'N', 'w', 'r', 'a', 'p' };
+```
+
+#### Oscillator Frequency
+If the oscillator frequency on your CAN2515 board is not 16MHz, change the number to match. The 
+module will not work if this number does not match the oscillator frequency.
+```
+const unsigned long CAN_OSC_FREQ = 16000000;     // Oscillator frequency on the CAN2515 board
+```
+
+#### Framehandler
+The following section of code configures the module for using the library framehandler. If the
+application does not need framehandler, it can be safely deleted. If not included, the function
+setupCBUS() must also be modified to delete:
+```
+// register our CBUS frame handler to receive messages in user list
+  CBUS.setFrameHandler(framehandler, (byte *) opcodes, nopcodes);
+```
+Framehandler is NOT included in the CANmINnOUT example.
+```
+// Forward Function declarations
+void framehandler(CANFrame *msg);
+
+// User list of opcodes.  This list is an example and can be changed as required. List is retained in program memory
+const byte opcodes[] PROGMEM = {OPC_DFNON, OPC_DFNOF, OPC_DSPD, OPC_GLOC, OPC_KLOC, OPC_PLOC};
+
+// Calculate number of opcodes in list
+const byte nopcodes = sizeof(opcodes) / sizeof(opcodes[0]);
+DEBUG_PRINT(F("> Number of op codes ") << nopcodes);
+```
+The list of op codes at line c.108 is just an example.  It can consist of as many or as few op codes 
+as required.  They op codes specified MUST be in the cbusdefs.h or a compile error will occur.
+The hex op code may also be used (eg OPC_DFNON could be replaced by 0x49)
+
+## sendEvent function
+
+This function can be overloaded as required by the op code.  Included are functions for no data bytes
+and one data byte.  Simple extrapolation of these allows for two or three data bytes.
+
+## sendFrame function
+
+This function is a more general version of the sendEvent function.  This function is only required if 
+the application is required to generate op codes that are not within the subset of taught events 
+supported by the CBUS library.  Included are functions for op code only and two data bytes.  Simple
+extrapolation of these allows for one to seven data bytes.
+
+## CBUS Op Codes for Learnt Events
+
+The following learnt event Op codes are supported by the CBUS library:
+OP_CODE | HEX | Function
+----------|---------|---------
+ OPC_ACON | 0x90 | On event
+ OPC_ACON1| 0xB0 | On event with one data byte
+ OPC_ACON2| 0xD0 | On event with two data byte
+ OPC_ACON3| 0xF0 | On event with three data byte
+ OPC_ACOF | 0x91 | Off event
+ OPC_ACOF1| 0xB1 | Off event with one data byte
+ OPC_ACOF2| 0xD1 | Off event with two data byte
+ OPC_ACOF3| 0xF1 | Off event with three data byte
+ OPC_ARON | 0x93 | Accessory response event on
+ OPC_AROF | 0x94 | Accessory response event off
+ OPC_ASON | 0x98 | Short event on
+ OPC_ASON1| 0xB8 | Short event on with one data byte
+ OPC_ASON2| 0xD8 | Short event on with two data byte
+ OPC_ASON3| 0xF8 | Short event on with three data byte
+ OPC_ASOF | 0x99 | Short event off
+ OPC_ASOF1| 0xB9 | Short event off with one data byte
+ OPC_ASOF2| 0xD9 | Short event off with two data byte
+ OPC_ASOF3| 0xF9 | Short event off with three data byte
+
+Other op codes that are not supported in the CBUS library with learnt event variables
+can be accomodated in the frame handler.
+ 
+## Set Up and the Serial Monitor
+
+Without a CBUS switch, it is necessary to have another means of registering the module on 
+the CBUS and acquiring a node number.  This is accomplished by sending a single character to 
+the Arduino using the serial send facility in the serial monitor.
+
+#### 'r'
+This character will cause the module to renegotiate its CBUS status by requesting a node number.
+The FCU will respond as it would for any other unrecognised module.
+
+#### 'z'
+This character needs to be sent twice within 2 seconds so that its action is confirmed.
+This will reset the module and clear the EEPROM.  It should thus be used with care.
+
+Other information is available from the serial monitor using other commands:
+
+#### 'n'
+This character will return the node configuration.
+
+#### 'e'
+This character will return the learned event table in the EEPROM.
+
+#### 'v'
+This character will return the node variables.
+
+#### 'c'
+This character will return the CAN bus status.
+
+#### 'h'
+This character will return the event hash table.
+
+#### 'y'
+This character will reset the CAN bus and CBUS message processing.
+
+#### '\*'
+This character will reboot the module.
+
+#### 'm'
+This character will return the amount of free memory. 
+ 
