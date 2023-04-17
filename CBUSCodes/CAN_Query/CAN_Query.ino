6033f83bb4c85fe7cbd4a1702d25aaed73173ea2
John Fletcher <john@bunbury28.plus.com>
2023-04-17 12:21:38 +0100

    Update CAN_Query.ino
    Update comments

--- CBUSCodes/CAN_Query/CAN_Query.ino
+++ CBUSCodes/CAN_Query/CAN_Query.ino
@@ -1,9 +1,9 @@
 // CAN_Query.ino
 // copied from CANWrapper
+// That uses IoAbstraction and TaskManagerIO.
+// Code for responding is in CANASIGNAL and is going to be added to make CAN_Query_Response
 // I am going to extend this to have a state machine
 // using the example in arduino_state_event_keypad as a basis.
-// That uses IoAbstraction and TaskManagerIO.
-// Code for responding is in CANASIGNAL 
 
 /*
   Copyright (C) 2021 Martin Da Costa
