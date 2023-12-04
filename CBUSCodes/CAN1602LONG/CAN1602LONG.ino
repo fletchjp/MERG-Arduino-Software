eb1220638bbb0a9a4ef3b785fbb2e0375507350e
John Fletcher <john@bunbury28.plus.com>
2023-12-04 18:00:27 +0000

    Update CAN1602LONG.ino
    correct constant

--- CBUSCodes/CAN1602LONG/CAN1602LONG.ino
+++ CBUSCodes/CAN1602LONG/CAN1602LONG.ino
@@ -141,7 +141,7 @@ const char VER_MIN = 'a';       // code minor version
 const byte VER_BETA = 1;        // code beta sub-version
 const byte MODULE_ID = 99;      // CBUS module type
 
-const unsigned long CAN_OSC_FREQ = 8000000;     // Oscillator frequency on the CAN2515 board
+const unsigned long CAN_OSC_FREQ = 8000000UL;     // Oscillator frequency on the CAN2515 board
 
 #define NUM_LEDS 1              // How many LEDs are there?
 #define NUM_SWITCHES 1          // How many switchs are there?
