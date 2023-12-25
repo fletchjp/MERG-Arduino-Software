3c2b8e90e9ea7f34c5b503b067cc0ba3a051f58e
John Fletcher <john@bunbury28.plus.com>
2023-12-25 20:29:43 +0000

    Change to CBUSswitch,h

--- CBUSCodes/CBUS_1in1out_ACAN2040_v2/CBUS_1in1out_ACAN2040_v2.ino
+++ CBUSCodes/CBUS_1in1out_ACAN2040_v2/CBUS_1in1out_ACAN2040_v2.ino
@@ -61,7 +61,7 @@ static const byte WIRE_SCL = 7;
 
 // CBUS library header files
 #include <CBUSACAN2040.h>           // CAN controller and CBUS class
-#include <CBUSSwitch.h>             // pushbutton switch
+#include <CBUSswitch.h>             // pushbutton switch
 #include <CBUSLED.h>                // CBUS LEDs
 #include <CBUSconfig.h>             // module configuration
 #include <CBUSParams.h>             // CBUS parameters
