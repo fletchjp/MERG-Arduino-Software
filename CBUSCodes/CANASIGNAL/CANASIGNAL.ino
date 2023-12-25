6db366c43673860e1765681447c3bfbb7dc117f4
John Fletcher <john@bunbury28.plus.com>
2023-12-25 20:20:15 +0000

    Change to CBUSswitch.h

--- CBUSCodes/CANASIGNAL/CANASIGNAL.ino
+++ CBUSCodes/CANASIGNAL/CANASIGNAL.ino
@@ -153,7 +153,7 @@ public:
 
 // CBUS library header files
 #include <CBUS2515.h>               // CAN controller and CBUS class
-#include <CBUSSwitch.h>             // pushbutton switch
+#include <CBUSswitch.h>             // pushbutton switch
 #include <CBUSLED.h>                // CBUS LEDs
 #include <CBUSconfig.h>             // module configuration
 #include <cbusdefs.h>               // MERG CBUS constants
