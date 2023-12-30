c59c4864b9aa09f76cfe38bf9ab89b505ce6e47b
John Fletcher <john@bunbury28.plus.com>
2023-12-30 15:16:14 +0000

    Update DUEcanImplExample.ino
    Make changes to limit testing

--- TestEEPROM/DUEcanImplExample/DUEcanImplExample.ino
+++ TestEEPROM/DUEcanImplExample/DUEcanImplExample.ino
@@ -6,12 +6,13 @@
 
 #ifdef __SAM3X8E__
 #include "processor.h"            // auto-selects the processor type, and CAN lib, EEPROM lib etc.  
+#else
+#include <DueFlashStorage.h>
+DueFlashStorage dueFlashStorage;
 #endif
 // 3rd party libraries
 #include <Streaming.h>
 
-#include <DueFlashStorage.h>
-DueFlashStorage dueFlashStorage;
 
 // The struct of the configuration.
 struct Configuration {
@@ -28,7 +29,13 @@ Configuration configuration;
 void setup() {
   Serial.begin(115200);
   delay(500);
-
+  Serial << "DUEcanImpl testing" << endl;
+#ifdef __SAM3X8E__
+  Serial << "Using OpenLCB EEPROM with " << EEPROM.length() << endl;
+  Serial << "Location 0 " << EEPROM.read(0) << endl;
+  //EEPROM.write(0,1);
+  //Serial.flush();
+#else
   /* Flash is erased every time new code is uploaded. Write the default configuration to flash if first time */
   // running for the first time?
   uint8_t codeRunningForTheFirstTime = dueFlashStorage.read(0); // flash bytes will be 255 at first run
@@ -53,18 +60,25 @@ void setup() {
   else {
     Serial.println("no");
   }
+#endif
 #ifdef __SAM3X8E__
     Serial << "Using OpenLCB EEPROM with " << EEPROM.length() << endl;
+    Serial << "Location 0 " << EEPROM.read(0) << endl;
 #endif
 
 }
 
 void loop() {
   /* read configuration struct from flash */
+#ifndef __SAM3X8E__
   byte* b = dueFlashStorage.readAddress(4); // byte array which is read from flash at adress 4
   Configuration configurationFromFlash; // create a temporary struct
   memcpy(&configurationFromFlash, b, sizeof(Configuration)); // copy byte array to temporary struct
+#endif
 
+#ifdef __SAM3X8E__
+  Serial << "Testing in loop" << endl;
+#else
   // print the content
   Serial.print("a:");
   Serial.print(configurationFromFlash.a);
@@ -82,7 +96,6 @@ void loop() {
   Serial.print(configurationFromFlash.c);
   Serial.println();
   Serial.println();
-
   /* change some values in the struct and write them back */
   // increment b by 1 (modulus 100 to start over at 0 when 100 is reached)
   configurationFromFlash.b = (configurationFromFlash.b+1) % 100;
@@ -98,6 +111,7 @@ void loop() {
   byte b2[sizeof(Configuration)]; // create byte array to store the struct
   memcpy(b2, &configurationFromFlash, sizeof(Configuration)); // copy the struct to the byte array
   dueFlashStorage.write(4, b2, sizeof(Configuration)); // write byte array to flash
+#endif
 
   delay(1000);
 }
