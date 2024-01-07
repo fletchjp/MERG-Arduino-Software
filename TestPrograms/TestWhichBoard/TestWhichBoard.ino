3cbf6416d8bb0f4b6b6d3c6b5bdb021b61812dac
John Fletcher <john@bunbury28.plus.com>
2024-01-07 19:51:09 +0000

    Update TestWhichBoard.ino
    Include code from the example

--- TestPrograms/TestWhichBoard/TestWhichBoard.ino
+++ TestPrograms/TestWhichBoard/TestWhichBoard.ino
@@ -9,6 +9,8 @@
 ///
 /// To avoid the library overhead, the code can be inspected to find the macros needed.
 
+// Adding this line before the library will output compile time identification.
+#define ARCH_IDENTIFY_WARNING
 #define BOARD_INDENTITY_WARNING
 #include <MyMacros.h>
 
@@ -26,11 +28,45 @@ void heading()
   Serial << F(" compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(" using compiler ") << __cplusplus << endl;
 }
 
+// Optional define a structure variable. Only required for access to struct data.
+struct board myBoard;
+
 void setup() {
   // put your setup code here, to run once:
-  while(!Serial);
-  Serial.begin (115200);
+  Serial.begin (115200); while(!Serial); Serial.println();
   heading();
+  Serial.println("==== Setup ====");
+  Serial.print("Matching board define: ");
+  printlnMatch(); 
+  Serial.print("Arduino's 'BUILD_BOARD' property: ");
+  Serial.println(BUILD_BOARD);
+  delay(1000);
+  // Adds the personal collection of boards and 100+ boards from Board Identify library
+    // Use members in struct 'MyBoard' to access identity information from 'MyMacros.h'.
+    Serial.println("==== MyMacros (Automatically pulls in from Board Identify) ===="); 
+    Serial.print("Board Type: ");
+    Serial.println(myBoard.type);  
+    Serial.print("Board Make: ");
+    Serial.println(myBoard.make);
+    Serial.print("Board Model: ");
+    Serial.println(myBoard.model);
+    Serial.print("Board MCU: ");
+  
+    delay(3000);
+
+  // Should be identical to above
+    Serial.println("==== Board Identify (source board data) ====");
+    Serial.print("Board Type: ");
+    Serial.println(BoardIdentify::type);
+    Serial.print("Board Make: ");
+    Serial.println(BoardIdentify::make);
+    Serial.print("Board Model: ");
+    Serial.println(BoardIdentify::model);
+    Serial.print("Board MCU: ");
+    Serial.println(BoardIdentify::mcu);
+    Serial.println();
+
+    delay(3000);
 }
 
 void loop() {
