5ec1fd69bd2fd023a4441c26cafd9194917b1f09
John Fletcher <john@bunbury28.plus.com>
2024-01-22 12:34:28 +0000

    Update MEGATaskIntegrationCBUS.ino
    Add a lot more code

--- TestDisplay/LiquidCrystalIO/MEGATaskIntegrationCBUS/MEGATaskIntegrationCBUS.ino
+++ TestDisplay/LiquidCrystalIO/MEGATaskIntegrationCBUS/MEGATaskIntegrationCBUS.ino
@@ -578,6 +578,38 @@ void setupCBUS()
   CBUS.begin();
 }
 
+void eventhandler(byte index, CANFrame *msg) {
+ // as an example, control an LED if the first EV equals 1
+
+  Serial << F("> event handler: index = ") << index << F(", opcode = 0x") << _HEX(msg->data[0]) << endl;
+}
+
+/// Send an event routine built to start sending events based with one extra byte
+/// The events can be ACON1 or ACOF1 with 1 byte of data.
+bool sendEvent1(byte opCode, unsigned int eventNo, byte item)
+{
+    CANFrame msg;
+    msg.id = config.CANID;
+    msg.len = 6;
+    msg.data[0] = opCode;
+    msg.data[1] = highByte(config.nodeNum);
+    msg.data[2] = lowByte(config.nodeNum);
+    msg.data[3] = highByte(eventNo); // event number (EN) could be > 255
+    msg.data[4] = lowByte(eventNo); 
+    msg.data[5] = item;    // The extra byte
+    msg.ext = false;
+    msg.rtr = false;
+
+    bool res = CBUS.sendMessage(&msg);
+#if DEBUG
+    if (res) {
+      Serial << F("> sent CBUS event with opCode [ 0x") << _HEX(opCode) << F(" ] and event No ") << eventNo << endl;
+    } else {
+      Serial << F("> error sending CBUS event wit opcode [ 0x") <<  _HEX(opCode) << F(" ]") << endl;
+    }
+#endif
+    return res;
+}
 
 void setup() {
 /// setup 
@@ -670,7 +702,202 @@ lastPins = pins;
  }
 }
 
+void printConfig(void) {
+
+  // code version
+  Serial << F("> code version = ") << VER_MAJ << VER_MIN << F(" beta ") << VER_BETA << endl;
+  Serial << F("> compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(", compiler ver = ") << __cplusplus << endl;
+#if CANBUS8MHZ
+  Serial << F("> Set for 8Mhz crystal") << endl;
+#endif
+  Serial << F("> CS Pin ") << CHIPSELECT << endl;
+  Serial << F("> INT Pin ") << CBUSINTPIN << endl; 
+// copyrights collected so far
+  Serial << F("> © Duncan Greenwood (MERG M5767) 2019") << endl;
+  Serial << F("> © John Fletcher (MERG M6777) 2019") << endl;
+#if DEBUG
+  #if CBUS_EVENTS
+    Serial << F("> CBUS Events available") << endl;
+  #endif
+#endif
+
+  return;
+}
+
+
+
 void loop() {
+
+  ///
+  /// do CBUS message, switch and LED processing
+  //
+  CBUS.process();
+
+ //
+  /// process console commands
+  //
+
+  processSerialInput();
+
 /** as this indirectly uses taskmanager, we must include taskManager.runLoop(); in loop. */
     taskManager.runLoop();
 }
+
+void processSerialInput(void) {
+
+  byte uev = 0;
+  char msgstr[32], dstr[32];
+
+  if (Serial.available()) {
+
+    char c = Serial.read();
+
+    switch (c) {
+
+      case 'n':
+
+        // node config
+        printConfig();
+
+        // node identity
+        Serial << F("> CBUS node configuration") << endl;
+        Serial << F("> mode = ") << (config.FLiM ? "FLiM" : "SLiM") << F(", CANID = ") << config.CANID << F(", node number = ") << config.nodeNum << endl;
+        Serial << endl;
+        break;
+
+      case 'e':
+
+        // EEPROM learned event data table
+        Serial << F("> stored events ") << endl;
+
+        sprintf(msgstr, "  max events = %d, EVs per event = %d, bytes per event = %d", config.EE_MAX_EVENTS, config.EE_NUM_EVS, config.EE_BYTES_PER_EVENT);
+        Serial << msgstr << endl;
+
+        for (byte j = 0; j < config.EE_MAX_EVENTS; j++) {
+          if (config.getEvTableEntry(j) != 0) {
+            ++uev;
+          }
+        }
+
+        Serial << F("  stored events = ") << uev << F(", free = ") << (config.EE_MAX_EVENTS - uev) << endl;
+        Serial << F("  using ") << (uev * config.EE_BYTES_PER_EVENT) << F(" of ") << (config.EE_MAX_EVENTS * config.EE_BYTES_PER_EVENT) << F(" bytes") << endl << endl;
+
+        Serial << F("  Ev#  |  NNhi |  NNlo |  ENhi |  ENlo | ");
+
+        for (byte j = 0; j < (config.EE_NUM_EVS); j++) {
+          sprintf(dstr, "EV%03d | ", j + 1);
+          Serial << dstr;
+        }
+
+        Serial << F("Hash |") << endl;
+
+        Serial << F(" --------------------------------------------------------------") << endl;
+
+        // for each event data line
+        for (byte j = 0; j < config.EE_MAX_EVENTS; j++) {
+
+          if (config.getEvTableEntry(j) != 0) {
+            sprintf(dstr, "  %03d  | ", j);
+            Serial << dstr;
+
+            // for each data byte of this event
+            for (byte e = 0; e < (config.EE_NUM_EVS + 4); e++) {
+              sprintf(dstr, " 0x%02hx | ", config.readEEPROM(config.EE_EVENTS_START + (j * config.EE_BYTES_PER_EVENT) + e));
+              Serial << dstr;
+            }
+
+            sprintf(dstr, "%4d |", config.getEvTableEntry(j));
+            Serial << dstr << endl;
+          }
+        }
+
+        Serial << endl;
+
+        break;
+
+      // NVs
+      case 'v':
+
+        // note NVs number from 1, not 0
+        Serial << "> Node variables" << endl;
+        Serial << F("   NV   Val") << endl;
+        Serial << F("  --------------------") << endl;
+
+        for (byte j = 1; j <= config.EE_NUM_NVS; j++) {
+          sprintf(msgstr, " - %02d : %3hd | 0x%02hx", j, config.readNV(j), config.readNV(j));
+          Serial << msgstr << endl;
+        }
+
+        Serial << endl << endl;
+
+        break;
+
+      // CAN bus status
+      case 'c':
+
+        CBUS.printStatus();
+        break;
+
+      case 'h':
+        // event hash table
+        config.printEvHashTable(false);
+        break;
+
+      case 'y':
+        // reset CAN bus and CBUS message processing
+        CBUS.reset();
+        break;
+
+      case '*':
+        // reboot
+        config.reboot();
+        break;
+
+      case 'm':
+        // free memory
+        Serial << F("> free SRAM = ") << config.freeSRAM() << F(" bytes") << endl;
+        break;
+
+      case 'r':
+        // renegotiate
+        CBUS.renegotiate();
+        break;
+
+      case 'z':
+        // Reset module, clear EEPROM
+        static bool ResetRq = false;
+        static unsigned long ResWaitTime;
+        if (!ResetRq) {
+          // start timeout timer
+          Serial << F(">Reset & EEPROM wipe requested. Press 'z' again within 2 secs to confirm") << endl;
+          ResWaitTime = millis();
+          ResetRq = true;
+        }
+        else {
+          // This is a confirmed request
+          // 2 sec timeout
+          if (ResetRq && ((millis() - ResWaitTime) > 2000)) {
+            Serial << F(">timeout expired, reset not performed") << endl;
+            ResetRq = false;
+          }
+          else {
+            //Request confirmed within timeout
+            Serial << F(">RESETTING AND WIPING EEPROM") << endl;
+            config.resetModule();
+            ResetRq = false;
+          }
+        }
+        break;
+
+      case '\r':
+      case '\n':
+        Serial << endl;
+        break;
+
+      default:
+        // Serial << F("> unknown command ") << c << endl;
+        break;
+    }
+  }
+}
+
