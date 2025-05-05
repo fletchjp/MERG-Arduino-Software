8424c65dac9fcd61d642a7de7bb2af7e5238e058
John Fletcher <john@bunbury28.plus.com>
2025-05-06 00:41:32 +0100

    Create Receiver.ino
    Add ThroughLoRa Receiver example

---
+++ ArduinosConnectedTogether/PJONExamples/ThroughLoRa/Receiver/Receiver.ino
@@ -0,0 +1,38 @@
+
+
+
+#include <PJONThroughLora.h>
+
+/* To use this example, please download the LoRa third party Library from
+https://github.com/sandeepmistry/arduino-LoRa/ */
+
+
+PJONThroughLora bus(44);
+
+void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+	/* Make use of the payload before sending something, the buffer where payload points to is
+	   overwritten when a new message is dispatched */
+	if (payload[0] == 'B') {
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(30);
+		digitalWrite(LED_BUILTIN, LOW);
+	}
+};
+
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW); // Initialize LED 13 to be off
+	// Synchronous acknowledgement is not supported
+  bus.set_acknowledge(false);
+	// Obligatory to initialize Radio with correct frequency
+	bus.strategy.setFrequency(868100000UL);
+	// Optional
+	bus.strategy.setSignalBandwidth(250E3);
+	bus.begin();
+
+	bus.set_receiver(receiver_function);
+};
+
+void loop() {
+	bus.receive(1000);
+};
